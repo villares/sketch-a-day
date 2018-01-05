9b4751ebf87e1b4c2bde2611989087a4610c94fa
Alexandre B A Villares <abav@lugaralgum.com>
2018-01-05 19:41:18 -0200

    done for the day...

--- s180105/s180105.js
+++ s180105/s180105.js
@@ -1,8 +1,7 @@
 /*
 
-*/
+ */
 
-var easycam;
 
 var tetrah_list = [];
 var rot_x = 0;
@@ -11,8 +10,6 @@ var rot_y = 0;
 function setup() {
   canvas = createCanvas(500, 500, WEBGL);
   setAttributes('antialias', true);
-  easycam = createEasyCam();
-    print(Dw.EasyCam.INFO);
 
   for (var i = -5; i < 5; i++) {
     for (var j = -5; j < 5; j++) {
@@ -24,56 +21,54 @@ function setup() {
     }
   }
 }
-  function draw() {
-    background(0);
-    rot_x += 0.005;
-    rot_y += 0.01;
-    for (var i = 0; i < tetrah_list.length; i++) { // itera pela lista apas
-      tetrah_list[i].plot();
-    }
+function draw() {
+  background(0);
+  strokeWeight(5);
+  rot_x += 0.05;
+  rot_y += 0.01;
+  for (var i = 0; i < tetrah_list.length; i++) { // itera pela lista apas
+    tetrah_list[i].plot();
   }
+}
 
-  function Tetrahedron(x, y, z, radius, showFaces) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.vert = [];
-    this.radius = radius;
-    this.showFaces = showFaces;
-    var a = this.radius*2/3;
-    this.vert[0] = new p5.Vector( a, a, a ); // vertex 1
-    this.vert[1] = new p5.Vector(-a, -a, a ); // vertex 2
-    this.vert[2] = new p5.Vector(-a, a, -a ); // vertex 3
-    this.vert[3] = new p5.Vector( a, -a, -a ); // vertex 4
-    // draws tetrahedron 
-    this.plot = function(rx, ry) {
-      push();
-      noFill();
-      strokeWeight(5);
-      stroke(200, 0, 0, 10); 
-      translate(this.x, this.y, this.z);
-      rotateX(rot_y);
-      rotateY(rot_x);
-      beginShape();
-      vertex(this.vert[0].x, this.vert[0].y, this.vert[0].z); // vertex 1
-      vertex(this.vert[1].x, this.vert[1].y, this.vert[1].z); // vertex 2
-      vertex(this.vert[2].x, this.vert[2].y, this.vert[2].z); // vertex 3
-      endShape(CLOSE);
-      beginShape();
-      vertex(this.vert[0].x, this.vert[0].y, this.vert[0].z); // vertex 1
-      vertex(this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
-      vertex(this.vert[2].x, this.vert[2].y, this.vert[2].z); // vertex 3
-      endShape(CLOSE); 
-      beginShape();
-      vertex(this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
-      vertex(this.vert[0].x, this.vert[0].y, this.vert[0].z); // vertex 1
-      vertex(this.vert[1].x, this.vert[1].y, this.vert[1].z); // vertex 2
-      endShape(CLOSE); 
-      beginShape();
-      vertex(this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
-      vertex(this.vert[2].x, this.vert[2].y, this.vert[2].z); // vertex 3
-      vertex(this.vert[1].x, this.vert[1].y, this.vert[1].z); // vertex 2
-      endShape(CLOSE);
-      pop();
+function Tetrahedron(x, y, z, radius, showFaces) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.vert = [];
+  this.radius = radius;
+  var a = this.radius*2/3;
+  this.vert[0] = new p5.Vector( a, a, a ); // vertex 1
+  this.vert[1] = new p5.Vector(-a, -a, a ); // vertex 2
+  this.vert[2] = new p5.Vector(-a, a, -a ); // vertex 3
+  this.vert[3] = new p5.Vector( a, -a, -a ); // vertex 4
+  // draws tetrahedron 
+  this.plot = function(rx, ry) {
+    push();
+    var tz = this.z
+      if (dist(mouseX-width/2, mouseY-height/2, this.x, this.y)<50) {
+      tz = tz * 10;
+      stroke(0, 100, 200);
+    } else {
+      stroke(200, 100, 0);
     }
+    translate(this.x, this.y, tz);
+    rotateX(rot_y);
+    rotateY(rot_x);
+    line(this.vert[0].x, this.vert[0].y, this.vert[0].z, // vertex 1
+      this.vert[1].x, this.vert[1].y, this.vert[1].z); // vertex 2
+    line(this.vert[0].x, this.vert[0].y, this.vert[0].z, // vertex 1
+      this.vert[2].x, this.vert[2].y, this.vert[2].z); // vertex 3
+    line(this.vert[0].x, this.vert[0].y, this.vert[0].z, // vertex 1
+      this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
+    line(this.vert[1].x, this.vert[1].y, this.vert[1].z, // vertex 2
+      this.vert[2].x, this.vert[2].y, this.vert[2].z); // vertex 3
+    line(this.vert[1].x, this.vert[1].y, this.vert[1].z, // vertex 2
+      this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
+    line(this.vert[2].x, this.vert[2].y, this.vert[2].z, // vertex 3
+      this.vert[3].x, this.vert[3].y, this.vert[3].z); // vertex 4
+
+
+    pop();
   }
+}
