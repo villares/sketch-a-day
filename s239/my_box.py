6be534c81b1b2bb85b08b79129e4fd4a92f50901
Alexandre B A Villares <abav@lugaralgum.com>
2018-08-25 17:44:22 -0300

    239 first

---
+++ s239/my_box.py
@@ -0,0 +1,48 @@
+def my_box(side):
+    front = ((-1, +1, -1),
+             (-1, +1, +1),
+             (+1, +1, +1),
+             (+1, +1, -1),
+             )
+    back = ((-1, -1, -1),
+            (-1, -1, +1),
+            (+1, -1, +1),
+            (+1, -1, -1),
+            )
+    left = ((-1, -1, -1),
+            (-1, -1, +1),
+            (-1, +1, +1),
+            (-1, +1, -1),
+            )
+    right = ((+1, -1, -1),
+             (+1, -1, +1),
+             (+1, +1, +1),
+             (+1, +1, -1),
+             )
+    down = ((-1, -1, -1),
+            (+1, -1, -1),
+            (+1, +1, -1),
+            (-1, +1, -1),
+            )
+    top = ((-1, -1, +1),
+           (+1, -1, +1),
+           (+1, +1, +1),
+           (-1, +1, +1),
+           )
+
+    faces = ((top, color(255)),
+             (down, color(0)),
+             (right, color(150)),
+             (left, color(150)),
+             (back, color(75)),
+             (front, color(75)),
+             )
+
+    for pts, shade in faces:
+        hs = side / 2  # half side
+        #fill(shade)
+        beginShape()
+        for pt in pts:
+            x, y, z = pt
+            vertex(x * hs, y * hs, z * hs)
+        endShape(CLOSE)
