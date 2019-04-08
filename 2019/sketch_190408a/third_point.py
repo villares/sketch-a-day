b2832c05df742f38494f1c5b223755953db7f52f
villares <abav@lugaralgum.com>
2019-04-08 08:42:44 -0300

    third_point_refactor

--- 2019/sketch_190408a/third_point.py
+++ 2019/sketch_190408a/third_point.py
@@ -6,36 +6,35 @@ found at https://stackoverflow.com/questions/4001948/drawing-a-triangle-in-a-coo
 class NoTrianglePossible(BaseException):
     pass
 
-def third_point(point_a, point_b, ac_length, bc_length):
+def third_point(a, b, ac_len, bc_len):
     """
-    Returns two point_c options given: point_a, point_b, ac_length, bc_length    
+    Returns two point c options given:
+    point a, point b, ac length, bc length    
     """
     # To allow use of tuples, creates or recreates PVectors
-    point_a, point_b = PVector(*point_a), PVector(*point_b)
+    a, b = PVector(*a), PVector(*b)
 
     # check if a triangle is possible
-    ab_length = point_a.dist(point_b)
-    if ab_length > (ac_length + bc_length) or ab_length < abs(ac_length - bc_length):
+    ab_len = a.dist(b)
+    if ab_len > (ac_len + bc_len) or ab_len < abs(ac_len - bc_len):
         raise NoTrianglePossible("The sides do not form a triangle")
 
     # get the length to the vertex of the right triangle formed,
     # by the intersection formed by circles a and b
-    ad_length = (ab_length ** 2
-                 + ac_length ** 2
-                 - bc_length ** 2) / (2.0 * ab_length)
+    ad_len = (ab_len ** 2 + ac_len ** 2 - bc_len ** 2) / (2.0 * ab_len)
 
-    # get the height of the line at a right angle from a_length
-    h = sqrt(abs(ac_length ** 2 - ad_length ** 2))
+    # get the height of the line at a right angle from a_len
+    h = sqrt(abs(ac_len ** 2 - ad_len ** 2))
 
-    # Calculate the mid PVector (point_d), needed to calculate point_c(1|2)
-    d_x = point_a.x + ad_length * (point_b.x - point_a.x) / ab_length
-    d_y = point_a.y + ad_length * (point_b.y - point_a.y) / ab_length
-    point_d = PVector(d_x, d_y)
+    # Calculate the mid PVector (point d), needed to calculate point c(1|2)
+    d_x = a.x + ad_len * (b.x - a.x) / ab_len
+    d_y = a.y + ad_len * (b.y - a.y) / ab_len
+    d = PVector(d_x, d_y)
 
     # get point_c locations
-    c_x1 = point_d.x + h * (point_b.y - point_a.y) / ab_length
-    c_x2 = point_d.x - h * (point_b.y - point_a.y) / ab_length
-    c_y1 = point_d.y - h * (point_b.x - point_a.x) / ab_length
-    c_y2 = point_d.y + h * (point_b.x - point_a.x) / ab_length
+    c_x1 = d.x + h * (b.y - a.y) / ab_len
+    c_x2 = d.x - h * (b.y - a.y) / ab_len
+    c_y1 = d.y - h * (b.x - a.x) / ab_len
+    c_y2 = d.y + h * (b.x - a.x) / ab_len
 
     return PVector(c_x1, c_y1), PVector(c_x2, c_y2)
