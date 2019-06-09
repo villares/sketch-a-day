08e578d96bbde6a302eb79720caf1af0e9233561
villares <abav@lugaralgum.com>
2019-06-09 19:05:56 -0300

    190606b_fix

--- 2019/sketch_190606b/target/org.transcrypt.__runtime__.js
+++ 2019/sketch_190606b/target/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-06-06 23:32:30
+// Transcrypt'ed from Python, 2019-06-09 19:05:09
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
