4934fd234fd8ff6084612fc896b520e5ab430958
Alexandre B A Villares <abav@lugaralgum.com>
2020-02-19 21:30:22 -0300

    protect "negative sweep" arcs from distortion!
    The Bezier aprox. of an arc has a recursive strategy to avoid distortion, it subdivides arcs longer than HALF_PI radians (90 degrees).
    
    This was not working on negative sweeps (arc going in inverse direction)

--- 2020/sketch_2020_02_19a/arcs.py
+++ 2020/sketch_2020_02_19a/arcs.py
@@ -92,7 +92,7 @@ def b_arc(cx, cy, w, h, start_angle, end_angle, mode=0):
         beginShape()
     if mode != 1: # if not 'middle'
         vertex(px3, py3)
-    if theta < HALF_PI:
+    if abs(theta) < HALF_PI:
         bezierVertex(px2, py2, px1, py1, px0, py0)
     else:
         # to avoid distortion, break into 2 smaller arcs
