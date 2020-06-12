b323254a8e9950c7d1ec1678f04bfecda40af00e
villares <abav@lugaralgum.com>
2020-06-11 22:29:23 -0300

    11b

---
+++ 2020/sketch_2020_06_11b/gif_export.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+Alexandre B A Villares http://abav.lugaralgum.com - v2020_06_01 - Licensed under GPL v3
+Inspired by an example by Art Simon https://github.com/APCSPrinciples/AnimatedGIF/
+
+This is a helper for the Processing gifAnimation library https://github.com/extrapixel/gif-animation/tree/3.0
+Download the library from github.com/extrapixel/gif-animation/archive/3.0.zip 
+Unzip and copy the gifAnimation folder into your libraries folder, like shown below:
+user/sketchbook/libraries/gifAnimation (Linux) 
+user/Documents/Processing/libraries/gifAnimation (Mac/Windows) 
+
+# This file should be saved as a 'tab' named gif_export.py in your Processing Python Mode sketch
+# Restart the IDE and add these lines at the start of your sketch:
+add_library('gifAnimation')
+from gif_export import gif_export
+
+# then add this at the end of draw():
+gif_export(GifMaker, "filename")
+
+"""
+
+def gif_export(GifMaker,             # gets a reference to the library
+               filename="exported",  # .gif will be added
+               repeat=0,             # 0 makes it an "endless" animation
+               quality=200,          # quality range 0 - 255 test yourself,my guess is 0 best/high 255 worst/low
+               delay=220,            # this is quick
+               frames=0,             # 0 will stop only if 'e' key pressed
+               transparent=None,     # set a transparent color
+               finish=False):        # force stop
+    global gifExporter
+    try:
+        gifExporter
+    except NameError:
+        gifExporter = GifMaker(this, filename + ".gif")
+        gifExporter.setRepeat(repeat)
+        gifExporter.setQuality(quality)
+        gifExporter.setDelay(delay)
+        if transparent is not None:
+            gifExporter.setTransparent(transparent)
+        print("gif recording started")
+
+    gifExporter.addFrame()
+
+    if frames == 0:
+       if keyPressed and key=='e':
+           finish = True
+    elif frameCount >= frames:
+        finish = True
+                
+    if finish:
+        gifExporter.finish()
+        print("gif saved, exit")
+        exit()
---
+++ 2020/sketch_2020_06_11b/sketch_2020_06_11b.pyde
@@ -0,0 +1,50 @@
+add_library('gifAnimation')
+from gif_export import gif_export
+
+def setup():
+    size(500, 500)
+    noStroke()
+
+def draw():
+    f = radians(frameCount - 1)
+
+    background(240)
+    noStroke()
+    tc = poly_points(250, 250, 100, n=12)
+    for i, p in enumerate(tc):
+        t(p[0], p[1], 150,
+          1 * i * radians(60) + f,
+          d=81)
+
+    # if frameCount < 360:
+    #    if frameCount % 2 == 0:
+    #        gif_export(GifMaker, "sketch_2020_06_11b")
+    # else:
+    #     gif_export(GifMaker, finish=True)
+
+
+def t(x, y, s, rot=0, d=71):
+    pp = poly_points(x, y, s, 3, rot + PI)
+    e0 = div_points(pp[0], pp[1], d)
+    e1 = div_points(pp[1], pp[2], d)[::-1]
+    fill(0)
+    lines = zip(e0, e1)
+    for la, lb in zip(lines[:-1:2], lines[1::2]):
+        (x0, y0), (x1, y1) = la
+        (x3, y3), (x2, y2) = lb
+        quad(x0, y0, x1, y1,
+             x2, y2, x3, y3)
+
+def div_points(a, b, d=10):
+    return [(lerp(a[0], b[0], float(i) / d),
+             lerp(a[1], b[1], float(i) / d))
+            for i in range(d + 1)]
+
+def poly_points(x, y, r, n, rot=0):
+    a = TWO_PI / n
+    points = []
+    for i in range(n):
+        px = x + r * cos(a * i + rot)
+        py = y + r * sin(a * i + rot)
+        points.append((px, py))
+    return points
