fdc22e55468a8cba560a5acbb505294824aa1da7
Alexandre B A Villares <abav@lugaralgum.com>
2020-07-05 23:37:57 -0300

    Update generate_entries.py

--- Markdown_sketch_numbers/generate_entries.py
+++ Markdown_sketch_numbers/generate_entries.py
@@ -3,20 +3,26 @@
 # gets images, checks image types
 # build markdown for entries
 ## TO DO:
-# find last entry for me
-# insert new entries in propper place
+# [X] find last entry for me
+# [ ] insert new entries in propper place
+# [ ] insert docstrings as text on .md file
 
 from os import listdir
 from os.path import isfile, join
 
 from helpers import get_image_files, build_entry
 
-last_done = '2020_06_21' 
 YEAR = "2020"
 base_path = "/media/villares/VolumeD/GitHub/sketch-a-day"
 year_path = join(base_path,YEAR)
 folders = listdir(year_path)
 
+with open(join(base_path, 'README.md')) as readme:
+    imagens = (line[line.find(YEAR):line.find(']')]
+               for line in readme.readlines()
+               if '![' in line)
+    last_done = next(imagens)[:10]
+    
 # find folders after the last_done
 new_folders = []
 for f in reversed(folders):
@@ -25,9 +31,9 @@ for f in reversed(folders):
     else:
         new_folders.append(f)
 
-
 for f in new_folders:
     imgs = get_image_files(year_path, f)
     if imgs:
         print(build_entry(imgs[0], YEAR))
+
     
