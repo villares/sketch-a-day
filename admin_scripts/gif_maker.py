b9d1ecf59732a3d5d093ae61b6b07e2aa33bdfcb
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-07-18 15:27:25 -0300

    rename & add gif_maker

---
+++ admin_scripts/gif_maker.py
@@ -0,0 +1,61 @@
+
+from os import listdir
+from os.path import isfile, join
+
+from PIL import Image, GifImagePlugin
+
+from helpers import get_image_files, remove_transparency
+
+YEAR = "2019"
+base_path = "/media/villares/VolumeD/GitHub/sketch-a-day"
+year_path = join(base_path,YEAR)
+folders = listdir(year_path)
+
+
+images = []
+for folder in folders:
+    folder_path = join(year_path, folder)
+    f_images = get_image_files(year_path, folder)
+    if f_images:
+        img_path = join(folder_path, f_images[0])
+        images.append(Image.open(img_path))
+        
+# dst = Image.new('RGB', (400, 400))
+n = None # degub with 5
+thumbs = []
+for img in images[:n]:
+#     im.resize((400, 400), resample=Image.BICUBIC)
+    if img.format == 'GIF' and img.is_animated:
+        p = img.getpalette()
+        img.seek(img.n_frames // 2)
+        if not img.getpalette():
+                im.putpalette(p)
+        img = img.convert('RGBA')
+        print('gif')
+        
+    img.thumbnail((500, 250), Image.BICUBIC)
+    img = remove_transparency(img)
+    
+    bg = Image.new('RGB', (500, 250), (0, 0, 0))
+    print(img.size)
+    iw, bw = img.size[0], bg.size[0]
+    if  iw < bw:
+        x = (bw - iw) // 2
+    else:
+        x = 0
+    ih, bh = img.size[1], bg.size[1]
+    if  ih < bh:
+        y = (bh - ih) // 2
+    else:
+        y = 0     
+    bg.paste(img, (x, y))
+    thumbs.append(bg)
+
+thumbs[0].save('export{}.gif'.format(YEAR),
+               save_all=True, append_images=thumbs,
+               optimize=True,
+               duration=500,
+               loop=0)
+    
+
+        
--- admin_scripts/helpers.py
+++ admin_scripts/helpers.py
@@ -42,4 +42,22 @@ def build_entry(image, year):
 
 [{0}](https://github.com/villares/sketch-a-day/tree/master/{2}/{0}) [[Py.Processing](https://villares.github.io/como-instalar-o-processing-modo-python/index-EN)]
 """.format(name, ext, year)
+
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+    from PIL import Image
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').split()[-1]
+
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+
+    else:
+        return im
     
