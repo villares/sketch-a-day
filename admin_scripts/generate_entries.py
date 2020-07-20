53d4ad07d98e016559e82a41b2828dee60d6c43f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-07-20 20:55:48 -0300

    Update generate_entries.py

--- admin_scripts/generate_entries.py
+++ admin_scripts/generate_entries.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 # reads folder
 # finds missing folders from last_done
 # gets images, checks image types
@@ -18,33 +20,35 @@ year_path = join(base_path, YEAR)
 folders = listdir(year_path)
 readme_path = join(base_path, 'README.md')
 
-# open the readme markdown index
-with open(readme_path, 'rt') as readme:
-    lines = readme.readlines()   
-# find date of the first image    
-imagens = (line[line.find(YEAR):line.find(']')]
-               for line in lines
-               if '![' in line)
-last_done = next(imagens)[:10]
-# find folders after the last_done
-new_folders = []
-for f in reversed(folders):
-    if last_done not in f:
-        new_folders.append(f)        
-    else:
-        break
-# find insertion point
-for insert_point, line in enumerate(lines):
-    if last_done in line:
-        break
-# iterate on new folders
-for folder in reversed(new_folders):
-    imgs = get_image_names(year_path, folder)
-    if imgs:  # insert entry if matching image found
-        lines.insert(insert_point - 3,
-                     build_entry(imgs[0], YEAR))
-        print('adding: '+ folder)
-# overwrite the readme markdown index
-with open(readme_path, 'wt') as readme:
-    content = "".join(lines)
-    readme.write(content)    
+if __name__ == "__main__":
+    print("generated entries!")
+    # open the readme markdown index
+    with open(readme_path, 'rt') as readme:
+        lines = readme.readlines()   
+    # find date of the first image    
+    imagens = (line[line.find(YEAR):line.find(']')]
+                   for line in lines
+                   if '![' in line)
+    last_done = next(imagens)[:10]
+    # find folders after the last_done
+    new_folders = []
+    for f in reversed(folders):
+        if last_done not in f:
+            new_folders.append(f)        
+        else:
+            break
+    # find insertion point
+    for insert_point, line in enumerate(lines):
+        if last_done in line:
+            break
+    # iterate on new folders
+    for folder in reversed(new_folders):
+        imgs = get_image_names(year_path, folder)
+        if imgs:  # insert entry if matching image found
+            lines.insert(insert_point - 3,
+                         build_entry(imgs[0], YEAR))
+            print('adding: '+ folder)
+    # overwrite the readme markdown index
+    with open(readme_path, 'wt') as readme:
+        content = "".join(lines)
+        readme.write(content)    
