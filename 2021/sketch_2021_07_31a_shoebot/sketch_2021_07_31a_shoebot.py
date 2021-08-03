01947c2220df7a8418824befd40fb3483b018cc7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-08-02 23:20:27 -0300

    Update sketch_2021_07_31a_shoebot.py

--- 2021/sketch_2021_07_31a_shoebot/sketch_2021_07_31a_shoebot.py
+++ 2021/sketch_2021_07_31a_shoebot/sketch_2021_07_31a_shoebot.py
@@ -1,20 +1,23 @@
 #つぶやきshoebotpython
 from random import choice
-from math import pi
+#from math import pi
 w=500
 
 def setup():
  size(w,w)
  speed(5)
  stroke(0)
+ print('a')
     
 def draw():
  transform(CORNER)
  translate(w/2,w/2)
  for i in range(100):
-  rotate(radians=pi/2*choice((-1,1))) 
+  #rotate(radians=pi/2*choice((-1,1)))
+  rotate(90*choice((-1,1))) # degrees are default in shoebot
   line(0,0,i,0)
   translate(i,0)
+  
 
 
 
