bc82f2fdee516e8db34a82cbbabcf2364a7874f7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-08-14 18:33:39 -0300

    Update fix_image_name.py

--- admin_scripts/fix_image_name.py
+++ admin_scripts/fix_image_name.py
@@ -12,17 +12,20 @@ from helpers import is_img_ext
 cwd = os.getcwd()
 nome_pasta = os.path.split(cwd)[-1]
 nomes_arquivos = sorted(os.listdir(cwd))
-
+# Procura imagens no diretório corrente
 nomes_imagens = [file_name for file_name in nomes_arquivos
                 if is_img_ext(file_name)]
+# Imagens boas são as que tem o mesmo nome da pasta!
 imagens_boas = [nome_imagem for nome_imagem in nomes_imagens
                 if nome_pasta in nome_imagem]
 if imagens_boas:
     print('Não precisei fazer nada!')
 elif nomes_imagens:
-    nome_imagem = nomes_imagens[0]
+    # vai renomear a primeira para ficar com o nome da pasta
+    nome_imagem = nomes_imagens[0]  
     ext = nome_imagem.split('.')[-1]
     novo_nome = nome_pasta + '.' + ext
     os.rename(nome_imagem, novo_nome)
+    print(nome_imagem + ' -> ' + novo_nome)
 else:
     print('Não encontrei imagens!')
