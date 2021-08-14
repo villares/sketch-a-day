a3c8297a014fb4510ff2bb93ab92c2f7b5959f79
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-08-14 10:23:52 -0300

    clean up imported mode (we may have visitors)

--- 2021/sketch_2021_07_26pymunk_on_py5/sketch_2021_07_26pymunk_on_py5_imported_mode.py
+++ 2021/sketch_2021_07_26pymunk_on_py5/sketch_2021_07_26pymunk_on_py5_imported_mode.py
@@ -8,33 +8,27 @@ import random
 import sys
 import pymunk as pm
 
-
 random.seed(1)
 ticks_to_next_ball = 10
 balls = []
 
-def add_ball(space):
-    """Add a ball to the given space at a random position"""
-    mass = 1
-    radius =random.randint(10, 20)
-    inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
-    body = pm.Body(mass, inertia)
-    x = random.randint(120, 420)
-    body.position = x, -50
-    shape_ = pm.Circle(body, radius, (0, 0))
-    shape_.friction = 1
-    space.add(body, shape_)
-    return shape_
 
-
+def setup():
+    size(600, 600)
+    global space, wires
+    print("Joints. Just wait and the L will tip over")
+    space = pm.Space()
+    space.gravity = (0.0, 700.0)
+    wires = add_L(space)
+    color_mode(HSB)
+   
+   
 def add_L(space):
-    """Add a inverted L shape_ with two joints"""
+    """Add a inverted L shape with two joints"""
     rotation_center_body = pm.Body(body_type=pm.Body.STATIC)
     rotation_center_body.position = (300, 300)
-
     rotation_limit_body = pm.Body(body_type=pm.Body.STATIC)
     rotation_limit_body.position = (200, 300)
-
     body = pm.Body(10, 10000)
     body.position = (300, 300)
     l1 = pm.Segment(body, (-145, 0), (255.0, 0.0), 5)
@@ -46,27 +40,29 @@ def add_L(space):
     rotation_limit_joint = pm.SlideJoint(
         body, rotation_limit_body, (-100, 0), (0, 0), 0, joint_limit
     )
-
     space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
     return l1, l2
-    
-
-def setup():
-    size(600, 600)
-    global space, wires
-    print("Joints. Just wait and the L will tip over")
-    space = pm.Space()
-    space.gravity = (0.0, 700.0)
-    wires = add_L(space)
-    color_mode(HSB)
-    
-    
+  
+  
+def add_ball(space):
+    """Add a ball to the given space at a random position"""
+    mass = 1
+    radius =random.randint(10, 20)
+    inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+    body = pm.Body(mass, inertia)
+    x = random.randint(120, 420)
+    body.position = x, -50
+    shp = pm.Circle(body, radius, (0, 0))
+    shp.friction = 1
+    space.add(body, shp)
+  
+  
 def draw():
     global ticks_to_next_ball
     ticks_to_next_ball -= 1
     if ticks_to_next_ball <= 0:
         ticks_to_next_ball = 25
-        ball_shape = add_ball(space)
+        add_ball(space)
 
     for obj in reversed(space.shapes):
         if is_ball(obj) and obj.body.position.y > 600:
@@ -99,9 +95,6 @@ def draw():
     space.step(1 / get_frame_rate())
 
 
-def is_ball(obj):
-    return isinstance(obj, pm.shapes.Circle)
+def is_ball(obj): return isinstance(obj, pm.shapes.Circle)
      
-def is_segment(obj):
-    return isinstance(obj, pm.shapes.Segment)
-
+def is_segment(obj): return isinstance(obj, pm.shapes.Segment)
