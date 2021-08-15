101296f9ce960f742176a4b42f9fce73e2b97b26
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-08-15 17:03:06 -0300

    Update helpers.py

--- admin_scripts/helpers.py
+++ admin_scripts/helpers.py
@@ -6,36 +6,39 @@ def get_image_names(base, folder, word=None):
     """
     Return a list of image names from a directory
     named folder at base/folder by default only
-    if name contains the folder name.        
+    if name contains the folder name.
     """
     word = word or folder
     contents = listdir(join(base, folder))
-    image_files = [f for f in contents
-                   if is_img_ext(f)
-                   and word in f]
+    image_files = [f for f in contents if is_img_ext(f) and word in f]
     return image_files
 
 
 def is_img_ext(file_name):
     """
     Return True if file_name ends with
-    one of the valid_ext extensions.
+    one of the valid_extensions.
     """
     ext = file_name.split('.')[-1]
-    valid_ext = ('jpg',
-                 'png',
-                 'jpeg',
-                 'gif',
-                 'tif',
-                 'tga',
-                 #'svg',
-                 )
-    return ext.lower() in valid_ext
+    valid_extensions = (
+        'jpg',
+        'png',
+        'jpeg',
+        'gif',
+        'tif',
+        'tga',
+        #'svg',
+    )
+    return ext.lower() in valid_extensions
+
 
 def remove_transparency(im, bg_colour=(255, 255, 255)):
     from PIL import Image
+
     # Only process if image has transparency (http://stackoverflow.com/a/1963146)
-    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+    if im.mode in ('RGBA', 'LA') or (
+        im.mode == 'P' and 'transparency' in im.info
+    ):
 
         # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
         alpha = im.convert('RGBA').split()[-1]
@@ -43,7 +46,7 @@ def remove_transparency(im, bg_colour=(255, 255, 255)):
         # Create a new background image of our matt color.
         # Must be RGBA because paste requires both images have the same format
         # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
-        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg = Image.new('RGBA', im.size, bg_colour + (255,))
         bg.paste(im, mask=alpha)
         return bg
 
