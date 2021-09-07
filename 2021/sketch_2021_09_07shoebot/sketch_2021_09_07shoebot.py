34a810b34cc0b56f67a5a52f97f6cd7b3ca78f73
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-09-07 19:02:51 -0300

    Create sketch_2021_09_07shoebot.py

---
+++ 2021/sketch_2021_09_07shoebot/sketch_2021_09_07shoebot.py
@@ -0,0 +1,34 @@
+from random import randint
+
+stands = []
+letras = 'ABCDEFGHIJKLMNOPQRST'
+
+def setup():
+    size(800, 800)
+    colorrange(255)
+    background(0, 0, 100)
+    stroke(0)
+    strokewidth(0.5)
+    print(len(letras))
+    margin = 50
+    translate(margin / 2, margin / 2)
+    for i, l in enumerate(letras):
+        h1 = randint(15, 30)
+        n = x = 0
+        while x < WIDTH - margin:
+            n += 1
+            h2 = randint(0, 5)
+            h = h1 + h2
+            w = randint(5, 15)
+            if x + w > WIDTH - margin:
+                w = (WIDTH - margin) - x
+            a = w * h
+            if i % 2 == 0:
+                y = 35 + i * 38 - h
+            else:
+                y = i * 38 + 2
+            fill(h * 10 - 100, a % 256, w * 16)
+            #print((h * 10 - 100, a, w * 16))
+            rect(x, y, w, h)
+            x += w
+    
