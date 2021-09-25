aca00b790369018365db9f73d5b1966b8faa602a
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-09-25 13:29:34 -0300

    Create sketch_2021_09_25a_py5.py

---
+++ 2021/sketch_2021_09_25a_py5/sketch_2021_09_25a_py5.py
@@ -0,0 +1,39 @@
+import py5
+from math import sin, cos, pi, dist
+from itertools import product
+from random import randint, random
+
+hor = lambda x, y, w, h: randint(0, w) < dist((x, y), (w / 2, h / 2))
+# ver = lambda x, y, w, h: randint(0, h * 2) < h - h * cos(y / h * 2 * pi)    
+ver = lambda x, y, w, h: randint(0, w) > dist((x, y), (w / 2, h / 2))
+
+def setup():
+    py5.size(960, 960)
+    py5.background(32)
+    py5.scale(0.25)
+    grid(0, 0, py5.width * 4)
+    py5.saveFrame('sketch_2021_09_25a.png')
+   
+def grid(xo, yo, w):
+    qw = w // 4
+    for i in range(4):
+        x = xo + i * qw
+        for j in range(4):
+            y = yo + j * qw
+            r = randint(1, 3)
+            if qw > 64 and r == 1:
+                grid(x, y, qw)
+            elif r == 2:
+                py5.stroke(255)
+                region(x, y, qw, qw, hor)
+            else:
+                py5.stroke(255)
+                region(x, y, qw, qw, ver)
+
+def region(xo, yo, w, h, rule, overlap=0.25):
+    o = int(w * overlap)
+    for x, y in product(range(xo - o, xo + w + o), range(yo - o, yo + h )):
+        if rule(x - xo, y - yo, w, h):
+            py5.point(x, y)
+            
+py5.run_sketch()
