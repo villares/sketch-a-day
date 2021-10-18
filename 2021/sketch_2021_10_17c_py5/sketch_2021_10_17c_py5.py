6f9ac6c496e6f9f6527281f288497caa2b696e7e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-10-18 00:54:21 -0300

    Update sketch_2021_10_17c_py5.py

--- 2021/sketch_2021_10_17c_py5/sketch_2021_10_17c_py5.py
+++ 2021/sketch_2021_10_17c_py5/sketch_2021_10_17c_py5.py
@@ -1,6 +1,7 @@
 import py5
 import numpy as np
 from math import atan2, pi, sin, cos
+from itertools import chain
 
 tris = []
 unvisited_tris = set()
@@ -8,19 +9,21 @@ unvisited_tris = set()
 
 def setup():
     py5.size(600, 600)
-    inicial = ((0, -10), (0, 10), (15, 0))
-
-
 
 
 def draw():
     py5.background(240, 240, 220)
     py5.translate(py5.width / 2, py5.height / 2)
     py5.stroke(0, 64)
-    py5.fill(150, 50, 0, 100)
+    py5.fill(50, 150, 0, 100)
     for t in tris:
         py5.begin_shape()
         for x, y in t:
+            py5.vertex(x, y)
+        py5.end_shape(py5.CLOSE)
+    for u in unvisited_tris:
+        py5.begin_shape()
+        for x, y in u:
             py5.vertex(x, y)
         py5.end_shape(py5.CLOSE)
 
@@ -30,16 +33,17 @@ def mouse_dragged():
     drag = ((py5.mouse_x - ox, py5.mouse_y - oy),
             (py5.pmouse_x - ox, py5.pmouse_y - oy))
     unvisited_tris.add(drag)
-    tris.append(drag)
 
 
 def key_pressed():
-    grow_all()
-    
-def grow_all():    
+    global unvisited_tris 
     for t in list(unvisited_tris):
          grow(t)
-    print(len(tris))  # , len(visited_tris)
+#     new_tris = map(grow, list(unvisited_tris))
+#     print(list(new_tris))
+#     unvisited_tris = set(list(new_tris))
+#     tris.extend(new_tris)
+#     print(len(tris))  # , len(visited_tris)
 
 
 
@@ -61,6 +65,20 @@ def grow(t):
             tris.append(n)
             unvisited_tris.add(n)
 
+# def grow(t):
+#     edges = get_edges(t)
+#     new_tris = []
+#     for e in edges:
+#         e0, e1 = e[0], e[1]
+#         a = edge_angle(e)
+#         m = midpoint(e)
+#         d = edge_dist(e) * 0.87
+#         p = point_offset(m, d, a - pi / 2)
+#         if not point_in_tris(p):
+#             n = (e0, p, e1)
+#             new_tris.append(n)
+#     return new_tris
+
 def get_edges(t):
     t0 = t[0]
     return [
@@ -78,19 +96,31 @@ def point_offset(p, offset, angle):
 
 
 def point_in_tris(p):
-    x, y = p
-    p_in_t = lambda t: point_inside_poly(x, y, t)
+#    p_in_t = lambda t: point_inside_tri(p, t)
+    p_in_t = lambda t: point_inside_poly(p, t)
     return any(map(p_in_t, tris))
 
+def s(p1, p2, p3):
+    return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
 
-def point_inside_poly(x, y, points):
+def point_inside_tri(pt, tri):
+    v1, v2, v3 = tri
+    d1 = s(pt, v1, v2);
+    d2 = s(pt, v2, v3);
+    d3 = s(pt, v3, v1);
+    neg = (d1 < 0) or (d2 < 0) or (d3 < 0)
+    pos = (d1 > 0) or (d2 > 0) or (d3 > 0)
+    return not (pos and neg)
+
+def point_inside_poly(p, vertices):
     # ray-casting algorithm based on
     # https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
+    x, y = p
     inside = False
-    for i, p in enumerate(points):
-        pp = points[i - 1]
-        xi, yi = p
-        xj, yj = pp
+    for i, v in enumerate(vertices):
+        pv = vertices[i - 1]
+        xi, yi = v
+        xj, yj = pv
         intersect = ((yi > y) != (yj > y)) and (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
         if intersect:
             inside = not inside
