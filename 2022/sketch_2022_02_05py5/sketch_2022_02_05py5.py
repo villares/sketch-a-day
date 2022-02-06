75ba9a17a70ee44b2de423f593d66cebabd349a8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-02-05 21:47:02 -0300

    Update sketch_2022_02_05py5.py

--- 2022/sketch_2022_02_05py5/sketch_2022_02_05py5.py
+++ 2022/sketch_2022_02_05py5/sketch_2022_02_05py5.py
@@ -4,22 +4,26 @@ from random import randint, sample, choice
 
 space = pymunk.Space() # create a new space for your simulation
 space.gravity = (0, 900)
+BOX_NUMBER = 30
 
 def setup():
-    py5.size(500, 500)
+    global initial_shape_count
+    py5.size(500, 600)
     py5.color_mode(py5.HSB)
     pymunk.Poly.draw = draw_box  # this is called "monkeypatching"
     pymunk.Circle.draw = draw_ball
     pymunk.Segment.draw = draw_seg
+    mw, mh = py5.width, py5.height
     segments = [
-        ((0, 0), (0, 500)), 
-        ((500, 0), (500, 500)),
-        ((0, 500), (500, 500)),
-    ] + [((x, 500), (x, 350)) for x in range(25, 500, 25)]
+        ((0, 0), (0, mh)), 
+        ((mw, 0), (mw, mh)),
+        ((0, mh), (mw, mh)),
+    ] + [((x, mh), (x, mh * 0.75)) for x in range(25, mw, 25)]
     for point_a, point_b in segments:
         s = pymunk.Segment(space.static_body, point_a, point_b, 1)
         s.elasticity = 1
         space.add(s)
+    initial_shape_count = len(space.shapes)
     populate_boxes()
 
 def draw():
@@ -38,9 +42,9 @@ def draw():
     print(len(space.shapes))
 
 def populate_boxes():
-    while len(space.shapes) < 30:
-        x, y = randint(0, py5.width), randint(50, py5.height - 200)
-        if all(map(lambda obj:  obj.point_query((x, y)).distance > 0,
+    while len(space.shapes) < initial_shape_count + BOX_NUMBER:
+        x, y = randint(0, py5.width), randint(100, py5.height * 0.70)
+        if all(map(lambda obj:  obj.point_query((x, y)).distance > 30,
                    space.shapes)):
             create_box(x, y)
    
@@ -50,8 +54,9 @@ def add_balls():
 def create_box(x, y):
     body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
     body.position = x, y
-    w, h = choice((100, 90, 80, 110)), choice((10, 5, 15)) 
+    w, h = choice((10, 20, 30, 40)), choice((10, 20, 30, 40)) 
     space.add(body, pymunk.Poly.create_box(body, (w, h)))
+    body.angle += py5.radians(randint(-5, 5) * 10)
  
 def draw_box(obj):
     py5.push_matrix()
@@ -71,6 +76,7 @@ def create_ball(x, y):
     ball = pymunk.Circle(body, radius=5)
     ball.elasticity = 0.2
     space.add(body, ball)
+   
 
 def draw_ball(obj):
     py5.fill(0) #(obj.radius * 10, 255, 255)
@@ -94,7 +100,7 @@ def mouse_dragged():
 
 def mouse_wheel(e):
     for obj in space.shapes:
-        if is_box(obj):    
+        if is_box(obj):
              info = obj.point_query((py5.mouse_x, py5.mouse_y))
              if info.distance < 2:
                  obj.body.angle += py5.radians(e.getCount())
