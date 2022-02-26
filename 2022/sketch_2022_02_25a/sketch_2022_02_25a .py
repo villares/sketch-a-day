2936d5223c4d678f31e782c749c026309af99ef9
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-02-25 21:43:17 -0300

    Update sketch_2022_02_25a .py

--- 2022/sketch_2022_02_25a/sketch_2022_02_25a .py
+++ 2022/sketch_2022_02_25a/sketch_2022_02_25a .py
@@ -1,71 +1,52 @@
-from itertools import product, permutations
+from itertools import product
 from random import shuffle
 
-patterns = [  # TL, BL, TR, BR
-#(1, 2, 3, 4),
-(1, 1, 1, 1),
-(1, 1, 0, 0),
-(0, 0, 1, 1),
-(1, 0, 0, 0),
-(0, 1, 0, 0),
-(0, 0, 1, 0),
-(0, 0, 0, 1),
-(0, 0, 0, 0), 
+patterns = [  # TL, BL, TR, BR (1, 2, 3, 4),
+(1, 1, 1, 1), (1, 1, 0, 0), (0, 0, 1, 1), (1, 0, 0, 0),
+(0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1), (0, 0, 0, 0), 
 ]
 
-SPACING = 1
-MARGIN = 50
-M_SIZE = 15
+SPACING, MARGIN = 0.90, 50
+M_SIZE = 22
 TL, TR, BL, BR = (-1, -1), (1, -1), (1, -1), (1, 1)
-
-NBS = (
-(-1, -1), (-1, 0), (-1, 1),
-( 0, -1),          ( 0, 1),
-( 1, -1), ( 1, 0), ( 1, 1)
-)
+modules = {}
 
 def setup():
-    size(400, 400)
+    size(500, 500)
     rect_mode(CENTER)
     text_align(CENTER, CENTER)
     positions = product(range(10), repeat=2) 
-    
-    modules = []
-    for i, j in positions:
-        #if patterns:
-            #m = Module(i, j, [patterns.pop()])
+    for pos in positions:
             ptts = patterns[:]; shuffle(ptts)
-            m = Module(i, j, ptts)
+            m = Module(pos, ptts)
             m.plot()
-            modules.append(m)
+            modules[pos] = m
 
 class Module:
     
-    def __init__(self, i, j, vals):
-        self.i = i
-        self.j = j
+    def __init__(self, p, vals):
+        self.p = p
         self.vals = vals
         
     def plot(self):
         stroke(128)
         ms = M_SIZE
-        positions = product((ms / 2, -ms / 2), repeat=2)
-        x = ms + self.i * ms * 2 * SPACING + MARGIN
-        y = ms + self.j * ms * 2 * SPACING + MARGIN
+        corners = product((ms / 2, -ms / 2), repeat=2)
+        x = ms + self.p[0] * ms * 2 * SPACING + MARGIN
+        y = ms + self.p[1] * ms * 2 * SPACING + MARGIN
         vals = list(self.vals[0])
-        for xi, yj in positions:
+        for i, j in corners:
             v = vals.pop()
-            if v == 0:
-                fill(0)
-            elif v == 1:
-                fill(255)
-            else:
-                fill(v * 32)
-            square(x + xi, y + yj, ms)
+            fill(v * 255, 150)
+            square(x + i, y + j, ms)
             fill(255, 0, 0)
-            #text(v, x + xi, y + yj)
+            #text(v, x + i, y + j)
         
-        
+NBS = (
+(-1, -1), (-1, 0), (-1, 1),
+( 0, -1),          ( 0, 1),
+( 1, -1), ( 1, 0), ( 1, 1)
+)       
         
         
         
