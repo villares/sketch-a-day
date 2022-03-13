5a2d5ffe1362da007e9c2cd6a2c2b258789e523f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-03-12 23:11:12 -0300

    Update sketch_2022_03_12a.py

--- 2022/sketch_2022_03_12a/sketch_2022_03_12a.py
+++ 2022/sketch_2022_03_12a/sketch_2022_03_12a.py
@@ -1,5 +1,8 @@
 from itertools import product
 from random import sample
+
+import py5
+
 from villares.helpers import lerp_tuple
 
 MARGIN = 64
@@ -9,51 +12,52 @@ pts = []
 num_pts = 5
 
 def setup():
-    size(512, 512, P3D)
+    global w, h
+    py5.size(512, 512, py5.P3D)
     set_pts()
-    no_fill()
-
+    py5.no_fill()
+    w, h = py5.width, py5.height
+    
 def set_pts():
-    grid = list(product(range(MARGIN, width, SPACING), repeat=2))
+    grid = list(product(range(MARGIN, py5.width, SPACING), repeat=2))
     pts[:] = []
     pts.append(sample(grid, num_pts))
     pts.append(sample(grid, num_pts))
     
 def draw():
-    background(150)
-    stroke(0)
-    stroke_weight(1)
-    translate(width / 2, height / 2, -height / 2)
-    rotate_y(radians(frame_count / 10.0))
-    translate(-width / 2, -height / 2) 
+    py5.background(150)
+    py5.stroke(0)
+    py5.stroke_weight(1)
+    py5.translate(w / 2, h / 2, -h / 2)
+    py5.rotate_y(py5.radians(py5.frame_count / 10.0))
+    py5.translate(-w / 2, -h / 2) 
     for i in range(31):
         t = i / 30.0
-        l_pts = lerp_along_list(pts, t, loop_back=True)
-        begin_shape()
-        for x, y in l_pts:
-            vertex(x, y, lerp(-200, 200, t))
-        end_shape(CLOSE)
-    stroke(200, 0, 0)
-    stroke_weight(3)
-    t = millis() % 4000 / 3999 # remap(mouse_y, 0, height, 0, 1)
-    l_pts = lerp_along_list(pts, t, loop_back=True)
-    begin_shape()
-    for x, y in l_pts:
-        vertex(x, y, lerp(-100, 100, t))
-    end_shape(CLOSE)
+        draw_poly(lerp_along_list(pts, t, loop_back=True), t)
+    py5.stroke(200, 0, 0)
+    py5.stroke_weight(3)
+    t = py5.millis() % 4000 / 3999 # remap(mouse_y, 0, height, 0, 1)
+    draw_poly(lerp_along_list(pts, t, loop_back=True), t)
         
-        
+def draw_poly(pts, t):
+    py5.begin_shape()
+    for x, y in pts:
+        py5.vertex(x, y, py5.lerp(-200, 200, t))
+    py5.end_shape(py5.CLOSE)
+
 def lerp_along_list(lst, amt, loop_back=False):
     # Based on LerpVectorsExample by Jeremy Douglass
-    amt = constrain(amt, 0, 1)  # let's play safe
+    amt = py5.constrain(amt, 0, 1)  # let's play safe
     if loop_back:
         lst = list(lst) + [lst[0]]
     if len(lst) == 1:
         return lst[0]
     cunit = 1.0 / (len(lst) - 1)
-    return lerp_tuple(lst[floor(amt / cunit)],
-                      lst[ceil(amt / cunit)],
+    return lerp_tuple(lst[py5.floor(amt / cunit)],
+                      lst[py5.ceil(amt / cunit)],
                       amt % cunit / cunit)
 def key_pressed():
-    if key == ' ':
+    if py5.key == ' ':
         set_pts()
+        
+py5.run_sketch()
