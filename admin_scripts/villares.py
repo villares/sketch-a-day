56d2f83ca0d4288606d8c51152189cace74f7f2e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-04-17 18:04:16 -0300

    please!

---
+++ admin_scripts/villares.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import os
+import sys
+
+def please():
+    pass
+                
+parse()
+for arg in sys.argv[1:]:
+    if arg.startswith('p'):
+        please()
+    else:
+        print("""usage:
+        p        : run please()
+        """)
+        exit()
+
+
