519a02a8e670a8dc7982801d31c53e708e7fc134
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-04-20 23:27:27 -0300

    Update sketch_2022_04_20.py

--- 2022/sketch_2022_04_20/sketch_2022_04_20.py
+++ 2022/sketch_2022_04_20/sketch_2022_04_20.py
@@ -1,40 +1,38 @@
 """
-Based on follow 3 code from Keith Peters in the Processing examples
+Based on 'follow 3' code from Keith Peters in the Processing examples
 
 A segmented line follows the mouse. the relative angle from
 each segment to the next is calculated with atan2() and the
 position of the next is calculated with sin() and cos().
 """
 
-x = [300.0] * 20
-y = [180.0] * 20
 joints = [(250, 250)] * 20
 seg_length = 20
-
 
 def setup():
     size(500, 500)
     stroke_weight(9)
     color_mode(HSB)
 
-
 def draw():
     background(240)
     fill(0, 150)
     drag_segment(0, mouse_x, mouse_y)
-    for i in range(len(x) - 1):
+    #for i, (x, y) in enumerate(joints[:-1]): # doesn't work
+    # because I'm mutating joints as I go, and enumerate will
+    # not see the changes!
+    for i in range(len(joints) - 1):
         stroke(i * 10, 200, 100, 150)
-        drag_segment(i + 1, x[i], y[i])
-
+        x, y = joints[i]
+        drag_segment(i + 1, x, y)
 
 def drag_segment(i, xin, yin):
-    dx = xin - x[i]
-    dy = yin - y[i]
+    x, y = joints[i]
+    dx = xin - x
+    dy = yin - y
     angle = atan2(dy, dx)
-    x[i] = xin - cos(angle) * seg_length
-    y[i] = yin - sin(angle) * seg_length
-    draw_segment(x[i], y[i], angle)
-
+    joints[i] = xin - cos(angle) * seg_length, yin - sin(angle) * seg_length
+    x, y = joints[i]    
 
 def draw_segment(x, y, a):
     with push_matrix():
