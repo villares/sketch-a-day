dc747d7b81da57418e081eaef9f5008f1e8bd19a
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-05-01 00:04:19 -0300

    Update sketch_2022_04_30.py

--- 2022/sketch_2022_04_30/sketch_2022_04_30.py
+++ 2022/sketch_2022_04_30/sketch_2022_04_30.py
@@ -1,15 +1,13 @@
 S=600
-def n(f,x,y):
- return 18*noise(x*.01,y*.01,f*.01)
 def setup():
  size(S,S)
- color_mode(2)
+ color_mode(3)
 def draw():
- clear()
+ background(0)
  x,f=0,frame_count
  while x<S:
   y=0
   while y<S+8:
-   h=n(f,x,y+f)
+   h=18*noise(x*.01,(y+f)*.01,f*.01)
    fill(h*16,255,255);ellipse(x,y,min(8,h),min(8,h));y+=h
   x+=8 #つぶやきProcessing #py5
