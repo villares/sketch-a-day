a8a66267d8b0f5a91a2cfe1a04f26038aaaa7161
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-05-27 21:40:31 -0300

    27

---
+++ 2022/sketch_2022_05_27/sketch_2022_05_27.py
@@ -0,0 +1,116 @@
+import py5
+
+
+def setup():
+    py5.size(700, 700)
+    py5.no_stroke()
+    py5.color_mode(py5.HSB)
+    start_polygons()
+    
+
+def start_polygons():
+    global poligonos, cores
+    poligonos = [
+        ((-300, -300), (300, -300), (300, 300), (-300, 300)),
+    ]
+    
+    
+
+def draw():
+    py5.background(0)
+    py5.translate(py5.width / 2, py5.height / 2)
+    h = 0
+    for s in poligonos:
+        py5.begin_shape()
+        xc, yc = centroide(s)
+        w, h = py5.width, py5.height
+        c = py5.color(
+#                   64 + py5.dist(xc, yc,
+#                                     py5.mouse_x - w / 2,
+#                                     py5.mouse_y - h / 2) % 128,
+                  64 + py5.dist(xc, yc, -w / 4, -h / 4) % 128,
+                   255,
+                  64 + py5.dist(xc, yc, w / 4, h / 4) % 128
+                  )
+        py5.fill(c)
+        py5.stroke(c)
+        f = 0  # map(mouseX, 0, width, 0, 1)
+        for x, y in s:
+            py5.vertex(x + xc * f, y + yc * f)
+        py5.end_shape(py5.CLOSE)
+
+
+def dividir_quad(q):
+    d1 = py5.dist(*q[0], *q[2])
+    d2 = py5.dist(*q[1], *q[3])
+    if d1 < d2:
+        return q[:3], q[2:] + q[:1]
+    else:
+        return q[1:4], q[3:] + q[:2]
+
+def dividir_tri(t):
+    a, c, b = t
+    ab = centroide((a, b))
+    bc = centroide((b, c))
+    ca = centroide((c, a))
+    return (
+        (ab, a, ca),
+        (ca, ab, bc, c),
+        (ab, b, bc),
+    )
+
+
+def centroide(s):
+    xs, ys = zip(*s)
+    return (sum(xs) / len(xs),
+            sum(ys) / len(ys))
+
+
+def key_pressed():
+    if py5.key == ' ':
+        dividir_poligonos()
+    elif py5.key == py5.ENTER:
+        start_polygons()
+    elif py5.key == 's':
+        py5.save_frame('###.png')
+
+
+def dividir_poligonos():
+    novos_poligonos = []
+    for p in poligonos:
+        novos_poligonos.extend(dividir(p))
+    poligonos[:] = novos_poligonos
+
+
+def dividir(p):
+    if len(p) == 4:
+        return dividir_quad(p)
+    else:
+        return dividir_tri(p)
+
+
+# def dividir_tri(t):
+#     a, c, b = t
+#     ab = centroide((a, b))
+#     bc = centroide((b, c))
+#     ca = centroide((c, a))
+#     return (
+#         (ab, a, ca),
+#         (ab, b, bc),
+#         (ca, ab, bc, c),
+#     )
+
+# 
+# def dividir_tri(t):
+#     c, a, b = t
+#     ab = centroide((a, b))
+#     bc = centroide((b, c))
+#     ca = centroide((c, a))
+#     return (
+#         (a, ab, ca),
+#         (ab, b, bc),
+#         (ab, bc, c, ca),
+#     )
+
+
+py5.run_sketch()
--- README.md
+++ README.md
@@ -27,6 +27,14 @@ Here are listed some of the tools I have been using:
 
 ---
 
+### sketch_2022_05_27
+
+![sketch_2022_05_27](2022/sketch_2022_05_27/sketch_2022_05_27.png)
+
+[sketch_2022_05_27](https://github.com/villares/sketch-a-day/tree/master/2022/sketch_2022_05_27) [[py5](https://py5.ixora.io/)]
+
+---
+
 ### sketch_2022_05_26
 
 ![sketch_2022_05_26](2022/sketch_2022_05_26/sketch_2022_05_26.png)
