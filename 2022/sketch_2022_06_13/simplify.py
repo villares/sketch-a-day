ef5959e3af18e55539bce124b9d970f4b6da0da7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-06-13 18:46:32 -0300

    Update simplify.py

--- 2022/sketch_2022_06_13/simplify.py
+++ 2022/sketch_2022_06_13/simplify.py
@@ -14,3 +14,10 @@ def simplify_pts3(pts_list, min_dist):
         return [previous_point := (x, y) for x, y in pts_list
                 if dist((x, y), previous_point) >= min_dist]
         
+def simplified_points(pts_list, min_dist):
+        reference_point = pts_list[0]
+        yield reference_point
+        for x, y in pts_list[1:]:
+            if dist((x, y), reference_point) >= min_dist: 
+                reference_point = x, y
+                yield x, y
