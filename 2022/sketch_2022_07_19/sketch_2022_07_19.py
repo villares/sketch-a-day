e96d8319ee3264e9779bba4a6c11aed88cf34eb8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-07-25 13:02:28 -0300

    Update sketch_2022_07_19.py

--- 2022/sketch_2022_07_19/sketch_2022_07_19.py
+++ 2022/sketch_2022_07_19/sketch_2022_07_19.py
@@ -1,19 +1,24 @@
+
+
 from itertools import chain
+
 
 def setup():
     size(600, 600)
     first_triangle()
-    
+
+
 def first_triangle():
     global tris
     R, cx, cy = 300, width / 2, height * 0.60
     first_triangle = []
     for i in range(3):
         a = i * TWO_PI / 3 - TWO_PI / 4
-        x = cx + R  * cos(a)
-        y = cy + R  * sin(a)
+        x = cx + R * cos(a)
+        y = cy + R * sin(a)
         first_triangle.append((x, y))
     tris = [tuple(first_triangle)]
+
 
 def draw():
     background(0)
@@ -21,16 +26,19 @@ def draw():
     for va, vb, vc in tris:
         fill(128 + sum(va) % 128, 128 + sum(vb) % 128, 128 + sum(vc) % 128)
         triangle(*va, *vb, *vc)
-        
+
+
 def key_pressed():
     if key == ' ':
         tris[:] = divide_triangles(tris)
     elif key == 'n':
         first_triangle()
-        
+
+
 def divide_triangles(triangles):
     return chain.from_iterable(divide_triangle(tri) for tri in triangles)
-    
+
+
 def divide_triangle(tri):
     r = random(1)
     (vax, vay), (vbx, vby), (vcx, vcy) = a, b, c = tri
