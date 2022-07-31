79f1d4a4f4df44f2f4130f07e048512fb1580e63
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-07-31 10:11:31 -0300

    31

---
+++ 2022/sketch_2022_07_31/sketch_2022_07_31.py
@@ -0,0 +1,72 @@
+import random  # import sample, shuffle, seed
+from villares.helpers import save_png_with_src
+
+nodes = {}
+unvisited_nodes = []
+step = 6
+NBS = (
+    (-2, -2), (-2,  0),
+    (-2,  2), ( 0, -2),
+    ( 0,  2), ( 2, -2),
+    ( 2,  0), ( 2,  2)
+)
+nbs = []
+ox = oy = 0
+def setup():
+    global w, h
+    size(800, 800)
+    no_smooth()
+    w, h = int(width / 2 / step - 5), int(height / 2 / step - 5)
+    stroke_weight(2)
+    start(266)
+
+def start(rnd_seed):
+    global s
+    s = rnd_seed
+    random.seed(rnd_seed)
+    nbs[:] = NBS
+    random.shuffle(nbs)
+    nodes.clear()
+    unvisited_nodes[:] = []
+    x1, y1 = 0, 0
+    unvisited_nodes.extend((
+        (x1, y1),
+        (x1 + 1, y1 + 0),
+        (x1 + 0, y1 + 1),
+    ))
+
+def draw():
+    background(200)
+    translate(width / 2 + ox * step, height / 2 + oy * step)
+    for n, v in nodes.items():
+        xa, ya = n
+        xb, yb, c, g = v
+        if visible(xa, ya) and visible(xb, yb):
+            stroke((
+                color(128, 0, 128),
+                color(0, 128, 0),
+                color(0, 128, 128)
+                )[c % 4])
+            line(xa * step, ya * step, xb * step, yb * step)
+    grow()
+
+def grow():
+    new_nodes = []
+    while unvisited_nodes:
+        x, y = unvisited_nodes.pop()
+        _, _, c, g = nodes.get((x, y), (0, 0, len(unvisited_nodes), 0))
+        random.seed(g // 13 + c)
+        xnbs = random.sample(nbs, 4 - c)
+        for nx, ny in xnbs:
+            xnx, yny = x + nx, y + ny
+            if (xnx, yny) not in nodes:
+                nodes[(xnx, yny)] = (x, y, c, g + 1)
+                new_nodes.append((xnx, yny))
+    unvisited_nodes[:] = new_nodes
+    
+def key_pressed():
+    if key == ' ':
+        print(frame_count)
+        start(frame_count)
+    elif key == 's':
+        save_png_with_src(f'seed{s}')
--- README.md
+++ README.md
@@ -27,6 +27,14 @@ Here are listed some of the tools I have been using:
 
 ---
 
+### sketch_2022_07_31
+
+![sketch_2022_07_31](2022/sketch_2022_07_31/sketch_2022_07_31.png)
+
+[sketch_2022_07_31](https://github.com/villares/sketch-a-day/tree/main/2022/sketch_2022_07_31) [[py5](https://py5.ixora.io/)]
+
+---
+
 ### sketch_2022_07_30
 
 ![sketch_2022_07_30](2022/sketch_2022_07_30/sketch_2022_07_30.png)
