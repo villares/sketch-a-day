a9b251ed88fef3ac4c36d37eb8443b73a6176ecb
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-05 01:37:31 -0300

    Update inputs.py

--- 2022/sketch_2022_08_04arduino_and_sliders/inputs.py
+++ 2022/sketch_2022_08_04arduino_and_sliders/inputs.py
@@ -7,19 +7,13 @@ https://github.com/hackingmath/python-sliders http://farrellpolymath.com/
 2022-08-03 Converting for use with py5 and pyFirmata
 """
 
-
-from pyfirmata import Arduino, util
-from serial.tools import list_ports
 from javax.swing import JOptionPane
 from py5 import *
 
-comports = [comport.device for comport in list_ports.comports()]
-
-
 class InputInterface:
 
-    def __init__(self):
-        self.select_source()
+    def __init__(self, port=None):
+        self.source = get_arduino(port)
         if self.source is None:
             w, h = width(), height()
             # start, end, default
@@ -45,42 +39,44 @@ class InputInterface:
                 slider.update()
 
     def key_pressed(self):
-        if key == 'a':
+        k, kc = key(), key_code()
+        if k == 'a':
             self.sliders[1].down = True
-        if key == 'd':
+        if k == 'd':
             self.sliders[1].up = True
-        if key == 's':
+        if k == 's':
             self.sliders[2].down = True
-        if key == 'w':
+        if k == 'w':
             self.sliders[2].up = True
-        if key_code == LEFT:
+        if kc == LEFT:
             self.sliders[3].down = True
-        if key_code == RIGHT:
+        if kc == RIGHT:
             self.sliders[3].up = True
-        if key_code == DOWN:
+        if kc == DOWN:
             self.sliders[4].down = True
-        if key_code == UP:
+        if kc == UP:
             self.sliders[4].up = True
 
     def key_released(self):
-        if key == 'a':
+        k, kc = key(), key_code()
+        if k == 'a':
             self.sliders[1].down = False
-        if key == 'd':
+        if k == 'd':
             self.sliders[1].up = False
-        if key == 's':
+        if k == 's':
             self.sliders[2].down = False
-        if key == 'w':
+        if k == 'w':
             self.sliders[2].up = False
-        if key_code == LEFT:
+        if kc == LEFT:
             self.sliders[3].down = False
-        if key_code == RIGHT:
+        if kc == RIGHT:
             self.sliders[3].up = False
-        if key_code == DOWN:
+        if kc == DOWN:
             self.sliders[4].down = False
-        if key_code == UP:
+        if kc == UP:
             self.sliders[4].up = False
 
-    def digital(self, pin):
+    def digital_read(self, pin):
         space_pressed = is_key_pressed() and key() == ' '
         if self.source is not None:
             if pin == 13:
@@ -89,22 +85,6 @@ class InputInterface:
                 return self.source.digital_read(pin)
         else:
             return space_pressed
-
-    def select_source(self):
-        port_list = comports[:]
-        if port_list:
-            choice = option_pane(
-                "O seu Arduino está conectado?",
-                "Escolha a porta ou pressione Cancel\npara usar 'sliders':",
-                port_list,
-                -1)  # index for default option
-            if choice is not None:
-                self.source = get_arduino(comports[choice])
-            else:
-                self.source = None
-        else:
-            self.source = None
-        self.help()
 
     def help(self):
         if self.source is not None:
@@ -120,29 +100,6 @@ class InputInterface:
              ←(-) ou  → (+) para o slider 3
              ↓  (-) ou  ↑  (+) para o slider 4"""
         ok = JOptionPane.showMessageDialog(None, message)
-
-
-def option_pane(title, message, options, default=None, index_only=True):
-
-    if default is None:
-        default = options[0]
-    elif index_only:
-        default = options[default]
-
-    selection = JOptionPane.showInputDialog(
-        None,  # frame,
-        message,
-        title,
-        JOptionPane.INFORMATION_MESSAGE,
-        None,  # for Java null
-        options,
-        default)  # must be in options, otherwise 1st is shown
-    if selection:
-        if index_only:
-            return options.index(selection)
-        else:
-            return selection
-
 
 class Slider:
 
@@ -207,11 +164,33 @@ class Slider:
 
 
 def get_arduino(port=None):
-    try:
-        arduino = Arduino(port)
-    except BaseException:
+    from pyfirmata import Arduino, util
+    from serial.tools import list_ports
+    comports = [comport.device for comport in list_ports.comports()]
+    if not comports:
+        print('No ports found.')
         return None
-    util.Iterator(arduino).start()
+    elif isinstance(port, str) and port not in comports:
+        print(f'Port {port} not found.')
+        return None
+    elif len(comports) == 1:
+        port = comports[0]
+    elif port is None:
+        port = option_pane(
+            "Where is your board connected?",
+            "Please select the USB port:",
+            comports,
+            -1)  # index for default option
+        if port is None:
+            print('No port selected')
+            return None    
+    try:
+        print(f'Connecting to port {port}...')
+        arduino = Arduino(port)
+        util.Iterator(arduino).start()
+    except Exception as e:
+        print(repr(e))
+        return None
     for a in range(6):  # A0 A1 A2 A3 A4 A5
         arduino.analog[a].enable_reporting()
     arduino.analog_read = (lambda a: round(arduino.analog[a].read() * 1023)
@@ -225,3 +204,24 @@ def get_arduino(port=None):
                             if digital_pin_dict[d].read() is not None
                             else False)
     return arduino
+
+def option_pane(title, message, options, default=None, index_only=True):
+
+    if default is None:
+        default = options[0]
+    elif index_only:
+        default = options[default]
+
+    selection = JOptionPane.showInputDialog(
+        None,  # frame,
+        message,
+        title,
+        JOptionPane.INFORMATION_MESSAGE,
+        None,  # for Java null
+        options,
+        default)  # must be in options, otherwise 1st is shown
+    if selection:
+        if index_only:
+            return options.index(selection)
+        else:
+            return selection
