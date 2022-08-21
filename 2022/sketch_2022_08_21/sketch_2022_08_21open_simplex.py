17f0ceafade35296235f71099893451b17f7532e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-21 16:35:03 -0300

    08_21

---
+++ 2022/sketch_2022_08_21/sketch_2022_08_21open_simplex.py
@@ -0,0 +1,34 @@
+import py5
+import numpy as np
+
+s = 0.005
+ygrid, xgrid = np.mgrid[0:500:10, 0:500:10]
+
+
+def setup():
+    py5.size(500, 500, py5.P3D)
+    py5.noise_seed(0)
+    py5.color_mode(py5.HSB)
+
+
+def draw():
+    py5.background(0)
+    py5.lights()
+    py5.translate(py5.width / 2, py5.height / 2, py5.height / 2)
+    py5.rotate_x(py5.QUARTER_PI)
+    py5.translate(-py5.width / 2, -py5.height / 2, 0)
+    #z = np.full(50, py5.frame_count * s)
+    d = 128 + 128 * py5.os_noise((xgrid + py5.frame_count) * s, ygrid * s)
+    my_box(xgrid, ygrid, -200 + d / 2, 10, 10, d)
+    $print(py5.frame_count)
+
+
+@np.vectorize
+def my_box(x, y, z, *args):
+    py5.push()
+    py5.translate(x, y, z)
+    py5.fill(args[-1] % 256, 200, 200)
+    py5.box(*args)
+    py5.pop()
+
+py5.run_sketch()
