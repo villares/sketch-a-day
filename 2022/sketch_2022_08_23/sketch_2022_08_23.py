3f208afc6539ed9b527af17c9435083de4374553
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-23 22:52:26 -0300

    22 & 23

---
+++ 2022/sketch_2022_08_23/sketch_2022_08_23.py
@@ -0,0 +1,78 @@
+import py5
+from cell import Cell
+
+play = former_play_state = False
+sample_rate = 1
+
+
+def setup():
+    global cols, rows, board
+    py5.size(800, 800)
+    py5.color_mode(py5.HSB)
+    cols = int(py5.width / Cell.W * 1.5)
+    rows = int(py5.height / Cell.H * 2) - 1
+    init_board()
+
+
+def draw():
+    py5.background(0)
+    for cell in Cell.board.values():
+        cell.display()
+    if play and py5.frame_count % sample_rate == 0:
+        next_board()
+
+
+def key_pressed():
+    global play
+    if py5.key == 'e':
+        init_board()
+    elif py5.key == 'r':
+        init_board(rnd=True)
+    elif py5.key == ' ':
+        play = not play
+
+
+def mouse_dragged():
+    toggle()
+
+
+def mouse_pressed():
+    global play, former_play_state
+    former_play_state = play
+    play = False
+    toggle()
+
+
+def mouse_released():
+    global play
+    play = former_play_state
+    Cell.last_clicked = None
+
+
+def next_board():
+    for cell in Cell.board.values():
+        cell.calc_next_state()
+
+
+def toggle():
+    mx, my = py5.mouse_x, py5.mouse_y
+    i = (mx - Cell.W) // (Cell.W * 1.5)
+    if i % 2 == 0:
+        j = (my - Cell.H) // (Cell.H * 2)
+    else:
+        j = (my - Cell.H * 2) // (Cell.H * 2)
+    try:
+       cell = Cell.board[(i, j)]
+       cell.check_click()
+    except KeyError:
+        pass
+
+
+def init_board(rnd=False):
+    for i in range(cols):
+        for j in range(rows):
+            Cell(i, j, rnd)
+
+
+py5.run_sketch()
+
--- README.md
+++ README.md
@@ -27,6 +27,22 @@ Here are listed some of the tools I have been using:
 
 ---
 
+### sketch_2022_08_23
+
+![sketch_2022_08_23](2022/sketch_2022_08_23/sketch_2022_08_23.png)
+
+[sketch_2022_08_23](https://github.com/villares/sketch-a-day/tree/main/2022/sketch_2022_08_23) [[py5](https://py5.ixora.io/)]
+
+---
+
+### sketch_2022_08_22
+
+![sketch_2022_08_22](2022/sketch_2022_08_22/sketch_2022_08_22.png)
+
+[sketch_2022_08_22](https://github.com/villares/sketch-a-day/tree/main/2022/sketch_2022_08_22) [[py5](https://py5.ixora.io/)]
+
+---
+
 ### sketch_2022_08_21
 
 ![sketch_2022_08_21](2022/sketch_2022_08_21/sketch_2022_08_21.png)
