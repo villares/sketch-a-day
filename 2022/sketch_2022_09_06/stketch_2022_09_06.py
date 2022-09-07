811f9a361ea9b9b09f5971e7ef0c885d36c86c40
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-09-07 12:55:40 -0300

    Update stketch_2022_09_06.py

--- 2022/sketch_2022_09_06/stketch_2022_09_06.py
+++ 2022/sketch_2022_09_06/stketch_2022_09_06.py
@@ -1,40 +1,37 @@
 from itertools import product
 
+w = 30
+th = w * sqrt(3) / 2    
+
 def setup():
-    global x, y, w, th
-    size(200, 520)
-    translate(40, 20)
+    size(180, 520)
     no_stroke()
     rect_mode(CENTER)
-    w = 30
-    th = w * sqrt(3) / 2    
-    x = y = w
-    
-    
-    combos = list(product(
-         ((t, True), (t, False)),
-         ((s, True), (s, False)),
-         ((c, True), (c, False))
-        ))
+
+    shapes = (t, s, c)  # triangle, square, circle
+    on_offs = (((shp, True), (shp, False)) for shp in shapes)
+    combos = list(product(*on_offs))
     print(f'Combinations: {len(combos)}')
-    
+    x = y = w
+    translate(w, w / 2)
     for combo in combos:
         for func, state in combo:
-            func(state)
+            draw_shp(state * 255, func, x, y)
             x += w
         x += w
         if x > width - 5 * w:
             x = w
             y += 2 * w
 
-def t(on):
-    fill(255 * on)
+def draw_shp(c, shp, x, y):
+    fill(c)
+    shp(x, y)
+
+def t(x, y):
     triangle(x - w / 2, y + th / 2, x + w / 2, y + th / 2, x, y - th / 2)
     
-def s(on):
-    fill(255 * on)
+def s(x, y):
     square(x, y, w * 0.85),
    
-def c(on):
-    fill(255 * on)
+def c(x, y):
     circle(x, y, w)
