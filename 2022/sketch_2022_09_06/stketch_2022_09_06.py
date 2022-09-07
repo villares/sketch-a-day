2f9679b8905e0e87e508ebb3690e4d78533d139f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-09-06 23:26:23 -0300

    Update stketch_2022_09_06.py

--- 2022/sketch_2022_09_06/stketch_2022_09_06.py
+++ 2022/sketch_2022_09_06/stketch_2022_09_06.py
@@ -9,37 +9,32 @@ def setup():
     w = 30
     th = w * sqrt(3) / 2    
     x = y = w
-    combos = list(product((wt,bt), (ws, bs), (wc, bc)))
+    
+    
+    combos = list(product(
+         ((t, True), (t, False)),
+         ((s, True), (s, False)),
+         ((c, True), (c, False))
+        ))
     print(f'Combinations: {len(combos)}')
+    
     for combo in combos:
-        for s in combo:
-            s()
+        for func, state in combo:
+            func(state)
             x += w
         x += w
         if x > width - 5 * w:
             x = w
             y += 2 * w
 
-def wt():
-    fill(255)
+def t(on):
+    fill(255 * on)
     triangle(x - w / 2, y + th / 2, x + w / 2, y + th / 2, x, y - th / 2)
     
-def ws():
-    fill(255)
+def s(on):
+    fill(255 * on)
     square(x, y, w * 0.85),
    
-def wc():
-    fill(255)
-    circle(x, y, w)
-
-def bt():
-    fill(0)
-    triangle(x - w / 2, y + th / 2, x + w / 2, y + th / 2, x, y - th / 2)
-    
-def bs():
-    fill(0)
-    square(x, y, w * 0.85),
-   
-def bc():
-    fill(0)
+def c(on):
+    fill(255 * on)
     circle(x, y, w)
