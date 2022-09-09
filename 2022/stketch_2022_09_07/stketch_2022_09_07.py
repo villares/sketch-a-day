6b14612bf38622b402c623c4b451b78aaaf16f98
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-09-09 09:51:06 -0300

    Update stketch_2022_09_07.py

--- 2022/stketch_2022_09_07/stketch_2022_09_07.py
+++ 2022/stketch_2022_09_07/stketch_2022_09_07.py
@@ -1,19 +1,20 @@
 from itertools import product
 
-w = 20
-th = w * sqrt(3) / 2    
+w = 20 # width base-value for 
 
 def setup():
     size(990, 500)
     no_stroke()
+    
     elements = product((-1, 0, 1), repeat=2)  # 9 grid positions
     on_offs = (((el, True), (el, False)) for el in elements)
-    combos = list(product(*on_offs))
-    print(f'Combinations: {len(combos)}')
+    configs = list(product(*on_offs))
+    print(f'Configurations: {len(configs)}')
+    
     x = y = w
     translate(w / 2, w / 4)
-    for combo in combos:
-        for (xo, yo), state in combo:
+    for config in configs:
+        for (xo, yo), state in config:
             fill(state * 255)
             circle(x + xo * w / 3,
                    y + yo * w / 3,
