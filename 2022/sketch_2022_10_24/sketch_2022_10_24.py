93d53f16e144f23c34d6d9c303f91db75b145f82
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-10-24 21:02:28 -0400

    Update sketch_2022_10_24.py

--- 2022/sketch_2022_10_24/sketch_2022_10_24.py
+++ 2022/sketch_2022_10_24/sketch_2022_10_24.py
@@ -1,5 +1,7 @@
 # based on the previous colorful tree / árvore colorida
 # now with translucent triangles!
+# you'll need to install py5coding.org and use the "imported mode" feature
+# more info at https://github.com/villares/villares.github.io/blob/master/como-instalar-py5/index-EN.md
 
 def setup():
     size(800, 800)
