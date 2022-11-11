d817633bf091bcfc722e4f02a13569e6f545cac3
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-11-10 23:20:46 -0300

    Create sketch_2022_11_10.py

---
+++ 2022/sketch_2022_11_10/sketch_2022_11_10.py
@@ -0,0 +1,35 @@
+import PySimpleGUI as sg      
+
+bg = True
+
+def setup():
+    global window
+    size(600, 600)
+    color_mode(HSB)
+    text_size(30)
+    layout = [[sg.Text('PySimpleGUI FTW!')],      
+          [sg.Input(key='-Text-')],
+          [sg.Slider(key='-Hue-', range=(0, 255), orientation='h')], 
+          [sg.Button('Background')]]      
+    window = sg.Window('Sketch controls!', layout)      
+
+def draw():
+    global bg
+#     event, values = window.read(timeout=0)
+#     # print(event, values)     
+#     if event == sg.WIN_CLOSED:        
+#         exit_sketch()
+#         return
+#     elif event == 'Background':
+#         bg = not bg
+#         background(200)
+#         
+#     if bg:
+#         background(50, 100, 100)
+#     fill(255)
+#     text(values['-Text-'], 50, 500)
+#     fill(values['-Hue-'], 200, 200)
+    circle(100, 100, 50 + 50 * sin(frame_count / 10)) 
+          
+def exiting():
+    window.close()
