2acde26d3666d134f84f0122e3b534ba236ba9b4
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-11-11 00:48:25 -0300

    Create sketch_2022_11_10_thread_based.py

---
+++ 2022/sketch_2022_11_10/sketch_2022_11_10_thread_based.py
@@ -0,0 +1,40 @@
+import PySimpleGUI as sg
+
+bg = True
+values = {'-Text-': '', '-Hue-': 0}
+
+def setup():
+    global window
+    size(600, 600)
+    color_mode(HSB)
+    text_size(30)
+    layout = [
+        [sg.Text('PySimpleGUI FTW!')],
+        [sg.Input(key='-Text-', enable_events=True)],
+        [sg.Slider(key='-Hue-', range=(0, 255), orientation='h', enable_events=True)],
+        [sg.Button('Background')]]
+    window = sg.Window('Sketch controls!', layout)
+    launch_repeating_thread(controls, name='controls')
+
+def controls():
+    global bg, event, values
+    event, values = window.read()
+    print(millis())
+    if event == sg.WIN_CLOSED:
+        exit_sketch()
+        return
+    elif event == 'Background':
+        bg = not bg
+        background(200)
+
+def draw():
+    if bg:
+        background(50, 100, 100)
+    fill(255)
+    text(values['-Text-'], 50, 500)
+    fill(values['-Hue-'], 200, 200)
+    circle(100, 100, 50 + 50 * sin(frame_count / 10))
+
+def exiting():
+    window.close()
+    stop_thread('controls')
