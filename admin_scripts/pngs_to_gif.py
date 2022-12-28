3b11fc97dfaadf0bdf69e191b7f83c2a4b6f11bc
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-12-27 22:42:05 -0300

    Update pngs_to_gif.py

--- admin_scripts/pngs_to_gif.py
+++ admin_scripts/pngs_to_gif.py
@@ -1,21 +1,32 @@
 #! /usr/bin/python3
 """
-Create GIF from PNG files in the current working directory!
+Create GIF from PNG files in a directory!
 """
 from pathlib import Path
 from PIL import Image, GifImagePlugin
+import argparse
 
-images = [Image.open(file_path) for file_path
-          in sorted(Path.cwd().iterdir())
-          if file_path.suffix.lower() == '.png']
- 
-if images:
-    images[0].save(
-        'output.gif',
-        save_all=True, append_images=images[1:],
-        optimize=True,
-        duration=200,
-        loop=0
-        )
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', help='Input folder containing the PNG images')
+parser.add_argument('--output', default='out.gif', help='Output GIF file name')
+parser.add_argument('--duration', default=200, type=int, help='Frame duration in milliseconds')
+args = parser.parse_args()
+
+input_dir = Path(args.input or Path.cwd())
+if input_dir.is_dir():
+    images = [Image.open(file_path) for file_path
+              in sorted(input_dir.iterdir())
+              if file_path.suffix.lower() == '.png']
+    if images:
+        images[0].save(
+            input_dir / args.output,
+            save_all=True, append_images=images[1:],
+            optimize=True,
+            duration=args.duration,
+            loop=0
+            )
+    else:
+        print(f'No PNG images found at:\n{input_dir}')
 else:
-    print('No PNG images found!')
+    print(f'{input_dir}\nis not a valid input dir.')
+
