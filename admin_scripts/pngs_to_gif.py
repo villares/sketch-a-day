cd2964a945884f64afec87a665f93381f8e3666e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-12-28 10:49:16 -0300

    Update pngs_to_gif.py

--- admin_scripts/pngs_to_gif.py
+++ admin_scripts/pngs_to_gif.py
@@ -6,10 +6,13 @@ from pathlib import Path
 from PIL import Image, GifImagePlugin
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', help='Input folder containing the PNG images')
-parser.add_argument('--output', default='out.gif', help='Output GIF file name')
-parser.add_argument('--duration', default=200, type=int, help='Frame duration in milliseconds')
+parser = argparse.ArgumentParser(prog='PNG frames to GIF animation')
+parser.add_argument('-i', '--input', help='Input folder containing the PNG images')
+parser.add_argument('-o', '--output', default='output.gif', help='Output GIF file name')
+parser.add_argument('-d', '--duration', default=200, type=int, help='Frame duration in milliseconds')
+parser.add_argument('-NO', '--no-optimization', action='store_true', help='Turn off optimization')
+parser.add_argument('-l', '--loop', default=0, type=int, help='Number of loops (default=0, keep looping)')
+
 args = parser.parse_args()
 
 input_dir = Path(args.input or Path.cwd())
@@ -18,13 +21,18 @@ if input_dir.is_dir():
               in sorted(input_dir.iterdir())
               if file_path.suffix.lower() == '.png']
     if images:
+        output_path = input_dir / args.output
         images[0].save(
-            input_dir / args.output,
+            output_path,
             save_all=True, append_images=images[1:],
-            optimize=True,
+            optimize=not args.no_optimization,
             duration=args.duration,
-            loop=0
+            loop=args.loop
             )
+        print(f'Animation saved at:\n'
+              f'{output_path}\n'
+              #f'Optimization: {not args.no_optimization}'
+              )
     else:
         print(f'No PNG images found at:\n{input_dir}')
 else:
