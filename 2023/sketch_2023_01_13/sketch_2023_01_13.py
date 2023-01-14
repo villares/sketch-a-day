46bdf3b005920cdbb34662707f21ff1ff06a0deb
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-01-14 00:03:32 -0300

    13

---
+++ 2023/sketch_2023_01_13/sketch_2023_01_13.py
@@ -0,0 +1,31 @@
+"""
+SDF exploration
+
+Code for py5 (py5coding.org) imported mode
+"""
+
+import py5
+import numpy as np
+
+def setup():
+    global HW, HH, img
+    py5.size(600, 600)
+    HW = py5.width / 2
+    HH = py5.height / 2
+    img = py5.create_image(600, 600, py5.ALPHA)
+
+def L(x, y): return np.linalg.norm((x, y))  # return (x * x + y * y) ** 0.5
+
+@np.vectorize
+def vsdf(x, y, xo):
+    ring = abs(L(x - 50, y - 50) - 100 + xo) - 50
+    return min(y + 2 * xo, ring, x + 2 * xo)
+
+def draw():
+    py5.translate(HW, HH)
+    xg, yg = np.mgrid[-300:300, -300:300]
+    dg = vsdf(xg, yg, py5.mouse_x - HW)
+    py5.create_image_from_numpy(dg, 'L', dst=img)
+    py5.image(img, -HW, -HH)
+
+py5.run_sketch()
