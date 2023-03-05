9dd8a7b9ccface0b8f49d2e188bb9ecc0f207368
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-03-05 00:11:00 -0300

    Update sketch_2023_03_01.py

--- 2023/sketch_2023_03_01/sketch_2023_03_01.py
+++ 2023/sketch_2023_03_01/sketch_2023_03_01.py
@@ -1,4 +1,6 @@
 import py5
+from itertools import combinations
+from functools import cache
 
 N = 80
 especies_n = 4
@@ -7,7 +9,7 @@ leis = [[0] * especies_n for _ in range(especies_n)]
 balls = []
 
 def setup():
-    global right, left, top, bottom
+    global right, left, top, bottom, pairs
     py5.size(600, 400)  # displayWidth
     py5.no_smooth()
 
@@ -22,38 +24,43 @@ def setup():
         for i in range(especies_n):
             leis[i][j] = py5.random(-3, 3)
     print(leis)
-    py5.launch_repeating_thread(balls_update, name='balls_update', time_delay=0.5)
+    
+    pairs = tuple((balls[i], balls[j]) for i, j in combinations(range(N), 2))
+    
+    #py5.launch_repeating_thread(balls_update, name='balls_update', time_delay=0.)
 
 def draw():
     py5.background(0)
     py5.no_stroke()
-    for ball_i in balls:
-        ball_i.display()
+    balls_update()
+#     for ball_i in balls:
+#         ball_i.display()
     py5.window_title(f'{py5.get_frame_rate():.1f}')
- 
+
 
 def balls_update():
-    for ball_i in balls:
-        ball_i.tick()
-        for ball_j in balls:
+    for ball_i, ball_j in pairs:
             forca = ball_i.pos - ball_j.pos
             dsq = forca.mag_sq
-            if ball_i is not ball_j and dsq < 10000:
+            if dsq < 10000:
                 if(dsq < 10):
                     m = py5.remap(dsq, 10, 0, 0, 1)
                     if m < 0:
-                        forca *= 1
+                        forca *= -1
                         m = -m
                     forca.mag = m
                 else:
                     m = py5.remap(dsq, 10000, 0, 0, 0.001)
                     lm = leis[ball_i.especie][ball_j.especie] * m
                     if lm < 0:
-                        forca *= 1
+                        forca *= -1
                         lm = -lm
                     forca.mag = lm
                 ball_i.vel += forca
-
+#    for b in balls:
+#        b.tick()
+#        b.display() # this won't work if balls_updadte in another thread..
+    tuple(map(lambda b:b.tick() or b.display(), balls))
 
 class Ball:
 
@@ -74,6 +81,7 @@ class Ball:
         if self.pos.y < top:
             self.pos.y = bottom
 
+        # adding display to tick()... breakes id used in separate thread
     def display(self):
         py5.fill(self.especie * color_step, 255, 255)
         py5.ellipse(self.pos.x, self.pos.y, 6, 6)
