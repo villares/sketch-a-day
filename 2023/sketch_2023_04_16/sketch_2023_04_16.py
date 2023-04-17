214ba2d6a7981f46adea18e469cfadcb3e270df8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-04-17 18:48:52 -0300

    16 17

---
+++ 2023/sketch_2023_04_16/sketch_2023_04_16.py
@@ -0,0 +1,42 @@
+import py5
+from random import choice
+from villares.helpers import save_png_with_src
+
+options = [10, 20, 30, 40]
+
+def make_nr_choice(collection, avoid_last_num=1):
+    from collections import deque
+    if len(set(collection)) <= avoid_last_num:
+        raise ValueError(
+            f'Number of unique items in collection ({len(set(collection))}) must be '
+            f'bigger than number of previous items to avoid repeating ({avoid_last_num})')
+    def nr_choice(memory=deque([], avoid_last_num)):
+        options = list(set(collection) - set(memory))
+        chosen = choice(options)
+        memory.append(chosen)
+        return chosen
+    return nr_choice
+
+def setup():
+    py5.size(1000, 1000)
+    py5.color_mode(py5.HSB)
+    py5.stroke_weight(0.5)
+    py5.no_loop()
+
+def draw():
+    h = py5.height / 5
+    for nr in range(5):
+        nr_choice = make_nr_choice(options, 3)
+        x = 0
+        while x < py5.width:
+            c = nr_choice()
+            py5.fill(c * 5, 200, 200)
+            py5.rect(x, nr * h, c, h)
+            x += c
+
+def key_pressed():
+    if py5.key == 's':
+        save_png_with_src()
+    py5.redraw()
+
+py5.run_sketch()
---
+++ 2023/sketch_2023_04_17/sketch_2023_04_17.py
@@ -0,0 +1,43 @@
+import py5
+from random import choice
+from villares.helpers import save_png_with_src
+
+options = [8, 16, 24, 32, 64]
+
+def make_nr_choice(collection, avoid_last_num=1):
+    from collections import deque
+    if len(set(collection)) <= avoid_last_num:
+        raise ValueError(
+            f'Number of unique items in collection ({len(set(collection))}) must be '
+            f'bigger than number of previous items to avoid repeating ({avoid_last_num})')
+    def nr_choice(memory=deque([], avoid_last_num)):
+        options = list(set(collection) - set(memory))
+        chosen = choice(options)
+        memory.append(chosen)
+        return chosen
+    return nr_choice
+
+def setup():
+    py5.size(1000, 1000)
+    py5.color_mode(py5.HSB)
+    py5.stroke_weight(0.5)
+    py5.no_loop()
+
+def draw():
+    h = py5.height / 5
+    for nr in range(5):
+        nr_choice = make_nr_choice(options, 2)
+        x = 0
+        while x < py5.width:
+            c = nr_choice()
+            print(c)
+            py5.fill(c * 2, 200, 200)
+            py5.rect(x, nr * h, (c + 8), h)
+            x += c
+
+def key_pressed():
+    if py5.key == 's':
+        save_png_with_src()
+    py5.redraw()
+
+py5.run_sketch()
