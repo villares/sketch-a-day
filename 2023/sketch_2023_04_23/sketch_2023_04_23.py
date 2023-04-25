5bf58a0e6b3a226b146b3e8d9fd321fe5a4a7bf2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-04-24 22:12:08 -0300

    23

--- 2023/sketch_2023_04_23/sketch_2023_04_23.py
+++ 2023/sketch_2023_04_23/sketch_2023_04_23.py
@@ -1,29 +1,31 @@
+""" Save accumulated frames on a PDF. Not working with SVG! """
+
 fator_escala = 10
-salvar_svg = False
+salvar_pdf = False
 
 def setup():
     size(200, 200)  # com fator 10, vira 2000px x 2000px
     
 def draw():
-#    if salvar_svg:
-#        svg_output.scale(fator_escala)
+#    if salvar_pdf:
+#        pdf_output.scale(fator_escala)
     fill(255, 32)
     if is_mouse_pressed:    
         circle(mouse_x, mouse_y, width / 10)        
         
 def key_pressed():
-    global salvar_svg, file_name, svg_output
+    global salvar_pdf, file_name, pdf_output
     if key == 'h':
-        if not salvar_svg:
-            salvar_svg = True
+        if not salvar_pdf:
+            salvar_pdf = True
             background(200) # limpa a tela (mas não entra na gravação)
-            file_name = f'{frame_count}.svg' 
-            svg_output = create_graphics(
-                width * fator_escala, height * fator_escala, SVG, file_name)
-            begin_record(svg_output)
+            file_name = f'{frame_count}.pdf' 
+            pdf_output = create_graphics(
+                width * fator_escala, height * fator_escala, PDF, file_name)
+            begin_record(pdf_output)
             print(f'iniciando a gravação de {file_name}')
         else:
-            salvar_svg = False
+            salvar_pdf = False
             end_record()
             print(f'{file_name} gravado.')
 
