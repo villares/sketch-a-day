271b65d8cd7b58947312dc81a7825fb01b7a8b81
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-05-10 23:13:32 -0300

    10

--- 2023/sketch_2023_05_10/sketch_2023_05_10.py
+++ 2023/sketch_2023_05_10/sketch_2023_05_10.py
@@ -3,7 +3,9 @@ Inspired by 'follow 3' code from Keith Peters in the Processing examples
 you will need to install py5 http://py5coding.org and use the sketch runner
 for this is the "imported mode" style.
 """
-from functools import cache
+
+anchor_pos = 1
+adjust_pos = 1
 
 def setup():
     size(500, 500)
@@ -13,12 +15,12 @@ def setup():
     
 def init_segments():
     Segment.segments = []
-    Segment()
+    seg = Segment()
+    seg.drag((width / 2, width / 2))
     for i in range(100):
         anchor_seg = random_choice(Segment.segments[i//4:])
-        new_seg = Segment(link=(anchor_seg, 0.5))
+        new_seg = Segment(link=(anchor_seg, anchor_pos))
         new_seg.length = anchor_seg.length * 0.80
-    Segment.first_seg.drag((width / 2, width / 2))
     
                 
 def draw():
@@ -26,7 +28,9 @@ def draw():
     Segment.update_all()
 
 def mouse_dragged():
-    Segment.first_seg.drag(lerp_tuple(Segment.first_seg.start, (mouse_x, mouse_y), 0.25))
+    current_mouse = mouse_x, mouse_y
+    new_pos = lerp_tuple(Segment.first_seg.start, current_mouse, 0.25)
+    Segment.first_seg.drag(new_pos)
 
 def mouse_wheel(e):
     for s in Segment.segments:
@@ -37,19 +41,21 @@ def key_pressed():
     if key == ' ':
         init_segments()
 
+
+
 class Segment:
     
     segments = []
+    first_seg = None
     
     def __init__(self, start=None, end=None, link=None):
         self.start = start if start else (0, 0)
         self.end = end if end else (1, 1)
         self.link = link
         self.length = 60
+        if not self.segments:
+            self.__class__.first_seg = self
         self.segments.append(self)
-        if len(self.segments) == 1:
-            Segment.first_seg = self
-
         
     def __iter__(self):
         return iter((self.start, self.end))
@@ -72,47 +78,37 @@ class Segment:
         
     def draw(self):
         stroke((self.length * 2) % 255, 200, 128, 128)
-        line(*self.start, *self.end)        
-        #points(self.points_at_distance(self.length))
+        line(*self.start, *self.end)
+#         if self.link:
+#             points(self.points_at_distance(self.link[0].length, 1.33))
  
     def points_at_distance(self, R, amt):
         xm, ym = lerp_tuple(self.start, self.end, amt)
         (xa, ya), (xb, yb) = self
-        if xb == xa:
-            m_perp = 0
-            xp1 = xm + R
-            xp2 = xm - R
-            yp1 = ym
-            yp2 = ym
-        else:
-            m = (yb - ya) / (xb - xa)
-            m_perp = -1 / m
-            a = 1 + 1 / m_perp**2
-            b = -2 * ym * (1 + 1 / m_perp**2)
-            c = ym**2 * (1 + 1 / m_perp**2) - R**2
-            yp1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
-            yp2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
-            xp1 = (yp1 - ym) / m_perp + xm
-            xp2 = (yp2 - ym) / m_perp + xm
+        d = dist(xa, ya, xb, yb)
+        nx, ny = -(ya - yb) / d, (xa - xb) / d
+        px, py = nx * R, ny * R 
+        xp1 = xm + px
+        yp1 = ym + py
+        xp2 = xm - px
+        yp2 = ym - py
         return ((xp1, yp1), (xp2, yp2))
+
 
     def adjust(self):
         if self.link is not None:
             link_obj, amt = self.link
-            pa, pb = link_obj.points_at_distance(link_obj.length, 1.5)
-            #pa, pb = self.distance_sorted(self.start, pa, pb)
-            #pa, pb = self.distance_sorted((250, 250), pa, pb)
-            random_seed(int(id(self)))
+            pa, pb = link_obj.points_at_distance(link_obj.length, adjust_pos)
+            random_seed(int(id(self)) + int(self.start[0]/100))
             cp = random_choice((pa, pb))
-            self.end = lerp_tuple(self.end, cp, 0.10)
+            self.end = lerp_tuple(self.end, cp, 0.25)
 
     @staticmethod
-    @cache
-    def distance_sorted(po, pa, pb):
+    def dist_sorted(po, pa, pb):
         x, y = po
         xa, ya = pa
         xb, yb = pb
-        if (x - xa) ** 2 + (y - ya) ** 2 >= (x - xb) ** 2 + (y - yb) ** 2:
+        if (x - xa) ** 2 + (y - ya) ** 2 > (x - xb) ** 2 + (y - yb) ** 2:
             return pb, pa
         else:
             return pa, pb
@@ -133,4 +129,6 @@ def lerp_tuple(a, b, t):
     return tuple(lerp_tuple(ca, cb, t) if isinstance(ca, tuple)
                  else lerp(ca, cb, t)             
                  for ca, cb in zip(a, b))
+
+
 
