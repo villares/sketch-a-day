367b381f91dd57d91fae2d6d460e8d7b611c0696
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-05-13 13:00:28 -0300

    Create sketch_2023_05_13.py

---
+++ 2023/sketch_2023_05_13/sketch_2023_05_13.py
@@ -0,0 +1,28 @@
+"""
+Code using https://py5coding.org "imported mode" style, learn more
+at https://abav.lugaralgum.com/como-instalar-py5/index-EN.html
+"""
+
+tam = 60 
+
+def setup():
+    size(600, 600)
+    fill(0)
+    
+def draw():
+    background(240)
+    rows = cols = width // tam
+    for r in range(rows):
+        y = tam / 2 + r * tam 
+        for c in range(cols):
+            x = tam / 2 + c * tam 
+            w = tam * 0.9
+            hw = w / 2
+            d = dist(x, y, mouse_x, mouse_y)
+            x_down = x + (12 * (c + r + d)) % w - hw
+            triangle(x - hw, y - hw,
+                     x + hw, y - hw,
+                     x_down, y + hw)
+            
+            
+            
