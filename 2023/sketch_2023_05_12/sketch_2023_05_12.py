c91413dadd59bd509f9d0dc80d1ac8d23011a79b
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-05-12 23:40:09 -0300

    12

---
+++ 2023/sketch_2023_05_12/sketch_2023_05_12.py
@@ -0,0 +1,18 @@
+tam = 20
+
+def setup():
+    size(800, 800)
+    fill(0)
+    
+def draw():
+    background(240)
+    for x in range(tam, width, tam):
+        for y in range(tam, height, tam):
+            w = tam * 0.8
+            d = dist(x, y, mouse_x, mouse_y)
+            triangle(x - w / 2, y - w / 2,
+                     x + w / 2, y - w / 2,
+                     x + cos(d / 50) * w / 2, y + w / 2)
+            
+            
+            
