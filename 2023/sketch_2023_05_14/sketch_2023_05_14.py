b44b2ccc6d1c0ec319e1aee5ce10c864cc938e75
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-05-14 19:01:58 -0300

    13 14

--- 2023/sketch_2023_05_14/sketch_2023_05_14.py
+++ 2023/sketch_2023_05_14/sketch_2023_05_14.py
@@ -3,11 +3,12 @@ Code using https://py5coding.org "imported mode" style, learn more
 at https://abav.lugaralgum.com/como-instalar-py5/index-EN.html
 """
 
-tam = 60 
+tam = 30 
 
 def setup():
     size(600, 600)
     fill(0)
+    rect_mode(CENTER)
     
 def draw():
     background(240)
@@ -16,12 +17,11 @@ def draw():
         y = tam / 2 + r * tam 
         for c in range(cols):
             x = tam / 2 + c * tam 
-            w = tam * 0.9
-            hw = w / 2
-            x_down = x + (12 * (c + r)) % w - hw
-            triangle(x - hw, y - hw,
-                     x + hw, y - hw,
-                     x_down, y + hw)
+            d = 20 - c  - r
+            if d > 0:
+                circle(x, y, d + 5)
+            else:
+                square(x, y, d - 5)
             
             
             
