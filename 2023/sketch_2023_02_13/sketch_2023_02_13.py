0a9892e229b2fd94705c8c93db3210a5400d6b6f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-05-16 00:47:25 -0300

    Update sketch_2023_02_13.py

--- 2023/sketch_2023_02_13/sketch_2023_02_13.py
+++ 2023/sketch_2023_02_13/sketch_2023_02_13.py
@@ -4,6 +4,40 @@ from random import shuffle
 noise_increment = 0.005
 
 def setup():
+    global mesh_x, mesh_y, rgb_map, g_map, b_map
+    size(500, 500)
+    color_mode(HSB)
+    cs = [color(int(h / 8) * 4 + 64, 128 + h / 2, 255)
+           for h in range(256)]
+    shuffle(cs)
+    rgb_map =  np.dstack((
+        [c >> 16 & 0xFF for c in cs],
+        [c >> 8 & 0xFF for c in cs],
+        [c & 0xFF for c in cs]
+        ))[0]
+
+    mesh_x, mesh_y = np.meshgrid(
+        np.arange(0, width * noise_increment, noise_increment),
+        np.arange(0, height * noise_increment, noise_increment)
+    )
+
+def draw():    
+    h = remap(os_noise(mesh_x, mesh_y, frame_count * noise_increment),
+              -1, 1, 0, 255).astype(np.uint8)
+    npa = rgb_map[h]
+    set_np_pixels(npa, bands="RGB")
+    window_title(str(round(get_frame_rate(), 1)))
+    
+
+
+"""
+# previously
+import numpy as np
+from random import shuffle
+
+noise_increment = 0.005
+
+def setup():
     global mesh_x, mesh_y, r_map, g_map, b_map
     size(500, 500)
     color_mode(HSB)
@@ -24,3 +58,4 @@ def draw():
     rgb = np.dstack((r_map[h], g_map[h], b_map[h]))
     set_np_pixels(rgb, bands="RGB")
     window_title(str(round(get_frame_rate(), 1)))
+"""
