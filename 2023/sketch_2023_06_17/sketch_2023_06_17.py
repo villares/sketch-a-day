b3386880e7d841ad5f96e75160a092af39192137
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-06-17 12:07:36 -0300

    Create sketch_2023_06_17.py

---
+++ 2023/sketch_2023_06_17/sketch_2023_06_17.py
@@ -0,0 +1,35 @@
+from itertools import product, combinations
+
+def setup():
+    global combos
+    size(400, 400, P3D)
+    grid = list(product(range(-75, 100, 50),
+                       repeat=2))
+    combos = list(combinations(grid, 5))
+    print(len(combos))
+    frame_rate(10)
+    
+def draw():
+    translate(200, 225)  
+    ortho()
+    rotate_x(-atan(sin(radians(45))))
+    rotate_y(radians(45))
+
+    background(200)
+    i = frame_count % len(combos)
+    cs = (combos + combos)[i:i+10]
+    for z, positions in zip(range(-100, 101, 20), cs):
+        if z == -100:
+            fill(0, 0, 200)
+        else:
+            fill(255)
+        pos_plus_z = map(lambda p: p+(z,), positions)
+        for pos in pos_plus_z:
+            element(pos)
+
+def element(pos):
+    x, y, z = pos
+    push()
+    translate(x, z, y)
+    box(50, 5, 50)
+    pop()
