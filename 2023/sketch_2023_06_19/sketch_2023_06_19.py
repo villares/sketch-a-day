0928ee2c13c3381734518c6438a84423a1e94268
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-06-19 20:28:32 -0300

    Update sketch_2023_06_19.py

--- 2023/sketch_2023_06_19/sketch_2023_06_19.py
+++ 2023/sketch_2023_06_19/sketch_2023_06_19.py
@@ -1,34 +1,34 @@
 from itertools import product, combinations
+import py5
 
 def setup():
     global combos
-    size(400, 400, P3D)
+    py5.size(400, 400, py5.P3D)
     grid = list(product(range(-75, 100, 50),
-                       repeat=2))
+                        repeat=2))
     combos = list(combinations(grid, 5))
-    print(len(combos))
-    frame_rate(10)
-    color_mode(HSB)
-    
-def draw():
-    translate(200, 225)  
-    ortho()
-    rotate_x(-atan(sin(radians(45))))
-    rotate_y(radians(45))
+    print(len(combos)) # 4368
+    py5.frame_rate(10)
+    py5.color_mode(py5.HSB)
 
-    background(200)
-    i = frame_count % len(combos)
+def draw():
+    py5.translate(200, 225)
+    py5.ortho()
+    py5.rotate_x(-py5.atan(py5.sin(py5.radians(45))))
+    py5.rotate_y(py5.radians(45))
+    py5.background(200)
+    i = py5.frame_count % len(combos)
     cs = (combos + combos)[i:i+10]
     for z, positions in zip(range(-100, 101, 20), cs):
-        
-        fill((255 + z + i * 20) % 255, 200, 200)
+        py5.fill((255 + z + i * 20) % 255, 200, 200)
         pos_plus_z = map(lambda p: p+(z,), positions)
         for pos in pos_plus_z:
             element(pos)
 
 def element(pos):
     x, y, z = pos
-    push()
-    translate(x, z, y)
-    box(50, 5, 50)
-    pop()
+    with py5.push_matrix():
+        py5.translate(x, z, y)  # note the inversion
+        py5.box(50, 5, 50)
+
+py5.run_sketch()
