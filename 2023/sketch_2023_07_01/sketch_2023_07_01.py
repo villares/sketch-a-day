f299510b662abc04de5bcb8e651d7dc3c99dbeb4
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-07-01 23:03:05 -0300

    07_01

---
+++ 2023/sketch_2023_07_01/sketch_2023_07_01.py
@@ -0,0 +1,50 @@
+from itertools import product
+
+import py5
+
+
+s = 0 # random seed
+rx = ry = tx = ty = 0
+
+def setup():
+    py5.size(600, 600, py5.P3D)
+    
+def draw():
+    global rx, ry
+    py5.background(0)
+    py5.ortho()
+    py5.random_seed(s)    
+    boxes_pos = [(150 + py5.random_int(6) * 50, x, y) for i, (x, y)
+                 in enumerate(product(range(150, 451, 50), repeat=2))]
+    py5.translate(py5.width / 2, py5.height / 2, -py5.height / 2)
+    py5.rotate_y(ry)
+    py5.rotate_x(rx)
+    py5.translate(-py5.width / 2, -py5.height / 2,  -py5.height / 2)
+    for p in boxes_pos:
+        xyzbox(*p)
+
+    rx = py5.lerp(rx, tx, 0.1)    
+    ry = py5.lerp(ry, ty, 0.1)    
+    
+        
+def xyzbox(x, y, z, w=50):
+    with py5.push_matrix():
+        py5.stroke(255)
+        py5.fill(x / 2 - 50, y / 2 - 50, z / 2 - 50)
+        py5.translate(x , y, z)
+        py5.box(w)
+
+def key_pressed():
+    global s, tx, ty
+    if py5.key == 's':
+        py5.save_frame(f'{s}.png')
+        s += 1
+    elif py5.key == '1':
+        tx = ty = py5.HALF_PI
+    elif py5.key == '0':
+        tx = ty = 0
+    elif py5.key == '2':
+        ty = py5.PI
+        tx = 0
+
+py5.run_sketch()
