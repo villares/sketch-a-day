727458b0c9dfcff68ece38baccf7bab42181e4d8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-07-05 09:48:14 -0300

    Update sketch_2023_07_05.py

--- 2023/sketch_2023_07_05/sketch_2023_07_05.py
+++ 2023/sketch_2023_07_05/sketch_2023_07_05.py
@@ -6,16 +6,15 @@ rx = ry = tx = ty = ntx = nty = 0.02
 t = 1
 
 def setup():
+    global boxes
     py5.size(600, 600, py5.P3D)
+    boxes = SGenerator(0, make_boxes)
     
 def draw():
-    global s, ns, t, tx, ty
-
+    global t, tx, ty
     py5.background(0)
     #py5.ortho()
-    boxes_a = make_boxes(s)
-    boxes_b = make_boxes(ns)
-
+    
     rx = py5.lerp(tx, ntx, t)    
     ry = py5.lerp(ty, nty, t)    
     
@@ -23,18 +22,13 @@ def draw():
     py5.rotate_y(ry)
     py5.rotate_x(rx)
     py5.translate(-py5.width / 2, -py5.height / 2,  -py5.height / 2)
-    for pa, pb in zip(boxes_a, boxes_b):
-        xyzbox(*lerp_tuple(pa, pb, t))
+    
+    for b in boxes():
+        xyzbox(*b)
 
-    t = py5.lerp(t, 1, 0.1)
-
+    SGenerator.update_t()
     ms = py5.mouse_x // 20
-    if t > 0.99 and ms != ns:
-        s = ns
-        ns = ms
-        t = 0
-        tx, ty = ntx, nty # avoid rotation change
-
+    boxes.update_s(ms)
         
 def xyzbox(x, y, z, w=50):
     with py5.push_matrix():
@@ -53,15 +47,34 @@ def make_boxes(s):
     return [(150 + py5.random_int(6) * 50, x, y) for i, (x, y)
                  in enumerate(product(range(150, 451, 50), repeat=2))]
 
-# class SGenerator:
-#     t = 0
-#     generators = []
-#     
-#     def __init__(self, first_seed, func):
-#         self.s = self.ns = first_seed
-#         
-#         SGenerator.generators.append(self)
-        
+class SGenerator:
+    generators = []
+    
+    def __init__(self, first_seed, func):
+        self.func = func
+        self.s = self.ns = first_seed
+        self.t = 0        
+        SGenerator.generators.append(self)
+
+    def __call__(self):
+        rb = self.func(self.ns)
+        if self.t > 0.99:
+            return rb
+        ra = self.func(self.s)
+        return lerp_tuple(ra, rb, self.t)
+               
+    def update_s(self, us):
+        if self.t > 0.99 and us != self.ns:
+            self.s = self.ns
+            self.ns = us
+            self.t = 0
+       
+    @classmethod
+    def update_t(cls):
+        for g in cls.generators:
+            g.t = py5.lerp(g.t, 1, 0.1)
+
+
 def key_pressed():
     global s, ns, tx, ty, ntx, nty, t
     t = 0
