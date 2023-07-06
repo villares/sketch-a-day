a3bf2a406f213fb2ce3558106408db9331812525
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-07-06 19:53:27 -0300

    Update sketch_2023_07_06.py

--- 2023/sketch_2023_07_06/sketch_2023_07_06.py
+++ 2023/sketch_2023_07_06/sketch_2023_07_06.py
@@ -3,22 +3,12 @@ from random import choice, sample, seed
 import py5
 from inputs import InputInterface
 
-
-s = 1
-w = 100
-
 colors = (
-    py5.color(0),
+    py5.color(150, 150, 0),
     py5.color(150,0, 0),
-    py5.color(255)
+    py5.color(0, 0, 150)
     )
     
-def key_pressed():
-    inputs.key_pressed()
-    
-def key_released():
-    inputs.key_released()
-
 def setup():
     global inputs
     py5.size(1200, 600)
@@ -85,64 +75,9 @@ def star(x, y, radius_a, radius_b, n_points, rot=0):
             py5.quadratic_vertex(x + sx, y + sy, x + cx, y + cy)
     py5.end_shape()
 
-    
-def make_stars(s):
-    py5.random_seed(s)
-    return [(150 + py5.random_int(6) * 50, x, y) for i, (x, y)
-                 in enumerate(product(range(150, 451, 50), repeat=2))]
-
-def get_colors(s):
-    angles_dict = {
-        '1': (py5.HALF_PI + 0.3, py5.HALF_PI + 0.3),
-        '2': (py5.PI, 0),
-        }
-    return angles_dict.get(s, (0, 0))
-
-class LState:
-    """Lerpable State Objects"""
-    ls_objects = {}
-    
-    def __init__(self, func, name=None, first_seed=0):
-        self.func = func
-        self.name = name or len(self.ls_objects)
-        self.s = self.ns = first_seed
-        self.t = 0        
-        self.ls_objects[self.name] = self
-
-    def __call__(self, *args):
-        rb = self.func(self.ns, *args)
-        if self.t > 0.99:
-            return rb
-        ra = self.func(self.s, *args)
-        return lerp_tuple(ra, rb, self.t)
-               
-    def update_s(self, us):
-        if self.t > 0.99 and us != self.ns:
-            self.s = self.ns
-            self.ns = us
-            self.t = 0
-       
-    @classmethod
-    def update(cls, *args, **kwargs):
-        for g in cls.ls_objects.values():
-            g.t = py5.lerp(g.t, 1, 0.1)
-        for obj, v in args:
-            obj.update_s(v)
-        for k, v in kwargs.items():
-            cls.ls_objects[k].update_s(v)
-
-# LState class depends on this
-def lerp_tuple(a, b, t):   
-    return tuple(lerp_tuple(ca, cb, t) if isinstance(ca, tuple)
-             else py5.lerp(ca, cb, t)             
-             for ca, cb in zip(a, b))
-
 def key_pressed():
     if py5.key == 'p':
        py5.save_frame(f'{py5.frame_count}.png')
-    elif str(py5.key) in '0123':
-        angles.update_s(py5.key)
-
     inputs.key_pressed()
     
 def key_released():
