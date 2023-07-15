40573159b161fe80012dc4ea7af9871998fc0215
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-07-15 16:56:50 -0300

    15

---
+++ 2023/sketch_2023_07_15/inputs.py
@@ -0,0 +1,286 @@
+"""
+This will hopefully switch between Arduino (Firmata) variable input and
+nice _sliders_list based on Peter Farell's _sliders_list htts://twitter.com/hackingmath
+https://github.com/hackingmath/python-_sliders_list http://farrellpolymath.com/
+
+2020-03-14 Fix for Arduino no the first serial port (index 0)!
+2022-08-03 Converting for use with py5 and pyFirmata
+2022-08-05 Trying to make it compatible with all py5 "modes"
+2023-07-15 Added KEYBOARD_STEP constant and fixed keyboard crash when using Arduino
+"""
+
+import py5
+
+class InputInterface:
+    
+    
+    
+    def __init__(self, port=None, keyboard_step=2):
+        Slider.KEYBOARD_STEP = keyboard_step
+        self.source = get_arduino(port)
+        if self.source is None:
+            sketch = py5.get_current_sketch()
+            w, h = sketch.width, sketch.height
+            # start, end, default
+            A = Slider(0, 1023, 512)
+            B = Slider(0, 1023, 512)
+            C = Slider(0, 1023, 512)
+            D = Slider(0, 1023, 512)
+            A.position(40, h - 70)
+            B.position(40, h - 30)
+            C.position(w - 140, h - 70)
+            D.position(w - 140, h - 30)
+            self._sliders_list = {1: A, 2: B, 3: C, 4: D}
+
+    def analog_read(self, pin):
+        if self.source is not None:
+            return self.source.analog_read(pin)
+        else:
+            return self._sliders_list[pin].val
+
+    def update(self, display=True):
+        if self.source is None:
+            for pin, slider in self._sliders_list.items():
+                slider.update(display=display)
+
+    def key_pressed(self):
+        if self.source is None:
+            sketch = py5.get_current_sketch()
+            k, kc = sketch.key, sketch.key_code
+            if k == 'a':
+                self._sliders_list[1].down = True
+            if k == 'd':
+                self._sliders_list[1].up = True
+            if k == 's':
+                self._sliders_list[2].down = True
+            if k == 'w':
+                self._sliders_list[2].up = True
+            if kc == py5.LEFT:
+                self._sliders_list[3].down = True
+            if kc == py5.RIGHT:
+                self._sliders_list[3].up = True
+            if kc == py5.DOWN:
+                self._sliders_list[4].down = True
+            if kc == py5.UP:
+                self._sliders_list[4].up = True
+
+    def key_released(self):
+        if self.source is None:
+            sketch = py5.get_current_sketch()
+            k, kc = sketch.key, sketch.key_code
+            if k == 'a':
+                self._sliders_list[1].down = False
+            if k == 'd':
+                self._sliders_list[1].up = False
+            if k == 's':
+                self._sliders_list[2].down = False
+            if k == 'w':
+                self._sliders_list[2].up = False
+            if kc == py5.LEFT:
+                self._sliders_list[3].down = False
+            if kc == py5.RIGHT:
+                self._sliders_list[3].up = False
+            if kc == py5.DOWN:
+                self._sliders_list[4].down = False
+            if kc == py5.UP:
+                self._sliders_list[4].up = False
+
+    def digital_read(self, pin):
+        sketch = py5.get_current_sketch()
+        k, ikp = sketch.key, sketch.is_key_pressed
+        space_pressed = ikp and k == ' '
+        if self.source is not None:
+            if pin == 13:
+                return self.source.digital_read(13) or space_pressed
+            else:
+                return self.source.digital_read(pin)
+        else:
+            return space_pressed
+
+    def help(self):
+        from javax.swing import JOptionPane
+        if self.source is not None:
+            message = """   Teclas:
+            'h' para esta ajuda
+            'p' para salvar uma imagem"""
+        else:
+            message = """    Teclas:
+            'h' para esta ajuda
+            'p' para salvar uma imagem
+            'a' (-) ou 'd' (+) para o slider 1
+            's' (-) ou 'w' (+) para o slider 2
+             ←(-) ou  → (+) para o slider 3
+             ↓  (-) ou  ↑  (+) para o slider 4"""
+        ok = JOptionPane.showMessageDialog(None, message)
+
+
+class Slider:
+
+    _sliders_list = []
+    KEYBOARD_STEP = 2
+    
+    def __init__(self, low, high, default):
+        """slider has range from low to high
+        and is set to default"""
+        self.low = low
+        self.high = high
+        self.val = default
+        self.clicked = False
+        self.up, self.down = False, False
+        Slider._sliders_list.append(self)
+
+    def position(self, x, y):
+        """slider's position on screen"""
+        self.x = x
+        self.y = y
+        # the position of the rect you slide
+        self.rectx = self.x + py5.remap(self.val, self.low, self.high, 0, 120)
+        self.recty = self.y - 10
+
+    def update(self, display=True):
+        """updates the slider"""
+        sketch = py5.get_current_sketch()
+        mx, my, imp = sketch.mouse_x, sketch.mouse_y, sketch.is_mouse_pressed
+        # To draw the slider without any style or coord tansformation
+        if display:
+            py5.push()         
+            py5.reset_matrix() 
+            #py5.camera() # uncomment this if on P3D
+            py5.rect_mode(py5.CENTER)
+            # black translucid rect behind slider
+            py5.fill(0, 100)
+            py5.no_stroke()
+            py5.rect(self.x + 60, self.y, 130, 20)
+            # gray line behind slider
+            py5.stroke_weight(4)
+            py5.stroke(200)
+            py5.line(self.x, self.y, self.x + 120, self.y)
+            # press mouse to move slider
+            if (self.x < mx < self.x + 120 and self.y < my < self.y + 20):
+                py5.fill(250)
+                py5.text_size(10)
+                py5.text(str(int(self.val)), self.rectx, self.recty + 35)
+                if imp:
+                    self.rectx = mx
+            # draw rectangle
+            py5.no_stroke()
+            py5.fill(255)
+            py5.rect(self.rectx, self.recty + 10, 10, 20)
+            py5.pop()
+        # key usage
+        if self.up:
+            self.rectx += Slider.KEYBOARD_STEP
+        if self.down:
+            self.rectx -= Slider.KEYBOARD_STEP
+        # constrain rectangle
+        self.rectx = py5.constrain(self.rectx, self.x, self.x + 120)
+        self.val = py5.remap(
+            self.rectx,
+            self.x,
+            self.x + 120,
+            self.low,
+            self.high
+        )
+
+
+def get_arduino(port=None):
+    """
+    This is a PyFirmata 'helper' that tries to connect to an Arduino
+    compatible board.
+    
+    If no port is informed, using pyserial's serial.tools.list_ports.comports(),
+    if one port is found, tries that one. If more than one port is found,
+    shows for the user to choose one. Returns None if no port is found or if the
+    user cancels the dialog.
+    
+    If it successfully connects, it will return a pyfirmata Arduino object,
+    but before that, it starts a pyfirmata.util.Iterator, and adds to the object
+    both analog_read() and digital_read() functions that mimic Processing's
+    Firmata library interface:
+    Readings are never None, and analog pins return a value between 0 and 1023.
+    """   
+    from pyfirmata import Arduino, util
+    from serial.tools import list_ports
+    
+    comports = [comport.device for comport in list_ports.comports()]
+    if not comports:
+        print('No ports found.')
+        return None
+    elif isinstance(port, str) and port not in comports:
+        print(f'Port "{port}" not found.')
+        return None
+    elif isinstance(port, int):
+        if port >= len(comports):
+            print(f'Port [{port}] not found.')
+            return None
+        else:
+            port = comports[port]
+    elif len(comports) == 1:
+        port = comports[0]
+    elif port is None:
+        port = option_pane(
+            'Where is your board?',
+            'Please select the USB port where your '
+            'Arduino compatible board is connected:',
+            comports,
+            -1)  # index for default option
+        if port is None:
+            print('No port selected.')
+            return None
+    try:
+        print(f'Connecting to port {port}...')
+        arduino = Arduino(port)
+        util.Iterator(arduino).start()
+    except Exception as e:
+        print(repr(e))
+        return None
+    # Prepare analog_read() for A0 A1 A2 A3 A4 A5
+    for a in range(6):  
+        arduino.analog[a].enable_reporting()
+    arduino.analog_read = (lambda a: round(arduino.analog[a].read() * 1023)
+                           if arduino.analog[a].read() is not None
+                           else 0)
+    # Prepare digital_read() for D2 to D13
+    digital_pin_dict = {d: arduino.get_pin(f'd:{d}:i')
+                        for d in range(2, 14)}
+    for d in digital_pin_dict.keys():
+        digital_pin_dict[d].enable_reporting()
+    arduino.digital_read = (lambda d: digital_pin_dict[d].read()
+                            if digital_pin_dict[d].read() is not None
+                            else False)
+    return arduino
+
+
+def option_pane(title, message, options, default=None, index_only=False):
+    """
+    A helper for Java swing JOptionPane input dialog with drop down options.
+    
+    title     : str   - Dialog window's title (make it shorter than message).
+    message   : str   - Text shown before the drop down.
+    options   : list  - List of strings to show in the drop down.
+    default   : int   - None or index to the pre-selected option in the list.
+    index_only: False - Function returns an option string from the options list
+                        provided, or None, if the dialog was cancelled;
+                True  - Function returns the position index to the options list.    
+    """
+    from javax.swing import JOptionPane
+    
+    if default is None:
+        default = options[0]
+    elif index_only:
+        default = options[default]
+
+    selection = JOptionPane.showInputDialog(
+        None,     # frame
+        message,
+        title,
+        JOptionPane.INFORMATION_MESSAGE,
+        None,     # for Java null
+        options,
+        default)  # must be in options, otherwise first is shown
+    if selection:
+        if index_only:
+            return options.index(selection)
+        else:
+            # Trouble: selection can be java.lang.String
+            return str(selection) if selection else None
---
+++ 2023/sketch_2023_07_15/sketch_2023_07_15.py
@@ -0,0 +1,52 @@
+from inputs import InputInterface
+
+def setup():
+    global inputs
+    size(800, 800)
+    color_mode(HSB)
+    rect_mode(CENTER)  # retângulos desenhados pelo centro
+    no_stroke()  # sem contorno
+    frame_rate(30)
+    # Ask user for Arduino port, uses slider if none is selected`
+    inputs = InputInterface()
+
+def draw():
+    background(0)  
+    grid_elem = int(inputs.analog_read(1) / 16)  # 0 a 63 linhas e colunas na grade
+    elem_size = int(inputs.analog_read(2) / 16)  # 0 a 63 tamanho base dos quadrados
+    rand_size = int(inputs.analog_read(3) / 16)  # escala a randomização do tamanho
+    rand_posi = int(inputs.analog_read(4) / 16)  # escala a randomização da posição
+    # trava a random entre os ciclos de draw
+    # mas varia com o número de colunas na grade
+    random_seed(int(inputs.analog_read(1) / 4))
+    # espaçamento entre os quadrandos
+    spac_size = int(width / (grid_elem + 1))
+    for x in range(spac_size // 2, width, spac_size):  # um x p/ cada coluna
+        for y in range(spac_size // 2, width, spac_size):  # um y p/ cada linha
+            # sorteia um tamanho (se o rand_size > 0)
+            square_size = elem_size + rand_size * random(-1, 1)
+            offsetX = rand_posi * random(-1, 1)
+            offsetY = rand_posi * random(-1, 1)
+            HUE = (remap(offsetX + offsetY, -64, 64, 0, 255) + 32) % 255
+            SAT = remap(square_size, 0, 63, 0, 255)
+            fill(HUE, SAT, 255, 200)
+            rect(x + offsetX,  # desenha um quadrado
+                 y + offsetY,
+                 square_size,
+                 square_size)
+
+    # Updates reading or draws sliders and checks mouse dragging / keystrokes
+    inputs.update()
+
+
+def key_pressed():
+    global GIF_EXPORT
+    if key == 'p':  # save PNG
+        saveFrame("####.png")
+    if key == 'h':
+        inputs.help()
+
+    inputs.key_pressed()
+
+def key_released():
+    inputs.key_released()
