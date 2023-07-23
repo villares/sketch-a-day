9a9b6d04a9fca93526683a657a636c75e1433853
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-07-23 11:58:19 -0400

    Update sketch_2023_07_23.py

--- 2023/sketch_2023_07_23/sketch_2023_07_23.py
+++ 2023/sketch_2023_07_23/sketch_2023_07_23.py
@@ -3,7 +3,9 @@ import cv2
 import matplotlib.pyplot as plt
 
 # https://pt.wikipedia.org/wiki/Ficheiro:Mappa_Topographico_do_Municipio_de_S%C3%A3o_Paulo_-_Folha_37,_Acervo_do_Museu_Paulista_da_USP.jpg
-img_path = '/home/villares/GitHub/sketch-a-day/2023/sketch_2023_07_21/mapa.jpg'
+#img_path = '/home/villares/GitHub/sketch-a-day/2023/sketch_2023_07_21/mapa.jpg'
+
+img_path = 'sample.png'
 
 def apply_threshold(img_array, threshold):
     threshold_applied = []
@@ -15,7 +17,8 @@ def apply_threshold(img_array, threshold):
             else:
                 threshold_applied[len(threshold_applied)-1].append([0, 0, 0])
     new_img = np.array(threshold_applied, np.uint8)
-    cv2.imwrite(str(threshold)+".jpg", new_img)
+    cv2.imwrite(f'{threshold}.png', new_img)
 
 img = cv2.imread(img_path, 0)
-apply_threshold(img, 210)
+for n in range(5, 23):
+    apply_threshold(img, n * 10)
