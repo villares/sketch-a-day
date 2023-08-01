455c8ae2a4c67cc09d6783507cdd562f5053c16e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-08-01 00:54:38 -0300

    31

--- 2023/sketch_2023_07_31/sketch_2023_07_31.py
+++ 2023/sketch_2023_07_31/sketch_2023_07_31.py
@@ -3,23 +3,14 @@ import py5
 from shapely import Point, LineString, Polygon
 from shapely import MultiPoint, MultiLineString, MultiPolygon
 from shapely.ops import unary_union
-
-from villares.shapely_helpers import draw_shapely
 # From https://github.com/villares/villares/blob/main/shapely_helpers.py
+from villares.shapely_helpers import draw_shapely
 
 shapes = []
 
 def setup():
     py5.size(600, 600)
-#     ls = LineString(
-#         [(100, 50), (100, 200), (200, 50)]
-#         )
-#     shapes.append(ls.buffer(30))
-#     shapes.append(ls)
-    #shapes.append(unary_union(random_bars(50, 50, 500, 500)))
-    #)
-    #mp = MultiPoint([(py5.random(50, 500), py5.random(50, 500)) for _ in range(20)])
-    #shapes.append(mp)
+    py5.stroke_join(py5.ROUND)
     for i in range(5):
         shapes.append(random_bars(50, 50, 500, 500, b=20-i*3))
     
@@ -28,7 +19,6 @@ def random_lines(x, y, w, h, n=30):
             for _ in range(n)]
     
 def random_bars(x, y, w, h, n=30, b=4):
-    #return unary_union([l.buffer(b) for l in random_lines(x, y, w, h, n)])
     return MultiLineString(random_lines(x, y, w, h, n)).buffer(b)
     
 def draw():
@@ -38,6 +28,7 @@ def draw():
         py5.stroke_weight(3)
         draw_shapely(shps)
     
-    
+def key_pressed():
+    py5.save('out.png')
     
 py5.run_sketch(block=False)
