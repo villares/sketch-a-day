62b32a9cd4d926d8b869c7de403598b75515fefe
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-08-18 01:02:16 -0300

    16 17

--- 2023/sketch_2023_08_17/sketch_2023_08_17.py
+++ 2023/sketch_2023_08_17/sketch_2023_08_17.py
@@ -2,6 +2,7 @@ from shapely import Polygon
 import trimesh  # install mapbox_earcut!
 
 cube_list = []
+hole_list = []
     
 import py5
 
@@ -9,30 +10,36 @@ def setup():
     global mesh
     py5.size(700, 700, py5.P3D)
     for _ in range(10):
-        cube_list.append(cube(py5.random(-100, 100),
-                              py5.random(-100, 100),
-                              py5.random(-100, 100)))
-    
+        x, y, z = (py5.random(-200, 200),
+                   py5.random(-200, 200),
+                   py5.random(-200, 200))
+        cube_list.append(cube(x, y, z, 200))
+        hole_list.append(cube(x, y, z, 100))
+        
     mesh = cube_list[0]
     for c in cube_list[1:]:
         mesh = mesh.union(c)
-
+    hole = hole_list[0]
+    for c in hole_list[1:]:
+        hole = hole.union(c)
+    mesh = mesh.difference(hole)
+    mesh = mesh.difference(cube(0, 0, 0, 400))
 
 def draw():
-    py5.background(100, 200, 100)
+    py5.background(100, 100, 200)
     py5.lights()
     py5.no_stroke()
     py5.translate(py5.width/2, py5.height/2, -py5.height/2)
     py5.rotate_y(py5.radians(py5.mouse_x))
     draw_mesh(mesh)
 
-def cube(x, y, z, w=100):
-    hw = w / 2 
-    pts = ((-hw, -hw), (hw, -hw), (hw, hw), (-hw, hw))
-    c = trimesh.creation.extrude_polygon(Polygon(pts), w)
-    c.apply_translation((x, y, z - hw))
+def cube(x, y, z, w, h=None, d=None):
+    h = w or h
+    d = d or h
+    c = trimesh.creation.box((w, w, w))
+    c.apply_translation((x, y, z))
     return c
-    #trimesh.creation.box(w 
+    
 
 def draw_mesh(m):
     for i, face in enumerate(m.faces):
@@ -40,3 +47,4 @@ def draw_mesh(m):
             py5.vertices([m.vertices[v] for v in face])
 
 py5.run_sketch(block=False)
+
