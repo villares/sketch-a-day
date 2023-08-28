7a7cbc068b2ac45ae3040bc31070a506a650b179
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-08-27 21:22:37 -0300

    27

--- 2023/sketch_2023_08_27/sketch_2023_08_27.py
+++ 2023/sketch_2023_08_27/sketch_2023_08_27.py
@@ -11,7 +11,7 @@ import osmnx as ox
 ox.settings.log_console=True
 ox.settings.use_cache=True
 
-from villares.shapely_helpers import draw_shapely
+from geopandas import GeoDataFrame
 
 bgcolor = '#343434'
 edge_color = '#CCCCCC'
@@ -21,37 +21,40 @@ dist = 2000
 
 bbox = ox.utils_geo.bbox_from_point(point, dist=dist)
 fp = ox.features_from_point(point, tags={'building':True}, dist=dist)
-G = ox.graph_from_point(point, network_type='drive', dist=dist, truncate_by_edge=True, retain_all=True)
+G = ox.graph_from_point(point, network_type='walk', dist=dist, truncate_by_edge=True, retain_all=True)
 nodes, edges = ox.graph_to_gdfs(G)
 m_edges = edges.to_crs(epsg=3857)
+m_fp = fp.to_crs(epsg=3857)
 #m_edges = edges.set_crs('EPSG:4326').to_crs('EPSG:3857')
-
 paulista = m_edges[m_edges['name'] == 'Avenida Paulista']
 #selected_edges_gdf = gdf_meters[gdf_meters['edge_id'].isin(selected_edges)]
 paulista_length = paulista.geometry.length.sum()
-print(paulista_length)  # looks wrong!
-#import matplotlib.pyplot as plt
-# fig, ax = ox.plot_graph(G, bgcolor=bgcolor, node_size=0, edge_color=edge_color, show=False)
-# fig, ax = ox.plot_footprints(fp, ax=ax, bbox=bbox, color=bldg_color, show=False) #, save=True)
-# paulista.plot(ax=ax, color='red')
-# plt.show()
+print(paulista_length)  # value seems wrong!!!
 
 def setup():
-    py5.size(600, 600)
-    py5.background(bldg_color)
-    # m_edges['geometry'] = m_edges['geometry'].apply(lambda geom: transform(lambda x, y: (x, -y), geom))
+    py5.size(800, 800)
+    py5.background(bgcolor)
     minx, miny, maxx, maxy = m_edges.total_bounds
     scale_x = py5.width / (maxx - minx)
-    #scale_y = py5.height / (maxy - miny)
-    scale_y = py5.height / -(miny - maxy)  # to switch the y axis without doing x, -y first
+    scale_y = py5.height / (maxy - miny)
     translate_x = -minx * scale_x
     translate_y = -miny * scale_y
     matrix = [scale_x, 0, 0, scale_y, translate_x, translate_y]
-    m_edges['geometry'] = m_edges['geometry'].affine_transform(matrix)
+    screen_edges = m_edges['geometry'].affine_transform(matrix).apply(flip_y)
+    screen_buildings = m_fp['geometry'].affine_transform(matrix).apply(flip_y)
+    screen_paulista = paulista.affine_transform(matrix).apply(flip_y)
+    py5.no_stroke()
+    py5.fill(bldg_color)
+    draw_shapely(b for b in screen_buildings if b.area)    
     py5.stroke(edge_color)
-    draw_shapely(m_edges['geometry'])
-    
-    
+    draw_shapely(screen_edges)
+    py5.stroke(255, 0, 0)
+    draw_shapely(screen_paulista)
+    py5.save(f'{__file__[:-3]}.png')
+
+def flip_y(geom):
+    return transform(lambda x, y: (x, -y + py5.height), geom)
+ 
 def draw_shapely(shps, sketch: py5.Sketch=None):
     """
     Draw most shapely objects with py5.
@@ -77,12 +80,14 @@ def draw_shapely(shps, sketch: py5.Sketch=None):
         s.point(tuple(shps.coords)[0]) # yeah shps.coords for a lone Point is a CoordinateSequence.
     elif isinstance(shps, MultiPoint):
         s.points(tuple(p.coords)[0] for p in shps.geoms)
+    elif isinstance(shps, GeoDataFrame):
+        for shp in shps.geometry:
+                draw_shapely(shp)
     else:
         try:
             for shp in shps:
                 draw_shapely(shp)
         except TypeError as e:
             print(f"Unable to draw: {shps}")
-        
         
 py5.run_sketch(block=False)
