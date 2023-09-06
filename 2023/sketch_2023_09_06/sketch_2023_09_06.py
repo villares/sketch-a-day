b90c01d6f7bd05b8e8ea747ebf1943750cf66c95
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-09-06 00:50:52 -0300

    6

--- 2023/sketch_2023_09_06/sketch_2023_09_06.py
+++ 2023/sketch_2023_09_06/sketch_2023_09_06.py
@@ -9,10 +9,10 @@ def setup():
     
 def draw():
     background(255)
-    #background(0)
+    background(0)
     #image(img, 0, 0)
     xi, yi, wi, hi = 200, 100, 1000, 500
-    passo = 5
+    passo = 15
     for x in range(0, width, passo):
         for y in range(0, height, passo):
             xa = int(remap(x, 0, width, xi, xi + wi))
@@ -20,10 +20,9 @@ def draw():
             c = img.get_pixels(xa , ya)
             b = brightness(c)
             fill(color(hue(c), 255, 200))
-            #fill(0)
-            d = 1 + passo * (255 - b) / 255
-            #d = passo * b / 255
-            circle(passo / 2 + x, passo / 2 + y, d)
+            #d = 1 + passo * (255 - b) / 255
+            d = passo * b / 255
+            mais(passo / 2 + x, passo / 2 + y, 2 + d)
             
     save_frame(__file__[:-3] + '.png')
          
@@ -35,28 +34,13 @@ def clip_with_mask(img, mask, x, y):
     result.mask(mask)
     return result
 
-def g_balao_oval(w, h, draw_stroke=False):
-    x, y = w / 2, h / 2
-    b = create_graphics(int(w), int(h))
-    b.begin_draw()
-    b.clear()
-    if draw_stroke:
-        b.stroke_join(ROUND)
-        b.stroke_weight(5)
-        b.triangle(x - w * 0.45, y,
-                 x, y,
-                 x + w * 0.45, y + h * 0.45)
-    else:
-        b.no_stroke()
-    b.ellipse(x, y, w * 0.9, h * 0.9)
-    b.no_stroke() # triangulo sem traço para "furar" a ellipse
-    # triangulo mata meia espessura, mas fica legal
-    b.triangle(x - w * 0.45, y,
-             x, y,
-             x + w * 0.45, y + h * 0.45)
-    return b
+def mais(x, y, w):
+    rect_mode(CENTER)
+    rect(x, y, w, w / 3)
+    rect(x, y, w / 3, w)
 
 
 
 def key_pressed():
     redraw()
+
