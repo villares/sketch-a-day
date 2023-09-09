728a22f6bfc0e228bf1b61fe0dd3d408555a0a26
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-09-09 09:37:25 -0300

    9

--- 2023/sketch_2023_09_08/sketch_2023_09_08.py
+++ 2023/sketch_2023_09_08/sketch_2023_09_08.py
@@ -1,11 +1,12 @@
 import py5
 import numpy as np
 
-from itertools import chain
+from itertools import chain, product
 
 w = 100
 n = 6
 pad = 5
+
 
 def setup():
     py5.size(600, 600)
@@ -15,28 +16,31 @@ def generate_faces():
     global faces
     grid = {}
     face_list = []
-    for i in range(n):
-        for j in range(n):
-            grid[(i, j)] = (
-                w / 2 + i * w + py5.random(-10, 10),
-                w / 2 + j * w + py5.random(-10, 10)
-                )
-    for i in range(n - 1):
-        for j in range(n - 1):
-            a, b, c, d = (
-                grid[(i, j)],
-                grid[(i + 1, j)],
-                grid[(i + 1, j + 1)],
-                grid[(i, j + 1)]
-                )
-            face_list.append((
-                (a[0] + pad, a[1] + pad),
-                (b[0] - pad, b[1] + pad),
-                (c[0] - pad, c[1] - pad),
-                (d[0] + pad, d[1] - pad),
-                ))
+    for i, j in product(range(n), range(n)):
+        grid[(i, j)] = (
+            w / 2 + i * w + py5.random(-10, 10),
+            w / 2 + j * w + py5.random(-10, 10),
+        )
+    for i, j in product(range(n - 1), repeat=2):
+        a, b, c, d = (
+            grid[(i, j)],
+            grid[(i + 1, j)],
+            grid[(i + 1, j + 1)],
+            grid[(i, j + 1)],
+        )
+        face_list.append((
+            (a[0] + pad, a[1] + pad),
+            (b[0] - pad, b[1] + pad),
+            (c[0] - pad, c[1] - pad),
+            (d[0] + pad, d[1] - pad),
+        ))
     faces = np.array(face_list)
-    
+
+def bounding_box(vs):
+    x_min, y_min = np.min(vs, axis=0)
+    x_max, y_max = np.max(vs, axis=0)
+    return (x_min, y_min), (x_max, y_max)
+
 def draw():
     py5.background(150, 150, 250)
     py5.fill(255)
@@ -48,17 +52,8 @@ def draw():
         (min_x, min_y), (max_x, max_y) = bounding_box(f)
         py5.rect(min_x, min_y, max_x, max_y)
 
-# def bounding_box(vs):
-#     xs = [v.x for v in vs]
-#     ys = [v.y for v in vs]
-#     return (min(xs), min(ys)), (max(xs), max(ys))
-def bounding_box(vs):
-#    vs = np.array(vs)
-    x_min, y_min = np.min(vs, axis=0)
-    x_max, y_max = np.max(vs, axis=0)
-    return (x_min, y_min), (x_max, y_max)
-
 def key_pressed():
     generate_faces()
-    
+
+
 py5.run_sketch()
