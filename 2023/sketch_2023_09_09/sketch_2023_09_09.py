2038d1f61e0887a12ed0c0a936751ca1b836d866
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-09-10 01:42:55 -0300

    9

--- 2023/sketch_2023_09_09/sketch_2023_09_09.py
+++ 2023/sketch_2023_09_09/sketch_2023_09_09.py
@@ -9,8 +9,11 @@ pad = 5
 
 
 def setup():
+    global img
     py5.size(600, 600)
     generate_faces()
+    img = py5.load_image('a.png')
+    py5.no_loop()
 
 def generate_faces():
     global faces
@@ -41,18 +44,56 @@ def bounding_box(vs):
     return (x_min, y_min), (x_max, y_max)
 
 def draw():
-    py5.background(150, 150, 250)
-    py5.fill(255)
+    py5.background(150)
+    #py5.rect_mode(py5.CORNERS)
+    for f in faces:
+        bb = bounding_box(f)
+        (min_x, min_y), (max_x, max_y) = bb
+        w = max_x - min_x
+        h = max_y - min_y
+        m = make_mask(f)
+        rx, ry = py5.random(img.width - w), py5.random(img.height - h)
+        clipped = clip_with_mask(img, m, rx, ry)
+        py5.image(clipped, min_x, min_y)
+    py5.no_fill()
+    py5.stroke_weight(3)
+    py5.stroke(0, 0, 200)
+    #py5.translate(-1, -1)
     with py5.begin_shape(py5.QUADS):
         py5.vertices(list(chain(*faces)))
-    py5.rect_mode(py5.CORNERS)
-    py5.fill(255, 0, 0, 100)
-    for f in faces:
-        (min_x, min_y), (max_x, max_y) = bounding_box(f)
-        py5.rect(min_x, min_y, max_x, max_y)
+
+        
+def make_mask(f):
+    global fc
+    fc = f.copy()
+    (min_x, min_y), (max_x, max_y) = bounding_box(f)
+    fc[:,0] -= min_x
+    fc[:,1] -= min_y
+    w = max_x - min_x
+    h = max_y - min_y
+    pg = py5.create_graphics(int(w), int(h))
+    pg.begin_draw()
+    pg.no_stroke()
+    pg.begin_shape()
+    pg.vertices(fc)
+    pg.end_shape()
+    pg.end_draw()
+    return pg
+
+
+
+def clip_with_mask(img, mask, x, y):    
+    """Clip an image using a mask, its dimensions, and a given position."""
+    w, h = mask.width, mask.height
+    result = py5.create_image(int(w), int(h), py5.ARGB)
+    result.copy(img, int(x), int(y), w, h, 0, 0, w, h)
+    result.mask(mask)
+    return result
+
 
 def key_pressed():
     generate_faces()
+    py5.redraw()
 
 
-py5.run_sketch()
+py5.run_sketch(block=False)
