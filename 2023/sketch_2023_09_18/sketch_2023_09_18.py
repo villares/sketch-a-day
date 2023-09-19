73916756037629879a901edbfffef78b4e7a93df
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-09-19 01:42:54 -0300

    18

--- 2023/sketch_2023_09_18/sketch_2023_09_18.py
+++ 2023/sketch_2023_09_18/sketch_2023_09_18.py
@@ -1,37 +1,45 @@
 from pathlib import Path
 import py5
 
+BACKGROUND = py5.color(200, 200, 240)
+SELECTED = py5.color(100, 100, 200)
+
 files = []
-line_h = 200
-col_width = 200
+line_h = 160
+coll_w = 160
+start = 0
 
 def setup():
-    py5.size(1000, 1000)
+    py5.size(800, 800)
     py5.text_align(py5.LEFT, py5.TOP)
-   # py5.select_folder('Select a folder', list_files)
+    list_files(Path.cwd().parent.parent)
 
 def draw():
-    i = 0
+    py5.background(BACKGROUND)
+    i = start
     x = y = 0
     while i < len(files) and x < py5.width:
-        py5.fill(255)
-        py5.rect(x, y, col_width - 2, line_h - 2)
+        py5.fill(SELECTED if mouse_over(x, y) else 255)
+        py5.rect(x + 1, y + 1, coll_w - 2, line_h - 2)
         py5.fill(0)
         py5.text(files[i][0], x, y)
         y += line_h
         if y > py5.height:
             y = 0
-            x += col_width
+            x += coll_w
+        i += 1
 
+def mouse_over(x, y):
+    return x < py5.mouse_x < x + coll_w and y < py5.mouse_y < y + line_h
 
 def list_files(folder):
-    files.clear()
     if folder:
         for f in Path(folder).iterdir():
-            files.append((f.name, f))
+            files.append([f.name[:16], f, None, None])
 
 def key_pressed():
     if py5.key == 'o':
+        files.clear()
         py5.select_folder('Select a folder', list_files)
 
 
