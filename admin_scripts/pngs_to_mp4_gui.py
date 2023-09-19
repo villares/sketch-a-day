d125695496046e8138de71511dea0970b555e483
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-09-19 16:58:15 -0300

    Create pngs_to_mp4_gui.py

---
+++ admin_scripts/pngs_to_mp4_gui.py
@@ -0,0 +1,73 @@
+#!/home/villares/miniconda3/bin/python
+
+from pathlib import Path
+
+import PySimpleGUI as sg    # precisa instalar o PySimpleGUI (pode ser no pip install ou no Thonny "packages")
+import imageio              # precisa instalar:
+# precisa instalar pip install imageio[ffmpeg]
+
+
+sugestao_output = Path.cwd() / 'out.mp4'
+L_FONT = ('Courier', 20)
+I_FONT = ('Courier', 16)
+
+# Define the GUI layout
+layout = [
+    [sg.Text('Input folder:', font=L_FONT)],
+    [sg.InputText(font=I_FONT), sg.FolderBrowse(font=I_FONT)],
+    [sg.Text('Output file:', font=L_FONT)],
+    [sg.InputText(default_text=sugestao_output, font=I_FONT), sg.FileSaveAs(font=I_FONT)],
+    [sg.Text('Frame duration (milliseconds):', font=L_FONT), sg.InputText(default_text='200', font=I_FONT, size=(6, 1))],
+    [sg.Button('Create MP4', font=L_FONT), sg.Button('Cancel', font=L_FONT)]
+]
+
+# Create the GUI window
+window = sg.Window('Create GIF', layout)
+
+# Run the GUI event loop
+while True:
+    # Get the next GUI event
+    event, values = window.read()
+
+    # Exit the event loop if the window was closed or the Cancel button was clicked
+    if event in (None, 'Cancel'):
+        break
+
+    # Create the GIF if the Create GIF button was clicked
+    if event == 'Create MP4' and values:
+        # Get the input and output values from the GUI
+        input_dir = values[0] 
+        output_file = values[1]
+        duration = int(values[2]) if values[2] else 200
+        dir_path = Path(input_dir)
+        # Create the GIF if there are any images
+        try:
+            fps = 1000 / duration
+            # Load the PNG images from the input folder
+            images = [imageio.v3.imread(file_path) for file_path
+                  in sorted(dir_path.iterdir())
+                  if file_path.suffix.lower() == '.png']
+            if images:
+                writer = imageio.v2.get_writer(output_file, fps=fps)
+                for img in images:
+                  writer.append_data(img)
+                writer.close()
+#                 imageio.v3.imwrite(
+#                 output_file,
+#                 images,
+#                 duration=duration / 1000,
+#                 loop=loops,
+#                 )
+                sg.popup('MP4 created successfully!')
+            else:
+                sg.popup('No PNG images found!')
+        except Exception as e:
+            if str(e).startswith('all input arrays'):
+                sg.popup('Select only images of same size.')
+            else:    
+                sg.popup(str(e))
+        # Close the window
+        #window.close()
+
+# Clean up the GUI
+window.close()
