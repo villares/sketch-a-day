4fc8edfbd1190c58c59d4169119df204384b34e8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-10-08 00:38:39 -0300

    7

---
+++ 2023/sketch_2023_10_07/sketch_2023_10_07.py
@@ -0,0 +1,49 @@
+axioma = 'X'
+regras = {
+    'X': '[-X][+X][+FX]-FXB',
+    'F': 'FF',
+    }
+passo = 9
+angulo = 25  # angulo em graus
+iteracoes = 6
+
+def setup():
+    global frase_resultado
+    size(800, 800)
+    frase_inicial=axioma
+    for _ in range(iteracoes):
+        frase_resultado = ''
+        for simbolo in frase_inicial:
+            substituir = regras.get(simbolo, simbolo)
+            frase_resultado = frase_resultado + substituir
+        # print(frase_inicial, frase_resultado)
+        frase_inicial=frase_resultado
+    print(len(frase_resultado))
+
+#def draw():
+    background(210, 210, 150)
+    stroke_weight(3)
+    angulo = 25
+    # angulo = mouse_x / 70.0
+    translate(width / 2, height * 0.9)
+    for i, simbolo in enumerate(frase_resultado):
+        if simbolo == 'X':   # se simbolo for igual a 'X'
+            pass
+        elif simbolo == 'F':   # else if (senão se) o simbolo é F
+                stroke(i % 255)
+                line(0, 0, 0, -passo)
+                translate(0, -passo)
+        elif simbolo == 'B':
+            no_stroke()
+            fill(0, 0, 100)
+            circle(0, 0, passo)
+        elif simbolo == '+':
+            rotate(radians(-angulo))  # + random(-5, 5)))
+        elif simbolo == '-':
+            rotate(radians(+angulo))  # + random(-5, 5)))
+        elif simbolo == '[':
+            push_matrix()
+        elif simbolo == ']':
+            pop_matrix()
+
+    save(f'{__file__[:-3]}.png')
--- docs/README.md
+++ docs/README.md
@@ -35,6 +35,33 @@ Here are listed some of the tools I have been using:
 
 ---
 
+### sketch_2023_10_07
+
+![sketch_2023_10_07](https://raw.githubusercontent.com/villares/sketch-a-day/main/2023/sketch_2023_10_07/sketch_2023_10_07.png)
+
+[sketch_2023_10_07](https://github.com/villares/sketch-a-day/tree/main/2023/sketch_2023_10_07) [[py5](https://py5coding.org/)]
+
+#LSystem
+axiom = 'X'
+rules = {
+    'X': '[-X][+X][+FX]-FXB',
+    'F': 'FF',
+    } 
+# F -> foward step
+# B -> blue circle
+
+---
+
+### sketch_2023_10_06
+
+![sketch_2023_10_06](https://raw.githubusercontent.com/villares/sketch-a-day/main/2023/sketch_2023_10_06/sketch_2023_10_06.png)
+
+[sketch_2023_10_06](https://github.com/villares/sketch-a-day/tree/main/2023/sketch_2023_10_06) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2023_10_05
 
 ![sketch_2023_10_05](https://raw.githubusercontent.com/villares/sketch-a-day/main/2023/sketch_2023_10_05/sketch_2023_10_05.png)
