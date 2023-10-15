5bfa58440e3a930118e3da0405a1c38026b3a7d1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-10-15 14:26:21 -0300

    Create sketch_2023_10_15.py

---
+++ 2023/sketch_2023_10_15/sketch_2023_10_15.py
@@ -0,0 +1,22 @@
+import py5
+
+from villares.arcs import b_circle_arc
+
+def setup():
+    py5.size(500, 500)
+    r = 50
+    for y in range(0, py5.height + 1, r * 2):
+        with py5.begin_shape():
+            for i, x in enumerate(range(r, py5.width, r * 2)):
+                if i % 2 == 0:
+                    b_circle_arc(x, y, r, py5.PI, py5.PI, mode=2)
+                else:
+                    b_circle_arc(x, y, r, py5.PI, -py5.PI, mode=2)
+            w, i_offset = x, i
+            for i, x in enumerate(range(w, 0, -r * 2)):
+                if (i + i_offset) % 2 == 0:
+                    b_circle_arc(x, y + r, r, 0, -py5.PI, mode=2)
+                else:
+                    b_circle_arc(x, y + r, r, 0, py5.PI, mode=2)
+
+py5.run_sketch(block=False)
