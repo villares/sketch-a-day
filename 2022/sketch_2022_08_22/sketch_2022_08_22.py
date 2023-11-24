66e3dfdb4a359b1402a5aad023b7e44a984ed62d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-11-24 10:43:39 -0300

    24 wip

--- 2022/sketch_2022_08_22/sketch_2022_08_22.py
+++ 2022/sketch_2022_08_22/sketch_2022_08_22.py
@@ -20,7 +20,7 @@ def draw():
     #z = np.full(50, py5.frame_count * s)
     d = 128 + 128 * py5.os_noise(xgrid * s, ygrid * s, py5.frame_count * s)
     my_box(xgrid, ygrid, -200 + d / 2, 10, 10, d)
-    #print(py5.frame_count)
+    py5.window_title(f'{py5.get_frame_rate():.1f}')
 
 
 @np.vectorize
@@ -31,4 +31,9 @@ def my_box(x, y, z, *args):
     py5.box(*args)
     py5.pop()
 
+def key_pressed():
+    py5.print_line_profiler_stats()
+
+py5.profile_draw()
 py5.run_sketch()
+
