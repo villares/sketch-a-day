e9746f6b83615b5726ac06e62782a116e669bf9e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-12-08 11:50:07 -0300

    7

--- 2023/sketch_2023_12_07/sketch_2023_12_07.py
+++ 2023/sketch_2023_12_07/sketch_2023_12_07.py
@@ -1,17 +1,34 @@
 import py5
+from pathlib import Path
 
 img = None
-s = {'x':100, 'y':100, 'w':100, 'h':50}
+s = {
+    'x':600,
+    'y':200,
+    'w':120,
+    'h':180,
+    'source_step': 10,
+    'target_step':10,
+    'cor_a': '#145814FF',
+    'cor_b': '#589A58FF',
+    'texto1': 'inscrição\nprévia'
+}
 save_pdf = False
-
-
+modules = []
 
 def setup():
-    global img
-    py5.size(1200, 600)
+    global img, f1
+    py5.size(1600, 1000)
     img = py5.load_image('a.jpg')
-
-
+    load_modules(py5.sketch_path('modules'))
+    f1 = py5.create_font('Open Sans Bold', 50)
+    
+def load_modules(modules_folder):
+    modules[:] = []
+    for item in sorted(modules_folder.iterdir()):
+        if item.suffix == '.svg':
+            modules.append(py5.load_shape(item))
+        
 def draw():
     global save_pdf
     py5.background(200)
@@ -29,13 +46,31 @@ def draw():
         hi = int(py5.remap(h, 0, tw, 0, img.width))
         img.load_np_pixels()
         img_region = py5.create_image_from_numpy(img.np_pixels[yi:yi+hi,xi:xi+wi])
-        py5.translate(tw, 0)
+        py5.translate(tw, 0)  # draw on the right side of the screen
+
         if save_pdf:
             out = py5.create_graphics(int(py5.width / 2), py5.height, py5.PDF, 'out.pdf')
             py5.begin_record(out)
-            
-        py5.image(img_region, 0, 0)                
         
+        ss, ts = s['source_step'], s['target_step']
+        for i in range(0, img_region.width, ss):
+            for j in range(0, img_region.height, ss):
+                px = img_region.get_pixels(i, j)
+                x, y = i / ss * ts, j / ss * ts
+                b = 1 - py5.brightness(px) / 255
+                k = int(b * (len(modules) - 1))
+                m = modules[k]
+                m.disable_style()
+                py5.no_stroke()
+                py5.fill(s['cor_b'])
+                py5.square(x, y, ts)
+                py5.fill(s['cor_a'])
+                py5.shape(m, x, y, ts, ts)
+        py5.text_font(f1)
+        py5.text_leading(43)
+        py5.fill(255, 255, 240)
+        py5.text(s['texto1'], 100, 200)
+
         if save_pdf:
             py5.end_record()
             save_pdf = False
