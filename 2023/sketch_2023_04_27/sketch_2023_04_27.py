5d5cdcfa8ff43155633cd14c90e3bdf46c082073
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-12-16 11:35:20 -0300

    Update sketch_2023_04_27.py

--- 2023/sketch_2023_04_27/sketch_2023_04_27.py
+++ 2023/sketch_2023_04_27/sketch_2023_04_27.py
@@ -1,18 +1,17 @@
+import py5
 import numpy as np
 import cv2
 
 cap = None
 py5_img = None
-camera_on = True
 
 def setup():
-    size(800, 600)
-    launch_thread(open_capture)
+    py5.size(800, 600)
+    py5.launch_thread(open_capture)
     
 def draw():
-    background(255)
-    if camera_on:
-        camera()
+    py5.background(255)
+    camera()
 
 def open_capture():
     global cap
@@ -27,7 +26,7 @@ def camera():
     if cap:
         ret, frame = cap.read()  # frame is a numpy array
     if ret:
-        resized_frame = cv2.resize(frame, (width, height))
+        resized_frame = cv2.resize(frame, (py5.width, py5.height))
         rgb_np = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
         gray = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
         edges = 255 - cv2.Canny(gray, 100, 80, 5)
@@ -42,15 +41,17 @@ def camera():
         contours2, hierarchy2 = cv2.findContours(255 - img_bw_otsu, 
             cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        py5_img = create_image_from_numpy(edges, 'L', dst=py5_img)
-        image(py5_img, 0, 0)
-        fill(0, 100, 0, 150)
+        py5_img = py5.create_image_from_numpy(edges, 'L', dst=py5_img)
+        py5.image(py5_img, 0, 0)
+        py5.fill(0, 100, 0, 150)
         draw_contours(contours)
-        fill(0, 0, 100, 150)
+        py5.fill(0, 0, 100, 150)
         draw_contours(contours2)
         
 def draw_contours(contours):
         for c in contours:
-            with begin_closed_shape():
+            with py5.begin_closed_shape():
                 for p in c:
-                    vertex(*p[0])
+                    py5.vertex(*p[0])
+
+py5.run_sketch()
