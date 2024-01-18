31c3f962df897291c1d09f1ff7693e6a3e956881
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-01-17 22:01:39 -0300

    13 15 16

--- 2024/sketch_2024_01_16/sketch_2024_01_16.py
+++ 2024/sketch_2024_01_16/sketch_2024_01_16.py
@@ -10,22 +10,24 @@ from random import randint
 space = pymunk.Space()
 space.gravity = (0, 900)
 
-# create a valley-like floor
-segment1 = pymunk.Segment(space.static_body, (0, 0), (0, 500), 5)
-segment1.elasticity = 1
-segment2 = pymunk.Segment(space.static_body, (500, 0), (500, 500), 5)
-segment2.elasticity = 1
-segment3 = pymunk.Segment(space.static_body, (0, 500), (500, 500), 5)
-segment3.elasticity = 1
-
-space.add(segment1, segment2, segment3)
 
 balls = []
 
 def setup():
-    py5.size(500, 500)
+    global segment1, segment2, segment3
+    py5.size(1000, 1000)
     py5.color_mode(py5.HSB)
-    py5_tools.animated_gif('out.gif', duration=0.1, count=30, period=.1)
+    w, h = py5.width, py5.height 
+    #py5_tools.animated_gif('out.gif', duration=0.1, count=30, period=.1)
+    segment1 = pymunk.Segment(space.static_body, (0, 0), (0, h), 15)
+    segment1.elasticity = 0
+    segment2 = pymunk.Segment(space.static_body, (w, 0), (w, h), 15)
+    segment2.elasticity = 0
+    segment3 = pymunk.Segment(space.static_body, (0, h-15), (w, h-15), 15)
+    segment3.elasticity = 0
+
+    space.add(segment1, segment2, segment3)
+
 
 def draw():
     py5.background(150)
@@ -35,28 +37,29 @@ def draw():
     py5.stroke_weight(segment1.radius*2)
     py5.line(segment1.a.x, segment1.a.y, segment1.b.x, segment1.b.y)
     py5.line(segment2.a.x, segment2.a.y, segment2.b.x, segment2.b.y)
+    py5.line(segment3.a.x, segment3.a.y, segment3.b.x, segment3.b.y)
     py5.no_stroke()
     for ball in balls:
         py5.fill(ball.radius * 8, 255, 255)
         py5.circle(ball.body.position.x, ball.body.position.y, ball.radius * 2)
 
     # advance the simulation one step
-    space.step(1/py5.get_frame_rate())
+    space.step(0.02)
 
 def create_ball(x, y):
     # create a ball
     body = pymunk.Body(mass=1, moment=10)
     body.position = x, y
-    ball = pymunk.Circle(body, radius=randint(5, 25))
-    ball.elasticity = 0.5
+    ball = pymunk.Circle(body, radius=4)
+    ball.elasticity = 0.1
     balls.append(ball)
     space.add(body, ball)
     
 def key_pressed():
     if py5.key == ' ':
-        for x in range(100, 401, 50):
-            for y in range(100, 401, 50):
+        for x in range(50, 950, 9):
+            for y in range(50, 950, 9):
                 create_ball(x + py5.random(-1, 1), y)
 
-py5.run_sketch()
+py5.run_sketch(block=False)
 
