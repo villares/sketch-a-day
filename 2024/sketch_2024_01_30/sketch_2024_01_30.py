4876e8bd9b33642a752c68cbe0000b656903b326
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-02-03 13:52:29 -0300

    Update sketch_2024_01_30.py

--- 2024/sketch_2024_01_30/sketch_2024_01_30.py
+++ 2024/sketch_2024_01_30/sketch_2024_01_30.py
@@ -1,38 +1,51 @@
 """
+You'll need the shader files ToonFrag.glsl & ToonVert.glsl in the data folder of this sketch
+
+Header from the original Processing Java example
  * Toon Shading.
  * 
  * Example showing the use of a custom lighting shader in order    
  * to apply a "toon" effect on the scene. Based on the glsl tutorial 
  * from lighthouse 3D:
  * http://www.lighthouse3d.com/tutorials/glsl-tutorial/toon-shader-version-ii/
+
+2024-02-03 - Adding some suggestions from @GoToLoop at
+https://discourse.processing.org/t/py5-loves-3d-pd3-hints-shaders/43821
 """
 
-import py5
-import trimesh
-from sdf import sphere, box, cylinder, X, Y, Z
+from pathlib import Path
 
-f = sphere(1) & box(1.5)
-c = cylinder(0.5)
-f -= c.orient(X) | c.orient(Y) | c.orient(Z)
-#f.save('out.stl')
-m = trimesh.load_mesh('out.stl')
+import py5      # Installation instructions at https://py5coding.org
+import trimesh  # `pip install trimesh[easy]` The [easy] part is optional for this sketch
+from sdf import sphere, box, cylinder, X, Y, Z    # `pip install sdf-fork`
 
-shader_enabled = True    
+STL_FILE = 'out.stl'
+
+shader_enabled = True    # left button mouse click to toggle
+lights_enabled = False   # right button mouse click to toggle
 
 def setup():
     global s
     py5.size(500, 500, py5.P3D)
+        
+    if not Path(STL_FILE).exists():
+        f = sphere(1) & box(1.5)
+        c = cylinder(0.5)
+        f -= c.orient(X) | c.orient(Y) | c.orient(Z)
+        f.save(STL_FILE)
+
+    m = trimesh.load_mesh(STL_FILE)
     s = py5.convert_shape(m)
     s.disable_style()
     
     global toon
     toon =  py5.load_shader("ToonFrag.glsl", "ToonVert.glsl")
+    py5.shader(toon)
     
 def draw():
-    if shader_enabled == True:
-        py5.shader(toon)
-    py5.background(0)
-    #py5.lights()    # breakes shader!
+    py5.background(0)    
+    if lights_enabled: # breakes shader!
+        py5.lights()    
     dirY = (py5.mouse_y / py5.height - 0.5) * 2
     dirX = (py5.mouse_x / py5.width - 0.5) * 2
     #py5.directional_light(204, 204, 204, -dirX, -dirY, -1)
@@ -48,12 +61,15 @@ def draw():
     
     
 def mouse_pressed() :
-    global shader_enabled
-    if shader_enabled:
-        shader_enabled = False
-        py5.reset_shader()
-    else:
-        shader_enabled = True
+    global shader_enabled, lights_enabled
+    if py5.mouse_button == py5.LEFT:
+        shader_enabled = not shader_enabled
+        if shader_enabled:
+            py5.shader(toon)
+        else:
+            py5.reset_shader()
+    elif py5.mouse_button == py5.RIGHT:
+        lights_enabled = not lights_enabled
 
-    
+
 py5.run_sketch()
