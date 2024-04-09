9be357b5073b58b6ad6a708fb072ad73f23d7cd9
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-04-09 11:06:23 -0300

    Create sketch_2024_04_09.py

---
+++ 2024/sketch_2024_04_09/sketch_2024_04_09.py
@@ -0,0 +1,64 @@
+
+import py5
+import shapely
+
+
+D_HANDLE = 30
+pts = [(100, 100), (200, 200), (200, 100),(500, 100), (300, 500), (200, 400)]
+drag = None
+
+def setup():
+    py5.size(600, 600)
+    py5.text_size(18)
+    
+def draw():
+    global poly
+    py5.background(240)
+    py5.no_fill()
+    
+    poly = shapely.Polygon(pts).buffer(0)
+    py5.stroke(0, 200, 0)
+    py5.shape(py5.convert_shape(poly), 0, 0)
+
+    if poly.is_valid:
+        py5.stroke(0, 0, 200)
+        b = poly.buffer(30, join_style='bevel')
+        py5.shape(py5.convert_shape(b), 0, 0)
+        b = poly.buffer(-30, join_style='bevel')
+        py5.shape(py5.convert_shape(b), 0, 0)
+
+    else:
+        py5.text('Polygon is not simple', 100, 100)
+
+    for i, (x, y) in enumerate(pts):
+        py5.fill(0)
+        py5.no_stroke()
+        py5.circle(x, y, 3)
+        py5.no_fill()
+        py5.stroke(255, 0, 0)
+        if py5.dist(py5.mouse_x, py5.mouse_y, x, y) < D_HANDLE / 2:
+            py5.no_fill()
+            py5.circle(x, y, D_HANDLE)
+
+def mouse_pressed():
+    global drag
+    for i, (x, y) in enumerate(pts):  
+        if py5.dist(py5.mouse_x, py5.mouse_y, x, y) < D_HANDLE / 2: 
+            drag = i
+            break 
+
+def mouse_released(): 
+    global drag
+    drag = None
+
+
+def mouse_dragged(): 
+    if drag is not None:
+        x, y = pts[drag]
+        x += py5.mouse_x - py5.pmouse_x
+        y += py5.mouse_y - py5.pmouse_y
+        pts[drag] = x, y
+        
+        
+py5.run_sketch(block=False)
+
