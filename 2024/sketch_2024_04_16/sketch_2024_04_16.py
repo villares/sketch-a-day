da1a4afedcaebdda6e9420ee52e6fa4dfdbd7941
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-04-16 10:16:06 -0300

    16

---
+++ 2024/sketch_2024_04_16/sketch_2024_04_16.py
@@ -0,0 +1,77 @@
+# Alexandre B A Villares - https://abav.lugaralgum.com/
+# To run this you will need py5 https://abav.lugaralgum.com/como-instalar-py5/index-EN.html
+
+from itertools import product, combinations, permutations
+
+import py5
+
+from shape import Shape
+
+grid = product((0, 1, 2), (0, 1, 2))  # 3X3
+pts = list(grid)
+space, border = 50, 25
+name = "shapes"
+
+def setup():
+    global shapes
+    py5.size(350, 250)
+    W = int(py5.width - border * 2) // space
+    H = int(py5.height - border * 2) // space
+
+    shapes = []
+    for i in [4]: # 3 to 7, as no non-intersecting shapes exist with 8 or 9 pts.
+        shapes.extend(create_shapes(pts, i))
+    shapes.sort(key=shape_area)
+    print(f'shapes: {len(shapes)} Cols: {W} Rows: {H} Visible grid: {W*H}')
+
+    f = py5.create_graphics(
+        py5.width * 10,  # for big PDF export
+        py5.height * 10,   
+        py5.PDF, name + '.pdf'
+        )
+    py5.begin_record(f) # begin PDF export
+    f.scale(10)
+    py5.stroke_join(py5.ROUND)
+    py5.background(240)
+    py5.no_stroke()
+    i = 0
+    for y, x in product(range(H), range(W)):
+        if i < len(shapes):
+            shp = shapes[i]
+            with py5.push_matrix():
+                py5.translate(border + space * x,
+                              border + space * y)
+                py5.fill(0, 0, 100)
+                shp.draw(space * 0.4)
+                py5.fill(255, 0, 0)
+            i += 1
+    py5.end_record()  # end PDF export
+    py5.save_frame(name + '.png')
+
+def create_shapes(pts, num_pts=None, allow_intersecting=False):
+    """
+    Generate all distinct shapes, simple* polygons
+    from a collection of points.
+    
+    * not self intersecting
+    """
+    num_pts = num_pts or len(pts)
+    all_shapes = list(permutations(pts, num_pts))
+    tested, shapes = set(), []
+    for poly in all_shapes:
+        s = Shape(poly)
+        if (
+            s.is_simple and
+            not s.has_colinear and
+            #s.is_valid and
+            s not in tested
+            ):
+            tested.add(s)
+            shapes.append(s)
+    return shapes
+
+                        
+def shape_area(p):
+    return p.area
+
+py5.run_sketch(block=False)
