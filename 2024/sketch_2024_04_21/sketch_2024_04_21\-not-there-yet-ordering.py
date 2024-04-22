855b344e9617ae1ea213193e9d5113f5112832a0
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-04-21 22:28:22 -0300

    21

--- 2024/sketch_2024_04_21/sketch_2024_04_21\-not-there-yet-ordering.py
+++ 2024/sketch_2024_04_21/sketch_2024_04_21\-not-there-yet-ordering.py
@@ -8,18 +8,18 @@ from shape import all_from_points
 
 grid = product((-1, 0, 1), (-1, 0, 1))  # 3X3
 pts = list(grid)
-space, border = 100, 51
+space, border = 50, 25
 
 def setup():
     global shapes
-    py5.size(500, 500)
+    py5.size(1300, 300)
     py5.background(200)
     py5.text_font(py5.create_font('Inconsolata Bold', 18))
     W = int(py5.width - border * 2) // space
     H = int(py5.height - border * 2) // space
     shapes = []
     tm = 0
-    for i in [7]: # 3 to 7, as no non-intersecting shapes exist with 8 or 9 pts.
+    for i in [3, 4, 5, 6, 7]: # 3 to 7, as no non-intersecting shapes exist with 8 or 9 pts.
         m = py5.millis()
         shapes.extend(all_from_points(pts, i)) #, remove_flipped=True))
         dt = py5.millis() - m
@@ -34,6 +34,9 @@ def setup():
             v0 = s.points[vi - 2]
             ang = py5.degrees(corner_angle(v1, v0, v2))
             a.append(-ang)
+#         print(a, s.area)
+#         if not a:
+#             a.append(0)
         return int(min(a) + s.area)
         
     shapes.sort(key=min_angles)
@@ -80,6 +83,22 @@ def corner_angle(c, a, b):
 
 def cross_sign(x1, y1, x2, y2):
     return x1 * y2 > x2 * y1
+
+# for i in range(len(points)):
+#     p1 = points[i]
+#     ref = points[i - 1]
+#     p2 = points[i - 2]
+#     x1, y1 = p1[0] - ref[0], p1[1] - ref[1]
+#     x2, y2 = p2[0] - ref[0], p2[1] - ref[1]
+# 
+#     print('Points', p1, ref, p2)
+#     print('Angle', angle(x1, y1, x2, y2))
+#     if cross_sign(x1, y1, x2, y2):
+#         print('Inner Angle')
+#     else:
+#         print('Outer Angle')
+#     print('')
+
 
 def poly_area(points):
     points = list(points)
--- docs/README.md
+++ docs/README.md
@@ -36,6 +36,16 @@ Here are listed some of the tools I have been using:
 
 ---
 
+### sketch_2024_04_21
+
+![sketch_2024_04_21](https://raw.githubusercontent.com/villares/sketch-a-day/main/2024/sketch_2024_04_21/sketch_2024_04_21.png)
+
+[sketch_2024_04_21](https://github.com/villares/sketch-a-day/tree/main/2024/sketch_2024_04_21) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2024_04_20
 
 ![sketch_2024_04_20](https://raw.githubusercontent.com/villares/sketch-a-day/main/2024/sketch_2024_04_20/sketch_2024_04_20.png)
