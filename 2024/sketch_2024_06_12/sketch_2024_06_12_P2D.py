5523d0223e5c145a6544050ee7147333df231ed1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-13 12:45:46 -0300

    P2D

---
+++ 2024/sketch_2024_06_12/sketch_2024_06_12_P2D.py
@@ -0,0 +1,20 @@
+import py5
+
+def setup():
+    global shp
+    py5.size(300, 300, py5.P2D)
+    shp = py5.load_shape('a.svg')
+    path = shp.get_children()[1].get_children()[0]
+    py5.translate(50, 100)
+    py5.shape(shp, 0,0)
+    py5.shape(path, 0, 0)
+    py5.shape(shp, 100, 0)
+    py5.shape(shp, 0, 0)
+    py5.scale(3.5)
+    path.set_stroke(255)
+    path.set_fill(py5.color(255, 0, 0, 10))
+    py5.shape(path, 0, 0)
+    py5.save('out.png')
+    
+
+py5.run_sketch(block=False)
