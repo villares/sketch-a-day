ea306d37ccac49c4842719f94a68b47d7f6feeb2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-19 00:35:26 -0300

    18

--- 2024/sketch_2024_06_18/sketch_2024_06_18.py
+++ 2024/sketch_2024_06_18/sketch_2024_06_18.py
@@ -1,52 +1,54 @@
-import builtins
+import tokenize
 
 import py5
 
 from parse_ansi_strings import parse_ansi_strings, STYLE
 
+from tokenize_helper import get_tokens, reassemble_tokens
+
 src_lines = []
 col = False
 
+
 def setup():
-    py5.size(900, 900)
+    py5.size(1000, 500)
     with open(__file__) as f: # get source text
         src_lines.extend(f.readlines())
-    styled_source = ''.join(src_lines)
-    
-    for name in dir(builtins):
-        styled_source = styled_source.replace(
-        name, STYLE['RED'] + name + STYLE['END']
-        ) 
-    print(styled_source)
     py5.background(200)
     py5.fill(0)
+    tokens = get_tokens(src_lines)
+    styles = {
+        tokenize.COMMENT: 'DARKCYAN',
+        tokenize.STRING: 'BLUE',
+        tokenize.DOCSTRING: 'PURPLE',
+        'KEYWORD': 'RED',
+        'BUILTIN': 'GREEN',
+        }
+    styled_source = reassemble_tokens(tokens, styles)   
     prepare_styled_text()
     draw_styled_text(styled_source, 20, 20, line_height=12 * 1.1)
     py5.save('out.png')
-    
+
+
 def prepare_styled_text(font_size=12):
     global font, bold_font
     font = py5.create_font("Source Code Pro Bold", font_size)
     bold_font = py5.create_font("Source Code Pro", font_size)
 
 
-# ---COL-BREAK---
 def draw_styled_text(ansi_marked_txt, x, y, font_size=12, line_height=None):
     segments = parse_ansi_strings(ansi_marked_txt)
     line_height = line_height or font_size
     draw_text_segments(segments, x, y, font_size, line_height)
 
+
 def draw_text_segments(segments, x, y, font_size, line_height):
-    col = False
     py5.text_font(font)
     py5.text_size(font_size)
     py5.fill(0)
     tx, ty = x, y
     for style, li in segments:
-        if li.startswith('# ---COL-BREAK---'):  # magic col comment
-            li = ''
-            col = True
-        elif style == 'NEWLINE':
+        if style == 'NEWLINE':
             tx = x
             ty += line_height
             continue 
@@ -64,8 +66,7 @@ def draw_text_segments(segments, x, y, font_size, line_height):
         tx += py5.text_width(li)
         if ty > py5.height - line_height or col:
             x += x + py5.width / 2
-            ty = y
-            col = False
+            ty =  y
 
 
 py5.run_sketch(block=False)
--- 2024/sketch_2024_06_18/tokenize_helper.py
+++ 2024/sketch_2024_06_18/tokenize_helper.py
@@ -1,4 +1,3 @@
-
 """
 Functions useful draw source code lines with colors,
 to get comments, and also to get multi-line (doc)strings.
@@ -6,8 +5,19 @@ to get comments, and also to get multi-line (doc)strings.
 import tokenize
 import io
 from os import path
+import builtins
 
 from parse_ansi_strings import parse_ansi_strings, STYLE
+
+KEYWORDS = """
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
+""".split() #; print(KEYWORDS)
 
 tokenize.DOCSTRING = -tokenize.STRING # some monkeying around
 
@@ -58,8 +68,15 @@ def reassemble_tokens(token_tuples, styles={}):
         # Add token text to the current line
         if tok_type == tokenize.STRING and starting_triple_quote(tok_text):
             tok_type = tokenize.DOCSTRING
-        if e := styles.get(tok_type):
-            tok_text = STYLE[e] + tok_text + STYLE['END']
+        if style := styles.get(tok_type):
+            tok_text = STYLE[style] + tok_text + STYLE['END']
+        if tok_type == tokenize.NAME and tok_text in dir(builtins):
+            if style := styles.get('BUILTIN'):
+                tok_text = STYLE[style] + tok_text + STYLE['END']
+        if tok_type == tokenize.NAME and tok_text in KEYWORDS:
+            if style := styles.get('KEYWORD'):
+                tok_text = STYLE[style] + tok_text + STYLE['END']
+ 
         current_line += tok_text
         current_column = end_col
     lines.append(current_line)
@@ -72,8 +89,88 @@ if __name__ == '__main__':
     tokens = get_tokens(src_lines)
     comments = get_comments(tokens) + get_docstrings(tokens)
     styles = {
-        tokenize.COMMENT: 'PURPLE',
+        tokenize.COMMENT: 'DARKCYAN',
         tokenize.STRING: 'BLUE',
-        tokenize.DOCSTRING: 'RED',
+        tokenize.DOCSTRING: 'PURPLE',
+        'KEYWORD': 'GREEN',
+        'BUILTIN': 'RED',
         }
     print(reassemble_tokens(tokens, styles))
+
+# import token as TOKEN
+# for token_type in dir(TOKEN):
+#     print(token_type)
+
+
+"""
+AMPER
+AMPEREQUAL
+ASYNC
+AT
+ATEQUAL
+AWAIT
+CIRCUMFLEX
+CIRCUMFLEXEQUAL
+COLON
+COLONEQUAL
+COMMA
+COMMENT
+DEDENT
+DOT
+DOUBLESLASH
+DOUBLESLASHEQUAL
+DOUBLESTAR
+DOUBLESTAREQUAL
+ELLIPSIS
+ENCODING
+ENDMARKER
+EQEQUAL
+EQUAL
+ERRORTOKEN
+EXACT_TOKEN_TYPES
+GREATER
+GREATEREQUAL
+INDENT
+ISEOF
+ISNONTERMINAL
+ISTERMINAL
+LBRACE
+LEFTSHIFT
+LEFTSHIFTEQUAL
+LESS
+LESSEQUAL
+LPAR
+LSQB
+MINEQUAL
+MINUS
+NAME
+NEWLINE
+NL
+NOTEQUAL
+NT_OFFSET
+NUMBER
+N_TOKENS
+OP
+PERCENT
+PERCENTEQUAL
+PLUS
+PLUSEQUAL
+RARROW
+RBRACE
+RIGHTSHIFT
+RIGHTSHIFTEQUAL
+RPAR
+RSQB
+SEMI
+SLASH
+SLASHEQUAL
+SOFT_KEYWORD
+STAR
+STAREQUAL
+STRING
+TILDE
+TYPE_COMMENT
+TYPE_IGNORE
+VBAR
+VBAREQUAL
+"""
