c8cb51933c63ce67675d4a3e4b10eb9635589de1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-22 16:54:29 -0300

    Update sketch_2024_06_22.py

--- 2024/sketch_2024_06_22/sketch_2024_06_22.py
+++ 2024/sketch_2024_06_22/sketch_2024_06_22.py
@@ -3,21 +3,21 @@ import numpy as np
 
 def setup():
     global opx
-    py5.size(500, 500)
+    py5.size(300, 300)
     py5.rect_mode(py5.CENTER)
-    py5.rect(250, 250, 250, 125)
+    py5.rect(150, 150, 25, 25)
     opx = py5.get_np_pixels(bands='RGB')
     
 def draw():
     ang = py5.mouse_x
     px = opx.copy()
-    A = np.tan(np.radians(ang/2)) / 10
-    B = -np.sin(np.radians(ang)) / 10
-    for y in range(-py5.height//2, py5.height//2):
+    A = np.tan(np.radians(ang/2))
+    B = -np.sin(np.radians(ang)) 
+    for y in range(0, py5.height):
         px[y] = np.roll(px[y], int(y*A) % py5.height, axis=0)
-    for x in range(-py5.width//2, py5.width//2):
-        px[:, x] = np.roll(px[:, int(x*B) % py5.width], x, axis=0)
-    for y in range(-py5.height//2, py5.height//2):
+    for x in range(0, py5.width):
+        px[:, x] = np.roll(px[:, x], int(x*B), axis=0)
+    for y in range(0, py5.height):
         px[y] = np.roll(px[y], int(y*A) % py5.height, axis=0)   
     py5.set_np_pixels(px, bands='RGB')
 
