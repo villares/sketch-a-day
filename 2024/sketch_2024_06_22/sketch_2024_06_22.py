fea3cd457d497ea004d1d807d55cff8ea0494f75
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-22 17:07:36 -0300

    22

--- 2024/sketch_2024_06_22/sketch_2024_06_22.py
+++ 2024/sketch_2024_06_22/sketch_2024_06_22.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def setup():
     global opx
-    py5.size(300, 300)
+    py5.size(500, 500)
     py5.rect_mode(py5.CENTER)
-    py5.rect(150, 150, 25, 25)
+    py5.rect(250, 250, 100, 100)
     opx = py5.get_np_pixels(bands='RGB')
     
 def draw():
@@ -14,11 +14,11 @@ def draw():
     A = np.tan(np.radians(ang/2))
     B = -np.sin(np.radians(ang)) 
     for y in range(0, py5.height):
-        px[y] = np.roll(px[y], int(y*A) % py5.height, axis=0)
+        px[y] = np.roll(px[y], int(y*A - py5.height/2) , axis=0)
     for x in range(0, py5.width):
-        px[:, x] = np.roll(px[:, x], int(x*B), axis=0)
+        px[:, x] = np.roll(px[:, x], int(x*B - py5.width/2), axis=0)
     for y in range(0, py5.height):
-        px[y] = np.roll(px[y], int(y*A) % py5.height, axis=0)   
+        px[y] = np.roll(px[y], int(y*A  - py5.height/2) % py5.height, axis=0)   
     py5.set_np_pixels(px, bands='RGB')
 
 def key_pressed():
