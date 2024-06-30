66539f30e28d6d2abe7efb2c68e6964f34f7b08d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-30 20:26:14 -0300

    30

---
+++ 2024/sketch_2024_06_30/sketch_2024_06_30.py
@@ -0,0 +1,43 @@
+import py5
+import py5_tools
+import numpy as np
+
+colors = ['red', 'green', 'blue']
+
+def setup():
+    py5.size(600, 600)
+    start()
+    py5_tools.animated_gif('out.gif', count=100, period=0.2, duration=0.2)
+
+def start():
+    global px, w, h
+    py5.background(0)
+    py5.rect_mode(py5.CENTER)
+    py5.stroke_weight(5)
+    py5.no_stroke()
+    w, h = py5.width, py5.height
+    for i, d in enumerate(range(w * 2, 0, -20)):
+        py5.fill(colors[i % 3])
+        py5.circle(w / 2, h / 2, d)
+    px = py5.get_np_pixels(bands='RGB')
+    
+def draw():
+    f = py5.frame_count // 100
+    py5.random_seed(f)
+    direction = py5.random_choice((-1, 1))
+    
+    channel = py5.random_choice((0, 1, 2))
+    y = py5.random_int(h // 50) * 50
+    px[y-100:y, :, channel] = np.roll(px[y-100:y, :, channel], direction , axis=1)
+    
+    channel = py5.random_choice((0, 1, 2))
+    x = py5.random_int(w // 50) * 50
+    px[:, x-100:x, channel] = np.roll(px[:, x-100:x, channel], direction, axis=0)
+    
+    py5.set_np_pixels(px, bands='RGB')
+
+def key_pressed():
+    py5.save_frame('###.png')
+    start()
+
+py5.run_sketch(block=False)
