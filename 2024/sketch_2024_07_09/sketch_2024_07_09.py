92a1eb7a9f05ec663e397c99a97ac5dbb891d103
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-07-09 21:31:27 -0300

    Update sketch_2024_07_09.py

--- 2024/sketch_2024_07_09/sketch_2024_07_09.py
+++ 2024/sketch_2024_07_09/sketch_2024_07_09.py
@@ -1,52 +1,33 @@
 well = {}
-piece = []
+falling_piece = []
 
-cores = {
+colors = {
     'r': color(200, 0, 0),  # red
     'g': color(0, 200, 0),  # green
-    'b': color(0, 0, 200),  # azul
-    'y': color(200, 200, 0),  # azul
+    'b': color(0, 0, 200),  # blue
+    'y': color(200, 200, 0),  # yellow
     'm': color(200, 0, 200),  # magenta
     'c': color(0, 200, 200),  # cian
     't': color(200, 200, 200),  # gray
-    'w': 128  # wall, dark gray
+    'w': 128  # well walls, dark gray
     }
 
 pieces = (
-    (((6, 0), 'r'),    # O
-     ((7, 0), 'r'),
-     ((6, 1), 'r'),
-     ((7, 1), 'r')),
-    (((4, 0), 'b'),    # J
-     ((5, 0), 'b'),
-     ((6, 0), 'b'),
-     ((6, 1), 'b')),
-    (((4, 0), 'g'),    # L
-     ((5, 0), 'g'),
-     ((6, 0), 'g'),
-     ((6, -1), 'g')),
-    (((4, 0), 'y'),    # I
-     ((5, 0), 'y'),
-     ((6, 0), 'y'),
-     ((7, 0), 'y')),
-    (((5, 0), 'c'),    # S
-     ((5, 1), 'c'),
-     ((6, 1), 'c'),
-     ((6, 2), 'c')),
-    (((6, 0), 'm'),    # N
-     ((6, 1), 'm'),
-     ((5, 1), 'm'),
-     ((5, 2), 'm')),
-    (((4, 0), 't'),    # T
-     ((5, 0), 't'),
-     ((6, 0), 't'),
-     ((5, 1), 't')),
+    (((6, 0), 'g'), ((7, 0), 'g'), ((6, 1), 'g'), ((7, 1), 'g')), # O
+    (((4, 0), 'b'), ((5, 0), 'b'), ((6, 0), 'b'), ((6, 1), 'b')), # J
+    (((4, 1), 'y'), ((5, 1), 'y'), ((6, 1), 'y'), ((6, 0), 'y')), # L
+    (((4, 0), 'r'), ((5, 0), 'r'), ((6, 0), 'r'), ((7, 0), 'r')), # I
+    (((5, 0), 'c'), ((5, 1), 'c'), ((6, 1), 'c'), ((6, 2), 'c')), # S
+    (((6, 0), 'm'), ((6, 1), 'm'), ((5, 1), 'm'), ((5, 2), 'm')), # N
+    (((4, 0), 't'), ((5, 0), 't'), ((6, 0), 't'), ((5, 1), 't')), # T
 )
 
 W, H, s = 11, 20, 25   # Well Width, Well Height, single block size
 
 def setup():
     size(525, 525)
+    text_align(CENTER, CENTER)
+    text_size(80)
     start()
     
 def start():
@@ -58,98 +39,97 @@ def start():
         well[W, y] = 'w'
     for x in range(W + 1):
         well[x, H] = 'w'
-    new_piece()
+    new_falling_piece()
+    loop() # reactivates draw if it is paused
     
 def draw():
     global score
     background(0)
-    for (x, y), b in list(well.items()) + piece:
-        fill(cores[b])
+    for (x, y), b in list(well.items()) + falling_piece:
+        fill(colors[b])
         square(x * s, y * s, s)
-        
+    fill(200)
+    text(score, W * s + (width - W * s) / 2, s * 2)        
     if game_over:
          fill(255)
-         text_align(CENTER, CENTER)
-         text_size(80)
          text('GAME OVER',
               width / 2, height / 2)
+         no_loop() # pause draw
     elif frame_count % 10 == 0:
-        if check_move(piece, 0, 1):
-            move_piece(0, 1)
+        if check_move(falling_piece, 0, 1):
+            move_falling_piece(0, 1)
         else:
             add_to_well()
-            piece[:] =[]
-            new_piece()
+            falling_piece[:] =[]
+            new_falling_piece()
         r = check_filled_row()
         if r != -1:
-            collapse_row(r)
+            collapse_on_row(r)
             score += 1
  
-def new_piece():
+def new_falling_piece():
     global game_over
-    piece[:] = random_choice(pieces)
-    if not check_move(piece):
+    falling_piece[:] = random_choice(pieces)
+    if not check_move(falling_piece):
         game_over = True
     
 def add_to_well():
-    for (x, y), b in piece:
+    for (x, y), b in falling_piece:
         well[x, y] = b
-        
+
 def check_move(p, h=0, v=0):
     for (x, y), _ in p:
         if well.get((x + h, y + v)):
             return False
     return True
 
-def move_piece(h, v):
-    piece[:] = (((x + h, y + v), b) for (x, y), b in piece)
+def move_falling_piece(h, v):
+    falling_piece[:] = (((x + h, y + v), b) for (x, y), b in falling_piece)
 
 def check_filled_row():
     for r in range(H):
-        row = 0
+        filled_blocks = 0
         for c in range(W):
             if well.get((c, r)):
-                row += 1
-        if row == W:
-            print(row, r)
-            return r
-    return -1
+                filled_blocks += 1
+        if filled_blocks == W:
+            return r  # return first filled row found
+    return -1 # no filled row
 
 def clear_row(r):
     for c in range(1, W):
         if (c, r) in well:
             del well[c, r]
 
-def collapse_row(r):
-    for row in range(r, -1, -1):
-        clear_row(row)
+def collapse_on_row(r):
+    # clear row r and move blocks down to it
+    for row in range(r, -1, -1): # moving upwards, starting from row r
+        clear_row(row) 
         for c in range(1, W):
             if b := well.get((c, row-1)):
                 well[c, row] = b
     
-def piece_centroid():
-    positions = ((x, y) for (x, y), _ in piece)
+def rounded_falling_piece_centroid():
+    positions = ((x, y) for (x, y), _ in falling_piece)
     coords = tuple(zip(*positions))
     max_x, max_y = map(max, coords)
     min_x, min_y = map(min, coords)
-    return (max_x + min_x) / 2, (max_y + min_y) / 2
+    return (max_x + min_x) // 2, (max_y + min_y) // 2
 
-def rotated_piece():
-    c = piece_centroid()
-    cx, cy = int(c[0]), int(c[1])
-    ttop = (((x - cx, y - cy), b) for (x, y), b in piece) # translated to origin piece
-    return tuple(((y + cx, -x + cy), b) for (x, y), b in ttop)
+def rotated_falling_piece():
+    cx, cy = rounded_falling_piece_centroid() # causes y displacement of -1 offset later...
+    # translated to origin falling piece copy
+    ttofpc = (((x - cx, y - cy), b) for (x, y), b in falling_piece) 
+    return tuple(((y + cx, -x + cy + 1), b) for (x, y), b in ttofpc) # note y + 1 offset
 
 def key_pressed():
     if key == ' ':
         start()
-    elif key_code == LEFT and check_move(piece, -1, 0):
-        move_piece(-1, 0)
-    elif key_code == RIGHT and check_move(piece, 1, 0):
-        move_piece(1, 0);
-    elif key_code == UP:
-        rp = rotated_piece()
-        if check_move(rp):
-            piece[:] = rp
-        
-
+    elif key_code == LEFT and check_move(falling_piece, -1, 0):
+        move_falling_piece(-1, 0)
+    elif key_code == RIGHT and check_move(falling_piece, 1, 0):
+        move_falling_piece(1, 0);
+    elif key_code == DOWN and check_move(falling_piece, 0, 1):
+        move_falling_piece(0, 1);
+    elif key_code == UP and check_move(rp := rotated_falling_piece()):
+            falling_piece[:] = rp
