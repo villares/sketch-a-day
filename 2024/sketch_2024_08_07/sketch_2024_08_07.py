a6a9f8fcf8ba7282ca1fbbbb9cbd756687027c55
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-08-07 20:49:58 -0300

    8

--- 2024/sketch_2024_08_07/sketch_2024_08_07.py
+++ 2024/sketch_2024_08_07/sketch_2024_08_07.py
@@ -24,57 +24,20 @@ def poly(x, y, r, n=8, rot=py5.radians(30)):
         sx = x + py5.cos(i * py5.TWO_PI / n + rot) * r
         sy = y + py5.sin(i * py5.TWO_PI / n + rot) * r
         poly_pts.append((sx, sy))
-    for a, b, c in triangulate(poly_pts, (x, y)):
-        xa, ya = a
-        xb, yb = b
-        xc, yc = c
-        mab = (xa + xb) / 2, (ya + yb) / 2
-        mbc = (xc + xb) / 2, (yc + yb) / 2
-        xct, yct = line_intersect((c, mab), (mbc, a))
-        for sa, sb, sc in triangulate((a, b, c), (xct, yct)):
+    for a, b, c in triangulate(poly_pts):
+        for sa, sb, sc in triangulate((a, b, c)):
             py5.triangle(*sa, *sb, *sc)
             
         
-def triangulate(pts, center):
-    xc, yc = center
+def triangulate(pts):
+    xs, ys = tuple(zip(*pts))
+    xc, yc = sum(xs) / len(xs), sum(ys) / len(ys)
     triangles = []
     for i, (x, y) in enumerate(pts):
         x0, y0 = pts[i-1]
         triangles.append(((x, y), (x0, y0), (xc, yc)))
     return triangles
 
-    
-def line_intersect(*args, **kwargs):
-    """
-    Adapted from Bernardo Fontes https://github.com/berinhard/sketches/
-    """
-    as_Py5Vector = kwargs.get('as_Py5Vector', False)
-    in_segment = kwargs.get('in_segment', True)
-    
-    if len(args) == 2:  # expecting 2 Line objects or 2 tuples of 2 point tuples.
-        (x1, y1), (x2, y2) = args[0]
-        (x3, y3), (x4, y4) = args[1]
-    elif len(args) == 4:
-        (x1, y1), (x2, y2) = args[:2]
-        (x3, y3), (x4, y4) = args[2:]
-    elif len(args) == 8:
-        x1, y1, x2, y2, x3, y3, x4, y4 = args
-    else:
-        raise ValueError("line_intersect requires 2 lines, 4 points or 8 coords.")
-            
-    divisor = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-    if divisor:
-        uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / divisor
-        uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / divisor
-    else:
-        return None
-    if not in_segment or 0 <= uA <= 1 and 0 <= uB <= 1:
-        x = x1 + uA * (x2 - x1)
-        y = y1 + uA * (y2 - y1)
-        return Py5Vector(x, y) if as_Py5Vector else (x, y)
-    else:
-        return None
-    
         
 def key_pressed():
     py5.save('out.png')
