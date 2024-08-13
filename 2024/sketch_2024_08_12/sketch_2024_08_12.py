dd4ba30d76537eb62202970f764fab28e53716a0
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-08-13 08:37:50 -0300

    Update sketch_2024_08_12.py

--- 2024/sketch_2024_08_12/sketch_2024_08_12.py
+++ 2024/sketch_2024_08_12/sketch_2024_08_12.py
@@ -1,3 +1,11 @@
+"""
+Sketch adapting code from https://networkx.org/documentation/stable/auto_examples/graphviz_layout/plot_circular_tree.html
+Note: "This example needs Graphviz and PyGraphviz"
+
+I had trouble at first pip installing graphviz and pygraphviz but managed
+to install, via pacman, on Manjaro, python-graphviz and python-pygraphviz
+and then installed in my local env with pip
+"""
 import py5
 
 import networkx as nx
