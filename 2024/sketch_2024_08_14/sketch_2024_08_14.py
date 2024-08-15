27623e1b8f636642ec7e7ec90c992b194842e80d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-08-15 16:02:14 -0300

    14...

--- 2024/sketch_2024_08_14/sketch_2024_08_14.py
+++ 2024/sketch_2024_08_14/sketch_2024_08_14.py
@@ -8,14 +8,14 @@ from py5_tools import animated_gif
 
 import networkx as nx
 
-
 margin = 50
+s = 100 # seed
 G = nx.balanced_tree(4, 5)
 
 def setup():
     global pos
     py5.size(800, 800, py5.P3D)
-    pos = nx.spring_layout(G, dim=3, seed=100)
+    pos = nx.spring_layout(G, dim=3, seed=s)
     #pos = nx.spiral_layout(G, dim=2)
     pos = resize_layout(pos)
     animated_gif('out.gif', frame_numbers=range(1, 361, 10), duration=0.2, optimize=False)
@@ -27,14 +27,14 @@ def draw():
     py5.background(200, 200, 150)
     py5.stroke(0)
     py5.stroke_weight(1)
-    py5.lines(pos[a] + pos[b] for a, b in G.edges)  # better!
+    py5.lines(pos[a] + pos[b] for a, b in G.edges)  # draw edges
     py5.stroke(0, 0, 200)
     py5.stroke_weight(5)
-    py5.points(pos[n] for n in G.nodes)
+    py5.points(pos[n] for n in G.nodes)  # draw nodes
     py5.pop_matrix()
     py5.fill(255)
     py5.text_size(margin / 2)
-    py5.text('G = nx.balanced_tree(4, 5)\npos = nx.spring_layout(G, dim=3, seed=1)', margin, margin)
+    py5.text(f'G = nx.balanced_tree(4, 5)\npos = nx.spring_layout(G, dim=3, seed={s})', margin, margin)
 
 def resize_layout(pos):
     """ Resize the positions dict layout to screen a and center on origin. """
