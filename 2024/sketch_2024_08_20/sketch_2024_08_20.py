e9fc905bd453a6e536e9c08678eba62079d34d01
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-08-20 12:24:33 -0300

    20

--- 2024/sketch_2024_08_20/sketch_2024_08_20.py
+++ 2024/sketch_2024_08_20/sketch_2024_08_20.py
@@ -4,9 +4,10 @@ import py5
 a = np.array([150, 10, 10, 250])
 b = np.array([20, 20, 250, 100]) 
 t = np.linspace(0, 1, 20)
-a_expanded = a[:, np.newaxis]
-b_expanded = b[:, np.newaxis]
-r = a_expanded + (b_expanded - a_expanded) * t
+# a_expanded = a[:, np.newaxis]  # np.newaxis is an 
+# b_expanded = b[:, np.newaxis]  # alias for None!
+# r = a_expanded + (b_expanded - a_expanded) * t
+r = py5.lerp(a[:,np.newaxis], b[:,np.newaxis], t)
 
 def setup():
     py5.size(300, 300)
