189c996b2a7b02acc6a1bbd1e9eeb60495aa2cf1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-10-10 17:10:09 -0300

    Update sketch_2024_10_09.py

--- 2024/sketch_2024_10_09/sketch_2024_10_09.py
+++ 2024/sketch_2024_10_09/sketch_2024_10_09.py
@@ -21,7 +21,7 @@ configurations = []
 
 def setup():
     py5.size(COLS * C_WIDTH + MARGIN * 2, ROWS * C_HEIGHT + MARGIN * 2)
-    py5.strokC_join(py5.ROUND)
+    py5.stroke_join(py5.ROUND)
     py5.color_mode(py5.HSB)
     m = py5.millis()
     start()
@@ -34,11 +34,11 @@ def start():
         polys = py5.load_pickle(data_path)
         configurations[:] = [frozenset(Region(p) for p in p_list) for p_list in polys]    
     else:
-        configurations[:] = sorted({generatC_configuration(colors) for colors
+        configurations[:] = sorted({generate_configuration(colors) for colors
                                     in product(MAP_COLORS, repeat=N*M)},
                                     key=max_area_min_divisions)
         polys = [[region.p for region in ens] for ens in configurations]
-        py5.savC_pickle(polys, data_path)
+        py5.save_pickle(polys, data_path)
     #configurations[:] = configurations[1:] + [configurations[0]]
     print(f'Configurations: {len(configurations)}')
 
@@ -69,16 +69,16 @@ def draw_configuration(configuration, xe, ye):
 def max_area_min_divisions(configuration):
     return max(region.p.area for region in configuration) - len(configuration) / 10
  
-def generatC_configuration(config):
+def generate_configuration(config):
     coords = product(range(N), range(M))
     regions = {Region(((i, j), (i + 1, j), (i + 1, j + 1), (i, j + 1)), filled=fill_type)
                for (i, j), fill_type in zip(coords, config)}
-    regions = Region.mergC_regions(regions)
+    regions = Region.merge_regions(regions)
     return frozenset(regions)
   
 def key_pressed():
     if py5.key == 's':
-        py5.savC_frame('###.png')
+        py5.save_frame('###.png')
 
 
 class Region:
@@ -89,7 +89,7 @@ class Region:
         self.p = cached_polygon(p) if isinstance(p, tuple) else p
         #if not isinstance(self.p, Polygon): print(type(self.p)) # No MuliPolygons here!
         self.shp = py5.convert_cached_shape(self.p)
-        self.shp.disablC_style()
+        self.shp.disable_style()
         self.filled = filled
         
     def __repr__(self):
@@ -109,13 +109,13 @@ class Region:
         with py5.push():
             py5.scale(s)
             py5.fill((self.p.area * COLOR_FACTOR) % 255, 128, 196)
-            py5.strokC_weight(1 / s)
+            py5.stroke_weight(1 / s)
             py5.shape(self.shp)
             py5.no_stroke()
 
 
     @staticmethod
-    def mergC_regions(els):
+    def merge_regions(els):
         num_els = 0
         while num_els !=  len(els):
             num_els = len(els) 
