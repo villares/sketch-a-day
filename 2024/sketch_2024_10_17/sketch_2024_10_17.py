72ba3c62f9780c6a93a322aac25607bff15c7cb0
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-10-17 22:24:30 -0300

    17

---
+++ 2024/sketch_2024_10_17/sketch_2024_10_17.py
@@ -0,0 +1,73 @@
+
+import py5  # check out https://github.com/py5coding 
+
+walkers = []
+STEP = 10
+N = 1
+
+def setup():
+    global img
+    py5.size(600, 600, py5.P2D)
+    img = py5.create_graphics(py5.width, py5.height)
+    with img.begin_draw():
+        img.stroke_weight(10)
+        img.fill(255, 0, 0)
+        img.circle(300, 300, 550)
+    #py5.image(img, 0, 0)
+    #py5.stroke_weight(3)
+    start()
+    
+def start():
+    for _ in range(50):
+        walkers.append(Walker(260 + py5.random_int(-N, N) * STEP,
+                              260 + py5.random_int(-N, N) * STEP,))
+
+def draw():
+    py5.background(200)
+    for _ in range(1):
+        for w in walkers.copy():
+            w.draw()
+            w.move()
+    print(len(walkers))
+
+def key_pressed():
+    if py5.key == 's':
+        py5.save_frame('###.png')
+    elif py5.key == ' ':
+        walkers.clear()
+        start()
+     
+class Walker():
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.history = []
+        self.history_set = set()
+        self.stuck = 0
+     
+    def move(self):
+         xs = self.x + py5.random_choice((-STEP, 0, STEP))
+         ys = self.y + py5.random_choice((-STEP, 0, STEP))
+         for other in walkers:
+            if other != self:
+                 if (xs, ys) in other.history_set:
+                     self.stuck += 1
+                     break
+         else:  # !!! when for was not interrupted !!!
+             if img.get_pixels(xs, ys) == py5.color(255, 0, 0):
+                 self.history.append((self.x, self.y))
+                 self.history_set.add((self.x, self.y))
+                 self.x = xs
+                 self.y = ys
+         #if self.stuck > 5:
+         #    walkers.remove(self)
+         
+         
+    def draw(self):
+         for i, (x, y) in enumerate(self.history[1:], 1):
+             px, py = self.history[i - 1]
+             py5.line(px, py, x, y)
+             
+             
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -32,6 +32,26 @@ Here are listed some of the tools I have been using more recently:
 
 ---
 
+### sketch_2024_10_17
+
+![sketch_2024_10_17](https://raw.githubusercontent.com/villares/sketch-a-day/main/2024/sketch_2024_10_17/sketch_2024_10_17.png)
+
+[sketch_2024_10_17](https://github.com/villares/sketch-a-day/tree/main/2024/sketch_2024_10_17) [[py5](https://py5coding.org/)]
+
+
+
+---
+
+### sketch_2024_10_16
+
+![sketch_2024_10_16](https://raw.githubusercontent.com/villares/sketch-a-day/main/2024/sketch_2024_10_16/sketch_2024_10_16.png)
+
+[sketch_2024_10_16](https://github.com/villares/sketch-a-day/tree/main/2024/sketch_2024_10_16) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2024_10_15
 
 ![sketch_2024_10_15](https://raw.githubusercontent.com/villares/sketch-a-day/main/2024/sketch_2024_10_15/sketch_2024_10_15.png)
