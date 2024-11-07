f7a6e52bb3979a847e9ced1c39c35901f1388346
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-11-07 10:25:27 -0300

    Update sketch_2024_11_06.py

--- 2024/sketch_2024_11_06/sketch_2024_11_06.py
+++ 2024/sketch_2024_11_06/sketch_2024_11_06.py
@@ -11,38 +11,54 @@ def draw():
     no_fill()
     stroke(255)
     R = 100
-    r = R * sqrt(3) / 2
-    rbh =  r * sqrt(2)
-    hex_unit(+r, -R * 3 / 2, rbh, False)
-    hex_unit(-r, -R * 3 / 2, rbh, False)
-    hex_unit(+r, +R * 3 / 2, rbh, False)
-    hex_unit(-r, +R * 3 / 2, rbh, False)
-    hex_unit(0, 0, rbh, False)
-    hex_unit(+r * 2, 0, rbh, False)
-    hex_unit(-r * 2, 0, rbh, False)
+    w = R * sqrt(3)
+    h = R * 1.5
+    main_radius = w / 2 * sqrt(2)
+
+    rows = 3
+    cols = 3
+    for row in range(rows):
+        y = (row - 1) * h
+        for col in range(cols):
+            x = (col - 1) * w
+            if row % 2 == 1:
+                x += w / 2
+            hex_unit(x, y, main_radius)
+    
+
+#     r = R * sqrt(3) / 2
+#     hex_unit(+r, -R * 3 / 2, main_radius)
+#     hex_unit(-r, -R * 3 / 2, main_radius)
+#     hex_unit(+r, +R * 3 / 2, main_radius)
+#     hex_unit(-r, +R * 3 / 2, main_radius)
+#     hex_unit(0, 0, main_radius)
+#     hex_unit(+r * 2, 0, main_radius)
+#     hex_unit(-r * 2, 0, main_radius)
    
 
-def hex_unit(x, y, r, pointy):
-    unit(x, y, 2  * r / sqrt(2) / sqrt(3), not pointy)
-    hexagon(x, y, r, pointy)
-
-
-def unit(xu, yu, ru, pointy=True, N=2, M=4): 
-    vs = hex_points(xu, yu, ru, pointy=pointy)
-    for x, y in vs[N:M]:
-        hexagon(x, y, ru / 2, pointy=pointy)
+def hex_unit(xu, yu, ru, pointy=False):
+    draw_hexagon(xu, yu, ru, pointy)  # big hexagon
+    sur = 2  * ru / sqrt(2) / sqrt(3)
+    vs = hexagon_points(xu, yu, sur, not pointy)
+    for x, y in vs[2:4]:
+        draw_hexagon(x, y, sur / 2, not pointy)  # smaller hexagons
         
-def hexagon(x, y, r, pointy=True):
-    vs = hex_points(x, y, r, pointy)
+def draw_hexagon(x, y, r, pointy=True):
+    vs = regular_polygon_points(x, y, r, 6, pointy)
     with begin_closed_shape():
             vertices(vs)
 
-def hex_points(x, y, cr, pointy=True):
+def hexagon_points(x, y, cr, pointy=True):
+    # returns a list of tuples for the coords of vertices of hexagon
+    return regular_polygon_points(x, y, cr, 6, pointy)
+
+def regular_polygon_points(x, y, cr, n, pointy=True):
     # cr is the circumradius
-    ang = TAU / 6
-    return [(x+cos(i * ang+pointy * ang / 2) * cr,
-             y+sin(i * ang+pointy * ang / 2) * cr)
-           for i in range(6)]
+    ang = TAU / n
+    return [(x+cos(i * ang + pointy * ang / 2) * cr,
+             y+sin(i * ang + pointy * ang / 2) * cr)
+           for i in range(n)]
+
 
 def key_pressed():
     if key == 'S':
