dc1d2be4ce61feae21b6afe52a2edbed4e87e21b
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-11-16 01:09:56 -0300

    Update sketch_2024_11_15.py

--- 2024/sketch_2024_11_15/sketch_2024_11_15.py
+++ 2024/sketch_2024_11_15/sketch_2024_11_15.py
@@ -1,26 +1,23 @@
 import py5
 
-from py5 import Py5Vector as V
-
-sqrt2 = py5.sqrt(2)
+from py5 import Py5Vector as Vector
     
 def setup():
     py5.size(500, 500)
     py5.background(0, 0, 200)
     py5.translate(200, 100)
     py5.stroke_weight(5)
-    start = V(0, 0)
+    current_pos = Vector(0, 0)
     heading = 0
-    pts = [start]
-    seg_size = 80
+    segment_size = 80
+    pts = [current_pos]
     for i in range(15):
         if i % 2 == 0:
             heading += py5.radians(-45)
         else:
             heading += py5.radians(90)
-        seg = V.from_heading(heading) * seg_size
-        start = start + seg
-        pts.append(start)
+        current_pos = current_pos + Vector.from_heading(heading) * segment_size
+        pts.append(current_pos)
     draw_poly(pts)        
 
 def draw_poly(pts):
