6f2bfe98eb1ff39a1e2e640ee2a113455e7ad89a
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-11-19 00:24:40 -0300

    18

---
+++ 2024/sketch_2024_11_18/sketch_2024_11_18.py
@@ -0,0 +1,115 @@
+import py5
+
+from py5 import Py5Vector as Vector
+  
+SQRT2 = py5.sqrt(2)
+
+shapes = []
+
+def setup():
+    global current_pos, heading
+    py5.size(500, 500)
+    py5.background(0, 0, 200)
+    py5.translate(250, 250)
+    py5.stroke_weight(2)
+    py5.no_fill()
+    current_pos = Vector(0, 0)
+    heading = 0
+    star_side = 30
+    octagon_side = star_side * 2 + star_side * SQRT2
+    forward(-star_side * SQRT2 / 2)
+    turn(-90)
+    forward(octagon_side / 2)
+    set_heading(0)
+    star = []
+    for i in range(16):
+        star.append(current_pos)
+        if i % 2 == 0:
+            turn(-45)
+            shapes.append(square_pts(star_side))
+        else:
+            shapes.append(triangle_pts(octagon_side / SQRT2))
+#             if i == 1:
+#                 shapes.append(kite_pts(octagon_side / SQRT2))
+            turn(90)
+        forward(star_side)
+    shapes.insert(0, star)
+    forward(-star_side)
+    turn(-90)
+    forward(octagon_side / SQRT2)
+    
+    set_heading(0)
+    octagon = []
+    for i in range(8):
+        forward(octagon_side)
+        turn(360 / 8)
+        octagon.append(current_pos)
+
+    draw_poly(octagon)
+    
+    
+    for i, shp in enumerate(shapes):
+        py5.fill(255 - i * 16, 100)
+        draw_poly(shp)
+
+    py5.save(__file__[:-3] + '.png')
+   
+def forward(d):
+    global current_pos
+    current_pos = current_pos.copy + (Vector.from_heading(heading) * d)
+
+def set_heading(deg):
+    global heading
+    heading = py5.radians(deg)
+
+def turn(deg):
+    global heading
+    heading += py5.radians(deg)
+
+def draw_poly(pts):
+    with py5.begin_closed_shape():
+        py5.vertices(pts)
+
+def triangle_pts(d):
+    pts = []
+    pts.append(current_pos)
+    forward(d)
+    pts.append(current_pos)
+    turn(-90-45)
+    forward(d * SQRT2)
+    pts.append(current_pos)
+    turn(-90-45)
+    forward(d)
+    turn(-90)
+    return pts
+
+# def kite_pts(d):
+#     pts = []
+#     turn(90)
+#     pts.append(current_pos)
+#     forward(d/2)
+#     turn(-45)
+#     pts.append(current_pos)
+#     forward(d/2)
+#     turn(-90)
+#     pts.append(current_pos)
+#     forward(d)
+#     pts.append(current_pos)
+#     turn(-90-45)
+#     forward(d)
+#     turn(-90)
+#     turn(-90)
+#     return pts
+
+def square_pts(d):
+    pts = []
+    for _ in range(4):
+        pts.append(current_pos)
+        forward(d)
+        turn(90)
+    return pts
+
+    
+
+py5.run_sketch(block=False)
+
