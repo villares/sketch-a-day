c98955bf68ae21fba4ce6e4c858092d2410da99d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-11-26 23:41:18 -0300

    26

---
+++ 2024/sketch_2024_11_26/sketch_2024_11_26.py
@@ -0,0 +1,21 @@
+
+def setup():
+    global img_c
+    size(446, 454)
+    img_a = load_image('xfce.png')
+    img_b = load_image('manjaro.png')
+    img_c = create_graphics(width, height)
+    H = 10
+    img_c.begin_draw()
+    for y in range(0, height, H * 2):
+        ra = img_a.get_pixels(0, y, width, H)
+        image(ra, 0, y)
+        rb = img_b.get_pixels(0, y + H, width, H)
+        image(rb, 0, y + H)
+    img_c.end_draw()
+    
+def draw():
+    image(img_c, 0, 0)
+    
+    #save('out.png')
+
