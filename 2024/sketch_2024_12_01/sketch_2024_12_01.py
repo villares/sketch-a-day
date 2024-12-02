3f709e59f790683be34a7a2b50c9134cbc5990cd
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-12-01 23:40:31 -0300

    Update sketch_2024_12_01.py

--- 2024/sketch_2024_12_01/sketch_2024_12_01.py
+++ 2024/sketch_2024_12_01/sketch_2024_12_01.py
@@ -1,18 +1,24 @@
+# Some ideas from:
+# https://forum.processing.org/two/discussion/20499/moving-a-translucent-window-around-the-screen/p1
+
 import py5
+from javax.swing import JRootPane
 
 drag = None
 
 def setup():
     global surface, f
     py5.size(500, 500)
-    #py5.window_resizable(True)
+    py5.window_resizable(True)
+
     surface = py5.get_surface()
     f = surface.get_native().getFrame()
     f.removeNotify()
     f.setUndecorated(True)
-    f.setOpacity(0.5)
     f.addNotify()
-    
+    #f.getRootPane().setWindowDecorationStyle(JRootPane.FRAME) # .NONE
+    f.setOpacity(0.5)
+
 def draw():
     #py5.background(255, 0, 0)
     py5.circle(py5.width / 2, py5.height / 2, py5.width)
@@ -20,15 +26,16 @@ def draw():
 
 def mouse_pressed():
     global drag
-    drag = py5.mouse_x, py5.mouse_y, py5.window_x, py5.window_y
+    drag = True
+
+def mouse_dragged():
+    if drag:
+        dx = int((py5.mouse_x - py5.pmouse_x) * 0.90)
+        dy = int((py5.mouse_y - py5.pmouse_y) * 0.90)
+        py5.window_move( f.location().x + dx,  f.location().y + dy)
         
 def mouse_released():
     global drag
-    if drag:
-        px, py, wx, wy = drag
-        dx = py5.pmouse_x - px
-        dy = py5.pmouse_y - py
-        surface.set_location(wx + dx , wy + dy)
-        drag = None
+    drag = False
     
 py5.run_sketch(block=False)
