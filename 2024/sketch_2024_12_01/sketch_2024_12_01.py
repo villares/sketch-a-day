64a9f38d5e8ee219f6485dc1478e2935ecd6e85c
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-12-02 00:29:02 -0300

    Update sketch_2024_12_01.py

--- 2024/sketch_2024_12_01/sketch_2024_12_01.py
+++ 2024/sketch_2024_12_01/sketch_2024_12_01.py
@@ -32,7 +32,11 @@ def mouse_dragged():
     if drag:
         dx = int((py5.mouse_x - py5.pmouse_x) * 0.90)
         dy = int((py5.mouse_y - py5.pmouse_y) * 0.90)
-        py5.window_move( f.location().x + dx,  f.location().y + dy)
+        #I should be able to use py5.window_x and _y but it didn't work
+        #py5.window_move(py5.window_x + dx,  py5.window_y + dy)
+        py5.window_move(f.location().x + dx,  f.location().y + dy)
+        print(f.location().x, f.location().y, py5.window_x, py5.window_y)
+
         
 def mouse_released():
     global drag
