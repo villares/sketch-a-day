677eba5a43ef9ba4a0029de3f52621a424f40699
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-12-03 22:19:58 -0300

    3

--- 2024/sketch_2024_12_03/sketch_2024_12_03.py
+++ 2024/sketch_2024_12_03/sketch_2024_12_03.py
@@ -1,39 +1,41 @@
-escala_noise = 0.01
+escala_noise = 0.02
 z = 0
-tam = 8
+tam = 6
 
 def setup():
     size(1200, 600)
     no_stroke()
 
 def draw():
-    background(50, 0, 150)
+    background(60, 0, 150)
     cols =  int(width / tam)
     rows =  int(width / tam)
     translate(width / 2, height / 2)
     for c in range(cols // 2):
         for r in range(rows // 2):
-            on = os_noise(
+            n = os_noise(
                 (mouse_x + c) * escala_noise,
                 (mouse_y + r) * escala_noise,
                  z * escala_noise
                 )
-            n = 1 + sin(on * 3)
-            #n = (((nn + 1) * 2) % 2) / 2 - 1
-            if n > 0.02:
-                d = tam - tam * n
-            else:
-                d = max(0, tam - tam * abs(10 * n))
-                fill(240, 240, 100, 200)
-                circle(tam / 2 + c * tam,
-                       tam / 2 + r * tam, d)
-                circle(-tam / 2 - c * tam,
-                       tam / 2 + r * tam, d)
-                circle(-tam / 2 - c * tam,
-                       -tam / 2 - r * tam, d)
-                circle(tam / 2 + c * tam,
-                       -tam / 2 -r * tam, d)
             
+            f = 1.2
+            d = (tam * f - abs(sin(-1 + n * PI)) * tam)
+            if n > 0.1:
+                fill(240, 240, 100)
+            else:
+                fill(20, 0, 100)
+                #fill(200, 0, 0, 100)
+#                d = max(0, tam - tam * abs(10 * n)) * f
+            circle(tam / 2 + c * tam,
+                   tam / 2 + r * tam, d)
+            circle(-tam / 2 - c * tam,
+                   tam / 2 + r * tam, d)
+            circle(-tam / 2 - c * tam,
+                   -tam / 2 - r * tam, d)
+            circle(tam / 2 + c * tam,
+                   -tam / 2 -r * tam, d)
+        
 def key_pressed():
     global z
     if key_code == UP:
