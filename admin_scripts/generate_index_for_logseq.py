e93469f569e89a7dcaf33e206b34fe5b0dfdc989
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-12-23 22:14:11 -0300

    Update generate_index_for_logseq.py

--- admin_scripts/generate_index_for_logseq.py
+++ admin_scripts/generate_index_for_logseq.py
@@ -2,13 +2,28 @@
 
 # Prepares page for logseq
 
+import re
 from pathlib import Path
 
 input_path = '/home/villares/GitHub/sketch-a-day/docs/{}.md'
 output_path = '/home/villares/GitHub/logseq/pages/sketch-a-day-{}.md'
 
 
-def main(min_year=2018, max_year=2024):
+def sketch_date(line):
+    # Define the regex patterns 
+    p6 = r'sketch_\d{6}' # sketch_NNNNNN
+    p8 = r'sketch_\d{4}_\d{2}_\d{2}' # sketch_NNNN_NN_NN
+    # If a match is found, return the matched string
+    if match := re.search(p6, line):
+        d = match.group(0)[7:]
+        return '20' + d[0:2], d[2:4], d[4:6]
+    elif match := re.search(p8, line):
+        d = match.group(0)[7:7+10]
+        return d.split('_')   
+    else:
+        return None
+
+def generate_sketch_a_day_index(min_year=2018, max_year=2024):
     for s_year in range(min_year, max_year + 1):
         s_year = str(s_year)
         # open the source markdown page from the sketch-a-day repo
@@ -16,11 +31,9 @@ def main(min_year=2018, max_year=2024):
             readme_as_lines = readme.readlines()
         # generate the target markdown page for logseq
         with open(output_path.format(s_year), 'wt') as readme:
+            writing = False
             for line in readme_as_lines:
-                if '![' in line:
-                    line = line.replace('https://raw.githubusercontent.com/villares/sketch-a-day/main/',
-                                        '/home/villares/GitHub/sketch-a-day/')
-                if r'\| 2' in line: ### {}\n\n'.format(line[2:line.find(']')])
+                if r'.md) \| [<b>2' in line : ### {}\n\n'.format(line[2:line.find(']')])
                     index_links = [] 
                     for y in range(min_year, max_year + 1):
                         if str(y) == s_year:
@@ -28,8 +41,20 @@ def main(min_year=2018, max_year=2024):
                         else:
                             index_links.append(f"[{y}](sketch-a-day-{y})")
                     readme.write(r' \| '.join(index_links))
-                else:
+                    writing = True
+                elif writing:
+                    if line.startswith('###') and (date_tuple := sketch_date(line)):
+                        line = line[:-1] + ' [[{}-{}-{}]]\n'.format(*date_tuple)
+                    elif '![' in line:
+                        line = '' + line.replace(
+                            'https://raw.githubusercontent.com/villares/sketch-a-day/main/',
+                            '/home/villares/GitHub/sketch-a-day/')
+                    elif 'https://github.com/villares/sketch-a-day/tree/' in line:
+                        line = '' + (line.replace('https://github.com/villares/',
+                                            'file:///home/villares/GitHub/')
+                                            .replace('tree/main/', '')
+                                            .replace('tree/master/', ''))
                     readme.write(line)
 
 if __name__ == '__main__':
-    main()
+    generate_sketch_a_day_index()
