3490ea06461db273fd012df5b2f702970275e8a2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-01-01 22:45:45 -0300

    Update add_year.py

--- admin_scripts/one_off/add_year.py
+++ admin_scripts/one_off/add_year.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 base_path = Path('/home/villares/GitHub/sketch-a-day/docs/')
-   
-template = (
+
+# Basic form of the new index of year links
+template = (  
 r"[<b>2025</b>](README.md) "
 r"\| [<b>2024</b>](2024.md) "
 r"\| [<b>2023</b>](2023.md) "
@@ -12,31 +13,32 @@ r"\| [<b>2020</b>](2020.md) "
 r"\| [<b>2019</b>](2019.md) "
 r"\| [<b>2018</b>](2018.md) "
 )
-
+# String used to find the year of the page being processed
 year_format = "[<b>{0}</b>]({0}.md)".format
-
-def index_line(line: str) -> bool:
-    return (r'\| [<b>' in line and
-            '2018' in line and
-            '2019' in line)
    
 def main(n):
     page = base_path / f'{n}.md'
     if not page.is_file():
         print(f'{page} not there.')
         return
+    # read page
     with open(page, 'rt') as readme:
         readme_as_lines = readme.readlines()
-
+    # overwrite with modifications
     with open(page, 'wt') as readme:
         for line in readme_as_lines:
-            print(f'Processing {n}.') 
-            if index_line(line):
+            if is_year_links_line(line):
                 line = template.replace(year_format(n), str(n))
                 line = line.replace(f'[<b>{n}</b>](README.md)', str(n))  # current year case
             readme.write(line)
+    print(f'{page} processed.') 
+
+def is_year_links_line(line: str) -> bool:
+    """A dirty check to find the index-links line instances."""
+    return (r'\| [<b>' in line and
+            '2018' in line and
+            '2019' in line)
 
 if __name__ == '__main__':
-    for y in range(2018, 2030):
+    for y in range(2018, 2029):
         main(y)
-    
