ff6e82217820af82b0ffbab1a63c62fde7b9b57d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-01-10 21:02:28 -0300

    removing join and dir list, moving to pathlib

--- admin_scripts/image_helpers.py
+++ admin_scripts/image_helpers.py
@@ -1,8 +1,7 @@
-from os import listdir
-from os.path import join
-import PIL.Image
 import io
+from pathlib import Path
 
+import PIL.Image
 
 def image_as_png_bytes(file_path, resize=None):
     """
@@ -31,7 +30,7 @@ def get_image_names(base, folder, word=None):
     Use word='' to get any images, irrespective of name.
     """
     word = word if word is not None else folder
-    contents = listdir(join(base, folder))
+    contents = Path(base, folder).iterdir()
     image_files = [f for f in contents if is_img_ext(f) and word in f]
     return image_files
 
@@ -48,22 +47,18 @@ def is_img_ext(file_name):
     )
 
 def remove_transparency(im, bg_colour=(255, 255, 255)):
-    from PIL import Image
-
     # Only process if image has transparency (http://stackoverflow.com/a/1963146)
     if im.mode in ('RGBA', 'LA') or (
         im.mode == 'P' and 'transparency' in im.info
     ):
-
         # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
         alpha = im.convert('RGBA').split()[-1]
 
         # Create a new background image of our matt color.
         # Must be RGBA because paste requires both images have the same format
         # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
-        bg = Image.new('RGBA', im.size, bg_colour + (255,))
+        bg = PIL.Image.new('RGBA', im.size, bg_colour + (255,))
         bg.paste(im, mask=alpha)
         return bg
-
     else:
         return im
