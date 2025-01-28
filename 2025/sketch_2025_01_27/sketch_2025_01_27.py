254808225f045fcba36147826d6237f273d9d863
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-01-27 23:38:08 -0300

    27

--- 2025/sketch_2025_01_27/sketch_2025_01_27.py
+++ 2025/sketch_2025_01_27/sketch_2025_01_27.py
@@ -3,43 +3,51 @@ from itertools import product
 from shapely import Polygon
 import py5
 
+B = 6
+
 def setup():
     py5.size(600, 600)
-    py5.color_mode(py5.HSB)
+    py5.stroke_weight(1.5)
+    py5.stroke_join(py5.ROUND)
     start()
+    grow()
+    shrink()
+    grow()
     
 def start():
     global ps
-    p = Polygon([(x, y) for x, y
+    pa = Polygon([(x, y) for x, y
                  in product(range(-200, 201, 100),
-                            repeat=2)]).buffer(0)
-    ps = [p]
+                            repeat=2)]).buffer(1)
+    pb = Polygon([(-x, -y) for x, y
+                 in product(range(-200, 201, 100),
+                            repeat=2)]).buffer(1)
+    ps = [pa.union(pb)]
     
 def draw():
-    #py5.background(230, 240, 240)
+    py5.background(0, 0, 150)
     py5.translate(py5.width / 2, py5.height / 2)
-    for p in ps:
+    for i, p in enumerate(ps):
         py5.no_fill()
-        py5.stroke(p.area % 255, 200, 200, 32)
+        py5.stroke(py5.color(255) if i % 2
+                   else py5.color(0))
         py5.shape(py5.convert_cached_shape(p))
    
 def shrink():
     p = ps[-1]
-    new_p = p.buffer(-10, cap_style='round')
+    new_p = p.buffer(-B, cap_style='round')
     print(new_p.area)
     while new_p.area > 0:
-        new_p = ps[-1].buffer(-10, cap_style='round')
-        ps[-1] = ps[-1].difference(new_p)
         ps.append(new_p)
-        
+        new_p = ps[-1].buffer(-B, cap_style='round')
 
 def grow():
     p = ps[-1]
-    new_p = p.buffer(10, cap_style='round')
+    new_p = p.buffer(B, cap_style='round')
+    print(new_p.area)
     while new_p.area < 1000000:
-        new_p = ps[-1].buffer(10, cap_style='round')
-        ps.append(new_p.difference(ps[-1]))
-
+        ps.append(new_p)
+        new_p = ps[-1].buffer(B, cap_style='round')
 
 def key_pressed():
     if py5.key == 's':
