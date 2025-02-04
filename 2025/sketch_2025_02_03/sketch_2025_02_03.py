28277836b7f35248bc5d0c1b6666c1a01a96ce5a
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-02-04 00:52:31 -0300

    Update sketch_2025_02_03.py

--- 2025/sketch_2025_02_03/sketch_2025_02_03.py
+++ 2025/sketch_2025_02_03/sketch_2025_02_03.py
@@ -11,14 +11,16 @@ def setup():
     py5.no_fill()
     py5.stroke_weight(2)
     py5.stroke(255)
-    pts = np.array(star_points(200, 200, 160, 90, 6))
+    xc, yc = 200, 200
+    n = 6
+    pts = np.array(star_points(xc, yc, 160, 90, n))
     with py5.begin_closed_shape():
         py5.vertices(pts)
-    z = chain(*zip(pts, [(200, 200)] * 12))
+    line_pts = list(chain(*zip(pts, [(xc, yc)] * n * 2)))
     with py5.begin_shape(py5.LINES):
-        py5.vertices(list(z))
+        py5.vertices(line_pts)
     translated_pts = pts + np.array((400, 0))
-    smaller_pts = (pts - np.array((200, 200))) / 5
+    smaller_pts = (pts - np.array((xc, yc))) / 5
     for x, y in translated_pts:
         with py5.push_matrix(), py5.begin_closed_shape():
             py5.translate(x, y)
@@ -36,7 +38,6 @@ def star_points(xc, yc, ra, rb, n):
         yb = yc + rb * sin(i * ang + ang / 2)
         result.append((xb, yb))
     return result
-
 
 def key_pressed():
     py5.save_frame('out###.png')
