4a58c71db456ce8a940e81b134553178b4abfe90
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-02-16 22:44:05 -0300

    Update pngs_to_mp4_gui.py

--- admin_scripts/pngs_to_mp4_gui.py
+++ admin_scripts/pngs_to_mp4_gui.py
@@ -2,12 +2,13 @@
 
 from pathlib import Path
 
+# ATENÇÃO:                    Precisa instalar as bibliotecas!
+import imageio                # precisa instalar `pip install imageio[ffmpeg]`
 import FreeSimpleGUI as sg    # precisa instalar o PySimpleGUI (pode ser no pip install ou no Thonny "packages")
-import imageio              # precisa instalar:
-# precisa instalar pip install imageio[ffmpeg]
 
 
-default_output = Path.home() / 'out.mp4'
+default_input = Path.cwd()
+default_output = 'out.mp4'
 L_FONT = ('Courier', 20)
 I_FONT = ('Courier', 16)
 
@@ -15,7 +16,7 @@ I_FONT = ('Courier', 16)
 layout = [
     [sg.Text('Input folder:', font=L_FONT)],
     [sg.InputText(font=I_FONT), sg.FolderBrowse(font=I_FONT)],
-    [sg.Text('Output file:', font=L_FONT)],
+    [sg.Text('Output file name:', font=L_FONT)],
     [sg.InputText(default_text=default_output, font=I_FONT), sg.FileSaveAs(font=I_FONT)],
     [sg.Text('Frame duration (milliseconds):', font=L_FONT), sg.InputText(default_text='200', font=I_FONT, size=(6, 1))],
     [sg.Button('Create MP4', font=L_FONT), sg.Button('Cancel', font=L_FONT)]
@@ -36,26 +37,25 @@ while True:
     # Create the GIF if the Create GIF button was clicked
     if event == 'Create MP4' and values:
         # Get the input and output values from the GUI
-        input_dir = values[0] 
+        input_dir = Path(values[0])
         output_file = values[1]
         duration = int(values[2]) if values[2] else 200
-        dir_path = Path(input_dir)
         # Create the GIF if there are any images
         try:
             fps = 1000 / duration
             # Load the PNG images from the input folder
             images = [imageio.v3.imread(file_path) for file_path
-                  in sorted(dir_path.iterdir())
+                  in sorted(input_dir.iterdir())
                   if file_path.suffix.lower() == '.png']
             if images:
                 #imageio.imwrite(output_file, images, fps=fps)
-                writer = imageio.v2.get_writer(output_file, fps=fps)
+                writer = imageio.v2.get_writer(input_dir / output_file, fps=fps)
                 for img in images:
                   writer.append_data(img)
                 writer.close()
                 sg.popup(f'MP4 saved as:\n{output_file}')
             else:
-                sg.popup(f'No PNG images found at:\n{dir_path}')
+                sg.popup(f'No PNG images found at:\n{input_dir}')
         except Exception as e:
             if str(e).startswith('all input arrays'):
                 sg.popup('Select only images of same size.')
