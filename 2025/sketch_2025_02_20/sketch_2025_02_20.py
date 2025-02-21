166903174218b0d2d47e72dc1a21a5b5cc67d03f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-02-20 23:37:53 -0300

    Update sketch_2025_02_20.py

--- 2025/sketch_2025_02_20/sketch_2025_02_20.py
+++ 2025/sketch_2025_02_20/sketch_2025_02_20.py
@@ -7,19 +7,22 @@ import py5_tools
 from controlP5 import ControlP5  # jars on jars sub-directory
 
 def setup():
-    py5.size(500, 500, py5.P3D)
     global slider
     global picker
-    #py5.hint(py5.ENABLE_DEPTH_SORT)
-    #py5.hint(py5.ENABLE_DEPTH_TEST)
+
+    py5.size(500, 500, py5.P3D)
+    #py5.hint(py5.ENABLE_DEPTH_SORT)  # kind of trouble
+    #py5.hint(py5.ENABLE_DEPTH_TEST) 
+
     py5.window_title("3D Demo")
+
     cp5 = ControlP5(py5.get_current_sketch())
     slider = cp5.addSlider("slider", 0, 255, 150, 30, 10, 200, 20)
     picker = cp5.addColorPicker("picker", 270, 10, 100, 20)
     picker.setColorValue(py5.color(50, 155, 110))
-    step = 15
+
     # This works but doesn't record the widgets! (can be useful!)
-    #py5_tools.animated_gif(f'out.gif', duration=0.1, frame_numbers=range(1, 360, step))
+    #py5_tools.animated_gif(f'out.gif', duration=0.1, frame_numbers=range(1, 360, 15))
 
 def draw():
     py5.background(209)
