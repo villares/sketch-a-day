37895e1c31a27a6522bedeced9f8d865cf2810a2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-02-23 23:37:58 -0300

    23

--- 2025/sketch_2025_02_23/sketch_2025_02_23.py
+++ 2025/sketch_2025_02_23/sketch_2025_02_23.py
@@ -17,16 +17,32 @@ def setup():
     city_boundary = ox.geocode_to_gdf("São Paulo, Brazil")
     polygon = city_boundary.geometry[0]
     polygon = affinity.translate(polygon, 46, 23)
-    polygon = affinity.scale(polygon, xfact=1000,
-                                      yfact=-1000)
+    polygon_s = affinity.scale(polygon,
+                               xfact=500,
+                               yfact=-500,
+                               origin='center'
+                               )
+    polygon2 = affinity.scale(polygon,
+                              xfact=500,
+                              yfact=-500,
+                              origin='centroid'
+                              )
 
     py5.fill(100)
     with py5.begin_closed_shape():
-        py5.vertices(polygon.exterior.coords)
+        py5.vertices(polygon_s.exterior.coords)
 
+    py5.fill(0, 0, 100)
+    with py5.begin_closed_shape():
+        py5.vertices(polygon2.exterior.coords)
+        
     py5.fill(255, 0, 0, 100)
-    shp = py5.convert_shape(polygon)
+    py5.no_stroke()
+    shp = py5.convert_shape(polygon, flip_y_axis=True)
     shp.disable_style()
-    py5.shape(shp)
+    shp.scale(500)
+    py5.shape_mode(py5.CENTER)
+    py5.shape(shp, 0, 0) #,0, 0, 1000, 1000)
+    py5.save('out.png')
 
 py5.run_sketch(block=False)
