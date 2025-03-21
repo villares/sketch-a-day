d6c8f06d954ec837af0ace0c304123a7843e7dd3
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-03-21 12:09:47 -0300

    Update sketch_2025_03_20.py

--- 2025/sketch_2025_03_20/sketch_2025_03_20.py
+++ 2025/sketch_2025_03_20/sketch_2025_03_20.py
@@ -1,26 +1,37 @@
 """
-polys_from_text(words, py5_font) -> list of shapely Polygons
-extrude_polys(polys, depth) -> trimesh 3D mesh
+This will run an interactive py5 demo for two helper functions:
 
-TODO: I might want to flip the Y axis...
+- polys_from_text(words, font: py5.Py5Font)
+  produces a shapely.GeometryCollection (containing MultiPolygons for each glyph)
+
+- extrude_polys(polys, depth)
+  produces a trimesh.Trimesh 3D mesh
+
+"s" key will save an STL file
+"f" key will flip  the Y axis of the mesh.
 """
 
 import py5
+import numpy as np
 import shapely
 import trimesh
 
-def polys_from_text(words: str,
-                    font: py5.Py5Font,
-                    leading: float = 0,
-                    alternate_spacing=False) -> list[shapely.Polygon]:
+def polys_from_text(
+    words: str,
+    font: py5.Py5Font,
+    leading: float = None, 
+    alternate_spacing=False) -> shapely.GeometryCollection:
     """
-    Produce a list of shapely Polygons (with holes!) from a string.
+    Produce from a string a shapely GeometryCollection containing
+    MultiPolygons for each glyph. The MultiPolygon.geoms contains
+    one or more shapely.Polygon objects (which can have holes).
     New-line chars will try to move text to a new line.
     
     The alternate_spacing option will pick the glyph
     spacing from py5.text_width() for each glyph, it can be
     too spaced, but good for monospaced font alignment.
     """
+    # Use the font size as leading value if none is provided.
     leading = leading or font.get_size()
     py5.text_font(font)
     space_width = py5.text_width(' ')
@@ -29,10 +40,11 @@ def polys_from_text(words: str,
     for c in words:
         if c == '\n':
             y_offset += leading
-            x_offset = 0  # assuming left aligned text...
+            x_offset = 0  # this assumes left aligned text...
             continue
         glyph_pt_lists = [[]] # starts with an empty list inside
-        # ignore "need to call beginShape() first" warning
+        # font.get_shape(c, 1) will trigger a "Need to call beginShape() first"
+        # Processing warning you can safely ignore.
         c_shp = font.get_shape(c, 1)  
         vs3d = [c_shp.get_vertex(i)
                 for i in range(c_shp.get_vertex_count())]
@@ -44,7 +56,7 @@ def polys_from_text(words: str,
             if (x, y) not in vs:
                 vs.add((x, y))
             else:
-                glyph_pt_lists.append([]) # leaves trailling empty list
+                glyph_pt_lists.append([]) # note this leaves trailling empty list
         if alternate_spacing:
             w = py5.text_width(c)
         else:
@@ -56,10 +68,10 @@ def polys_from_text(words: str,
                        for p in glyph_pt_lists[:-1] if len(p) > 2]
         if glyph_polys:  # there are still empty glyphs at this point
             glyph_shapes = process_glyphs(glyph_polys)
-            results.extend(glyph_shapes)
-    return results
+            results.append(glyph_shapes)
+    return shapely.GeometryCollection(results)
 
-def process_glyphs(polys):
+def process_glyphs(polys) -> shapely.MultiPolygon:
     """
     Try to subtract the shapely Polygons representing a glyph
     in order to produce appropriate looking glyphs!
@@ -74,10 +86,11 @@ def process_glyphs(polys):
                 break
         else:   # the for-loop's else only executes after unbroken loops
             results.append(p)
-    return results
+    return shapely.MultiPolygon(results)
 
-def extrude_polys(polys: shapely.Polygon | shapely.MultiPolygon,
-                  depth: float) -> trimesh.Trimesh:
+def extrude_polys(
+    polys: shapely.Polygon | shapely.MultiPolygon,
+    depth: float) -> trimesh.Trimesh:
     """
     Extrude a Polygon or MultiPolygon
     """
@@ -94,7 +107,7 @@ if __name__ == '__main__':
     # https://fonts.google.com/specimen/Saira+Stencil+One?query=stencil
 
     def setup():
-        global margin, slab
+        global margin, slab, holes
 
         py5.size(500, 500, py5.P3D)
         py5.color_mode(py5.HSB)
@@ -109,17 +122,16 @@ if __name__ == '__main__':
             'numpy &\n' \
             'shapely &\n' \
             'trimesh.'
-
-        shapes = shapely.MultiPolygon(
-            polys_from_text(t, f, alternate_spacing=True))
-        min_x, min_y, max_x, max_y = shapes.bounds
+        
+        holes = polys_from_text(t, f, alternate_spacing=True)
+        min_x, min_y, max_x, max_y = holes.bounds
         margin = 20
         slab_rect = shapely.Polygon(
             ((min_x - margin, min_y - margin),
              (max_x + margin, min_y - margin),
              (max_x + margin, max_y + margin),
              (min_x - margin, max_y + margin)))
-        clipped_rect = slab_rect - shapes
+        clipped_rect = slab_rect - holes
         slab = extrude_polys(clipped_rect, 10)
         
     def draw():
@@ -132,8 +144,9 @@ if __name__ == '__main__':
     def key_pressed():
         if py5.key == 's':
             slab.export('slab.stl')
+        if py5.key == 'f':
+            flip_y_matrix = np.eye(4)  
+            flip_y_matrix[1, 1] = -1   
+            slab.apply_transform(flip_y_matrix)
        
     py5.run_sketch(block=False)
-
-
-
