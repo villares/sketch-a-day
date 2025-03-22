a7fb33090ed1c0c9c276048a2594b24094b22ee8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-03-22 11:22:39 -0300

    Update generate_entries.py

--- admin_scripts/generate_entries.py
+++ admin_scripts/generate_entries.py
@@ -48,13 +48,16 @@ gui_mode = True  # default
 YEAR = '2025'   
 USER, REPO = 'villares', 'sketch-a-day'
 MAIN_SITE = 'https://abav.lugaralgum.com/sketch-a-day'
-SPONSOR_LINK = 'https://www.paypal.com/donate/?hosted_button_id=5B4MZ78C9J724'
+SPONSOR_LINKS = 'https://www.paypal.com/donate/?hosted_button_id=5B4MZ78C9J724\n'\
+                'https://liberapay.com/Villares\n'\  
+                'https://wise.com/pay/me/alexandrev562'
+  
 REPO_MAIN_URL = f'https://github.com/{USER}/{REPO}/tree/main'
 RAW_CONTENT = f'https://raw.githubusercontent.com/{USER}/{REPO}/main'
 TOOT_DEFAULT = (
     'Code at: {}\n'
     f'More sketch-a-day: {MAIN_SITE}\n'
-    f'If you like this, support my work: {SPONSOR_LINK}\n'
+    f'If you like this, support my work: \n{SPONSOR_LINKS}\n'
     )
 # base_path = "/Users/villares/sketch-a-day" # 01046-10 previously
 base_path = Path(f'/home/{USER}/GitHub/{REPO}/')
