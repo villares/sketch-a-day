5459634630ff03e3d970a84f5378f2b857458945
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-03-24 23:49:30 -0300

    24

---
+++ 2025/sketch_2025_03_24/sketch_2025_03_24.py
@@ -0,0 +1,64 @@
+from skimage.data import astronaut
+from skimage.color import label2rgb
+from skimage.segmentation import slic
+
+astronaut_image = astronaut() 
+astronaut_segments = slic(astronaut_image, 
+                          n_segments=100, 
+                          compactness=10)
+colored_segments = label2rgb(astronaut_segments, astronaut_image, kind='avg')
+# print(colored_segments)
+
+def setup():
+    global img
+    size(512, 512)
+    color_mode(HSB) # Matiz, Sat, Bri
+    no_stroke()
+    img = create_image_from_numpy(colored_segments, 'RGB')
+
+
+def draw():
+    background(0)
+    num = 1 + int(mouse_y / 10)
+    R = width / num
+    largura = R * 1.5
+    altura = R * sqrt(3)
+    random_seed(int(R))
+    
+    for fila in range(num):
+        for coluna in range(num):  # 0, 1, ... 9
+            x = int(coluna * largura)
+            y = int(fila * altura + altura / 2
+                    + (altura / 2 if coluna % 2 else 0))
+            D = R + R / 2 * sin(radians(x + frame_count * 10))
+            fill(img.get_pixels(x, y), 200)
+            d = random(D)
+            poligono(x, y, d)
+
+def poligono(xc, yc, ra, pontos=6):
+    ang = TWO_PI / pontos
+    begin_shape()
+    for i in range(pontos):
+        x = xc + cos(ang * i) * ra
+        y = yc + sin(ang * i) * ra
+        vertex(x, y)
+    end_shape(CLOSE)
+
+def estrela(xc, yc, ra, rb, pontas=6, rot=0):
+    ang = TWO_PI / pontas
+    begin_shape()
+    for i in range(pontas):
+        x = xc + cos(rot + ang * i) * ra
+        y = yc + sin(rot + ang * i) * ra
+        vertex(x, y)
+        x = xc + cos(rot + ang * i + ang / 3) * rb
+        y = yc + sin(rot + ang * i + ang / 3) * rb
+        vertex(x, y)
+    end_shape(CLOSE)
+
+    
+    
+    
+    
+    
+
