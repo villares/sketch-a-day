f23be0558966a3abafd4300e343ff93c99a54f25
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-04-20 23:56:52 -0300

    20

---
+++ 2025/sketch_2025_04_20/sketch_2025_04_20.py
@@ -0,0 +1,54 @@
+import py5
+
+grid = {}
+squares = []
+margin = 50
+N = 16
+
+def setup():
+    py5.size(700, 700)
+    calc_grid()
+
+def draw():
+    global mouse_vector
+    mouse_vector = py5.Py5Vector(py5.mouse_x, py5.mouse_y)
+    py5.random_seed(1)
+    py5.background(230, 240, 250)
+    py5.stroke_weight(1)
+    for s in squares:
+        py5.fill(0, 128, py5.random(255))
+        with py5.begin_closed_shape(py5.QUADS): 
+            py5.vertices(grid[i, j]for i, j in s) 
+    py5.stroke_weight(5)
+    py5.points(tuple(grid.values()))
+
+#     py5.no_fill()
+#     with py5.begin_closed_shape(py5.QUADS): 
+#         py5.vertices(grid[i, j]
+#                  for s in squares
+#                  for i, j in s) 
+
+def calc_grid():
+    cell_size = (py5.width - margin * 2) / N
+    squares.clear()
+    for i in range(N):
+        x = margin + i * cell_size + cell_size / 2 
+        for j in range(N):
+            y = margin + j * cell_size + cell_size / 2
+            grid[i, j] = py5.Py5Vector(x, y)
+            if i > 0 and j > 0:
+                squares.append((
+                    (i-1, j-1), (i, j-1),
+                    (i, j), (i-1, j)
+                    ))
+
+def mouse_dragged():
+    for (i, j), v in grid.items():
+        d = v - mouse_vector
+        d.mag = 1 / (d.mag + 1)
+        grid[i, j] += d
+    
+def key_pressed():
+    calc_grid()
+     
+py5.run_sketch(block=False)
