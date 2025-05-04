6e9099be0b96232533e7893bd73cc145d30fdf64
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-05-04 12:06:58 -0300

    Create sketch_2025_05_04.py

---
+++ 2025/sketch_2025_05_04/sketch_2025_05_04.py
@@ -0,0 +1,75 @@
+# inspired by Marius Watz's work...
+
+import py5
+from py5 import sin, cos, radians, PI, TAU
+from py5_tools import animated_gif
+
+def setup():
+    py5.size(600, 600, py5.P3D)
+    py5.color_mode(py5.HSB)
+    animated_gif('out.gif',
+                 duration=0.16,
+                 frame_numbers=range(1, 361, 3))
+    
+def draw():
+    py5.background(0)
+    py5.no_stroke()
+    py5.translate(py5.width / 2, py5.height / 2, 0)
+    py5.rotate_y(radians(py5.frame_count))
+    for i in range(6):
+        py5.rotate_y(TAU / 3)
+        for j in range(6):
+            py5.rotate_z(TAU / 4)
+            for k in range(6):
+                py5.rotate_x(TAU / 5)
+                py5.fill(128 + j * 16, 255, 255, 200)
+                if j % 2 == 1 and k % 2 == 0 and i % 2 == 0:
+                    py5.fill(255, 200)
+                if j % 2 == 0:
+                    spaced_arc(
+                        0, 0,
+                        60,
+                        radians(py5.frame_count),
+                        100 + 25 * i - 5 * k,
+                        25)            
+                else:
+                    solid_arc(
+                        0, 0,
+                        60,
+                        radians(py5.frame_count),
+                        100 + 25 * i - 5 * k,
+                        10)                    
+
+def solid_arc(x, y, deg, start, radius, w):
+    vs = []
+    for i in range(0, deg, 5):
+        a = radians(i) + start
+        vs.extend((
+            (cos(a) * (radius) + x,
+             sin(a) * (radius) + y),
+            (cos(a) * (radius + w) + x,
+             sin(a) * (radius + w) + y)))
+    py5.begin_shape(py5.QUAD_STRIP)
+    py5.vertices(vs)
+    py5.end_shape()
+
+def spaced_arc(x, y, deg, start, radius, w, s=6, t=3):
+    vs = []
+    for i in range(0, deg, s):
+        a = radians(i) + start
+        b = radians(i + t) + start
+        vs.extend((
+            (cos(a) * (radius) + x,
+             sin(a) * (radius) + y),
+            (cos(a) * (radius + w) + x,
+             sin(a) * (radius + w) + y),
+            (cos(b) * (radius + w) + x,
+             sin(b) * (radius + w) + y),
+            (cos(b) * (radius) + x,
+             sin(b) * (radius) + y),            
+            ))
+    py5.begin_shape(py5.QUADS)
+    py5.vertices(vs)
+    py5.end_shape()
+
+py5.run_sketch(block=False)
