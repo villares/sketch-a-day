7d6f99fdc6a3f293f673a48dd2afad4ed0507be1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-05-18 12:35:30 -0300

    Create sketch_2025_05_18.py

---
+++ 2025/sketch_2025_05_18/sketch_2025_05_18.py
@@ -0,0 +1,48 @@
+"""
+This is new on py5 version 0.10.5a. Run the following lines before importing py5,
+and it will add Processing's PeasyCam by Jonathan Feinberg to py5.
+https:#mrfeinberg.com/peasycam/ "Dead-simple mouse-driven camera for Processing" 
+"""
+import py5_tools
+print(py5_tools.processing.download_library("PeasyCam"))
+
+from itertools import product    
+
+import py5
+ 
+from peasy import PeasyCam   # the only tricky part is to find the module name!
+
+def setup():
+    global cam 
+    py5.size(512, 512, py5.P3D)
+    this_sketch = py5.get_current_sketch()
+    cam = PeasyCam(this_sketch, 400)
+    cam.setMinimumDistance(300)
+    cam.setMaximumDistance(500)
+
+    orbitDH = cam.getRotateDragHandler() # get the RotateDragHandler
+    panDH = cam.getPanDragHandler()      # get the PanDragHandler
+    cam.setCenterDragHandler(panDH)      # set it to the Center/Wheel drag
+    cam.setRightDragHandler(orbitDH)     # set it to the right-button mouse drag
+    cam.setLeftDragHandler(None)   
+
+def draw():
+    py5.background(0)
+    py5.color_mode(py5.HSB)
+    py5.random_seed(1)
+    cs = 32
+    for i, j, k in product(range(-3, 4), repeat=3):
+        x, y, z = i * cs, j * cs, k * cs 
+        bs = py5.random_choice((4, 8, 16, 32))
+        py5.fill(bs * 4, 255, 150)
+        with py5.push_matrix():
+            py5.translate(x, y, z)
+            py5.box(bs)
+    # Use the HUD feature to add screen anchored elements 
+    cam.beginHUD()
+    py5.fill(255)
+    py5.text_size(20)
+    py5.text('PeasyCam demo', 15, 15)
+    cam.endHUD()
+   
+py5.run_sketch(block=False)
