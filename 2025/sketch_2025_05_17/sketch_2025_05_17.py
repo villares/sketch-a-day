4b8d7d819f834f94bd05f71e53fea11e6128f20d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-05-17 21:25:43 -0300

    Create sketch_2025_05_17.py

---
+++ 2025/sketch_2025_05_17/sketch_2025_05_17.py
@@ -0,0 +1,37 @@
+"""
+This is new on py5 version 0.10.5a. Run the following lines before importing py5,
+and it will add Processing's PeasyCam by Jonathan Feinberg to py5.
+https://mrfeinberg.com/peasycam/ "Dead-simple mouse-driven camera for Processing" 
+"""
+import py5_tools
+print(py5_tools.processing.download_library("PeasyCam"))
+
+from itertools import product    
+
+import py5
+# 
+from peasy import PeasyCam 
+
+def setup():
+    global cam 
+    py5.size(512, 512, py5.P3D)
+    this_sketch = py5.get_current_sketch()
+    cam = PeasyCam(this_sketch, 500)
+    
+def draw():
+    py5.background(0)
+    py5.color_mode(py5.HSB)
+    py5.random_seed(1)
+    cs = 32
+    for i, j, k in product(range(-3, 4), repeat=3):
+        x, y, z = i * cs, j * cs, k * cs 
+        bs = py5.random_choice((4, 8, 16, 32))
+        py5.fill(bs * 4, 255, 150)
+        with py5.push_matrix():
+            py5.translate(x,  y, z)
+            py5.box(bs)
+
+def key_pressed():
+    redraw()
+   
+py5.run_sketch(block=False)
