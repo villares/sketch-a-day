56e30b6fec2ddd83468d898b37f1e51a317a8414
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-05-21 00:48:11 -0300

    20

--- 2025/sketch_2025_05_20/sketch_2025_05_20.py
+++ 2025/sketch_2025_05_20/sketch_2025_05_20.py
@@ -14,11 +14,13 @@ import numpy as np
 
 cubes = []
 update_picker = True
+cs = 100  # max cube size
 
 def setup():
     global cam, picker_map, current_pixels, previous_pixels
     py5.size(512, 512, py5.P3D)
     py5.color_mode(py5.HSB)
+    
     this_sketch = py5.get_current_sketch()
     cam = PeasyCam(this_sketch, 500)
     cam.setMinimumDistance(400)
@@ -28,46 +30,36 @@ def setup():
     cam.setCenterDragHandler(orbit_handler)    # set the orbit handler to the Center/Wheel drag
     cam.setRightDragHandler(pan_handler)       # set the pan handler to the right-button mouse drag
     #cam.setLeftDragHandler(None)      # remove orbit from the left mouse button
-    picker_map = np.empty((py5.height, py5.width), dtype=int) 
-    cs = 100  # cell size
-    for i, j, k in product(range(-1, 2), repeat=3):
-        cubes.append(Cube(i * cs, j * cs, k * cs))
+    
+    picker_map = np.empty((py5.height, py5.width), dtype=int)
+    picker_map.fill(-1)
     previous_pixels = py5.get_np_pixels()
     current_pixels = np.empty_like(previous_pixels)
+    setup_cubes()
 
-def predraw_update():
-    global mouse_over
-    mouse_over = -1
+def setup_cubes():
+    cubes.clear()
+    for cube_index, (i, j, k) in enumerate(product(range(-1, 2), repeat=3)):
+        cubes.append(Cube(i * cs, j * cs, k * cs, cube_index))
+    
+def draw():
     try:
         mouse_over = picker_map[py5.mouse_y, py5.mouse_x]
     except IndexError:
-        pass
+        mouse_over = -1
     picker_map.fill(-1)
     py5.window_title(str(round(py5.get_frame_rate())))
 
-def draw():
-    global update_picker
     if update_picker:
-    #if (py5.mouse_x, py5.mouse_y) == (py5.pmouse_x, py5.pmouse_y):
-        for cube_index, cube in enumerate(cubes):
-            py5.fill(10)
-            cube.draw_box()
-            py5.get_np_pixels(dst=current_pixels)
-            mask = np.any(previous_pixels != current_pixels, axis=-1)
-            picker_map[mask] = cube_index
-            py5.get_np_pixels(dst=previous_pixels) 
+        for cube in cubes:
+            cube.update_picker_map()
     py5.background(0)
-    for cube_index, cube in enumerate(cubes):
-        cube.draw(mouse_over == cube_index)
-    py5.get_np_pixels(dst=current_pixels)
-#     if (current_pixels != previous_pixels).any():
-#         update_picker = False        
-#         py5.get_np_pixels(dst=previous_pixels)
-#     else:
-#         update_picker = True
+    for cube in cubes:
+        cube.draw(mouse_over)
         
 def mouse_clicked():
     if py5.mouse_button == py5.LEFT:
+        mouse_over = picker_map[py5.mouse_y, py5.mouse_x]
         if mouse_over >= 0:
            cubes[mouse_over].clicked()
 
@@ -79,28 +71,44 @@ def mouse_released():
     global update_picker
     update_picker = True
 
+def key_pressed():
+    if py5.key == ' ':
+        setup_cubes()
+
 class Cube:
     
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, cube_index):
         self.x, self.y, self.z = x, y, z
-        self.on = py5.random_choice((True, False))
-        self.color = py5.random_int(126) * 2
+        self.core = py5.random_choice((True, False))
+        self.color = py5.random_int(127) * 2
+        self.cube_index = cube_index
                 
-    def draw(self, selection=False):
+    def draw(self, mouse_over):
+        selection = (self.cube_index == mouse_over)
         py5.stroke(selection * 255)
-        if self.on:
-            py5.fill(self.color, 255, 255, 255 - selection * 150)
-        else:
+        py5.fill(self.color, 255, 255, 255 - selection * 150)
+        self.draw_box(50)
+        if not self.core:
             py5.fill(200, 255 - selection * 150)
-        self.draw_box()
+            self.draw_box(100)
         
-    def draw_box(self):
+    def draw_box(self, s=None):
+        s = s or (100 - self.core * 50)
         with py5.push_matrix():
             py5.translate(self.x, self.y, self.z)
-            py5.box(100 - self.on * 50)
+            py5.box(s)
+
+    def update_picker_map(self):
+        py5.get_np_pixels(dst=previous_pixels) 
+        py5.fill(10)
+        py5.no_stroke()
+        self.draw_box()
+        py5.get_np_pixels(dst=current_pixels)
+        mask = np.any(previous_pixels != current_pixels, axis=-1)
+        picker_map[mask] = self.cube_index
 
     def clicked(self):
-        self.on = not self.on
+        self.core = not self.core
 
 py5.run_sketch(block=False)
 
