6b942681a744c3648dd8bc5b29459d396221e882
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-06-11 18:17:43 -0300

    10

---
+++ 2025/sketch_2025_06_10/sketch_2025_06_10.py
@@ -0,0 +1,48 @@
+# py5 imported mode code: learn about it at py5coding.org
+
+import shapely
+from shapely import Polygon, Point, LineString
+from shapely.affinity import translate as s_translate
+import trimesh
+
+def setup():
+    global subtracted_box
+    size(500, 500, P3D)
+    c = Point(50, 50).buffer(50)  # buffer com o raio
+    d = s_translate(c, 50, 0)
+    crescent = c - d
+    m = trimesh.creation.extrude_polygon(crescent, 100)
+    #shape(malha)
+    b = trimesh_box(0, 0, 0, 200)
+    subtracted_box = b.difference(m)
+
+def draw():
+    background(0, 0, 100)
+    lights()
+    translate(width / 2, height / 2)
+    rotate_y(radians(mouse_x))
+    fill(0, 200, 0)
+    draw_mesh(subtracted_box)
+     
+def trimesh_box(x, y, z, w, h=None, d=None):
+    h = w or h
+    d = d or h
+    c = trimesh.creation.box((w, h, d))
+    c.apply_translation((x, y, z))
+    return c
+
+def draw_mesh(m):
+    push_style()
+    no_stroke()
+    with begin_closed_shape(TRIANGLES):
+        vertices(m.vertices[v]
+                     for face in m.faces
+                     for v in face)   
+    pop_style()
+    vs = m.vertices
+    bs = m.facets_boundary
+#     for facet in bs:
+#         for a, b in facet:
+#             line(*vs[a], *vs[b])
+    a, b = np.vstack(bs).T
+    lines(np.column_stack((vs[a], vs[b])))
--- docs/README.md
+++ docs/README.md
@@ -22,6 +22,16 @@ If you appreciate what I have been doing, you may also support my artistic work,
 
 ---
 
+### sketch_2025_06_10
+
+![sketch_2025_06_10](https://raw.githubusercontent.com/villares/sketch-a-day/main/2025/sketch_2025_06_10/sketch_2025_06_10.png)
+
+[sketch_2025_06_10](https://github.com/villares/sketch-a-day/tree/main/2025/sketch_2025_06_10) [[py5](https://py5coding.org/)]
+
+A #trimesh example used in class yesterday!
+
+---
+
 ### sketch_2025_06_09
 
 ![sketch_2025_06_09](https://raw.githubusercontent.com/villares/sketch-a-day/main/2025/sketch_2025_06_09/sketch_2025_06_09.png)
