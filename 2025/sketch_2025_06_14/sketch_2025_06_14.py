b57cf9a27250b2f6566cb7c978d61b0b20920903
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-06-14 23:47:48 -0300

    14

--- 2025/sketch_2025_06_14/sketch_2025_06_14.py
+++ 2025/sketch_2025_06_14/sketch_2025_06_14.py
@@ -1,50 +1,64 @@
 from itertools import product
-from random import sample
-from shapely.geometry import LineString
+
+from shapely.geometry import LineString, LinearRing
+import py5
 
 poly = LineString([(0, 0), (1, 1), (2, 0), (3, 1)])
 
-
-
 MARGIN = 64
-SPACING = 128
-
-grid_pts = 5
+SPACING = 128 + 64
 
 def setup():
-    global poly
-    size(512, 512)
-    grid = list(product(range(MARGIN, width, SPACING), repeat=2))
-    poly = LineString(sample(grid, grid_pts))
-    background(200)
-    stroke(255)
-    stroke_weight(2)
-    shape(poly)
-
+    global grid
+    py5.size(512, 512)
+    grid = list(product(range(MARGIN, py5.width, SPACING), repeat=2))
+    make_polys()
+    
+def make_polys():
+    global poly_a, poly_b
+    py5.background(200)
+    simple = False
+    poly_a = make_simple_poly(grid)
+    poly_b = make_simple_poly(grid)
+    
+def make_simple_poly(grid, grid_pts = 4):
+    simple = False
+    while not simple:
+        poly = LinearRing(py5.random_sample(grid, grid_pts))
+        simple = poly.is_simple
+    return poly
     
 def draw():
-    #background(240)
+    py5.background(200)
+    py5.stroke_weight(2)
+    py5.stroke(0, 0, 200)
+    py5.points(grid)
+    py5.stroke(255)
+    py5.shape(poly_a)
+    py5.shape(poly_b)
+    
+    py5.stroke(0)
+    py5.stroke_weight(0.5)
+    points_a = get_poly_points(poly_a, 200)
+    points_b = get_poly_points(poly_b, 200)
+    py5.lines((xa, ya, xb, yb) for (xa, ya), (xb, yb)
+              in zip(points_a, points_b))
+    
+def get_poly_points(poly, num = 100):
+    d = poly.length / (num - 1)
+    return [(p.x, p.y) for p in
+            (poly.interpolate(i * d)
+             for i in range(num))]
 
-    stroke(0)
-    stroke_weight(2)
-    num_points = 100
-    d = poly.length / (num_points - 1)
-    pts = [(p.x, p.y) for p in (poly.interpolate(i * d)
-                                for i in range(num_points))]
-    points(pts)    # draw at the location
+# from typing import Sequence
+# def lerp_sequence(a: Sequence, b: Sequence, t: float) -> Sequence:
+#     return tuple(lerp_sequence(ca, cb, t) if isinstance(ca, Sequence)
+#                  else py5.lerp(ca, cb, t)             
+#                  for ca, cb in zip(a, b))
 
-def lerp_along_list(amt, lst):
-    # Based on LerpVectorsExample by Jeremy Douglass
-    amt = constrain(amt, 0, 1)  # let's play safe
-    if len(lst) == 1:
-        return lst[0]
-    cunit = 1.0 / (len(lst) - 1)
-    return lerp_sequence(lst[floor(amt / cunit)],
-                      lst[ceil(amt / cunit)],
-                      amt % cunit / cunit)
+def key_pressed():
+    py5.save_frame('###.png')
+    make_polys()
+    
 
-from typing import Sequence
-def lerp_sequence(a: Sequence, b: Sequence, t: float) -> Sequence:
-    return tuple(lerp_sequence(ca, cb, t) if isinstance(ca, Sequence)
-                 else lerp(ca, cb, t)             
-                 for ca, cb in zip(a, b))
+py5.run_sketch(block=False)
