098559d396590a3ac88d8553a458edd7c7725124
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-07-01 00:15:19 -0300

    30/6 Turing

---
+++ 2025/sketch_2025_06_30/sketch_2025_06_30.py
@@ -0,0 +1,31 @@
+def setup():
+    global img
+    size(712, 949)
+    img = load_image("https://upload.wikimedia.org/wikipedia/commons/6/66/Alan_Turing_%281951%29_%28crop%29.jpg")
+    no_stroke()
+    color_mode(HSB)
+    rect_mode(CENTER)
+    
+def draw():
+    background(0) 
+    num_filas = 128 # 64
+    esp = height / num_filas
+    num_colunas = int(width / esp)
+    if img:
+        for m in range(num_filas):
+            y = m * esp + esp / 2
+            for n in range(num_colunas):  # 0, 1, 2, 3, 4, 5
+                x = n * esp + esp / 2
+                xi = int(remap(x, 0, width, 0, img.width))
+                yi = int(remap(y, 0, height, 0, img.height))
+                cor = img.get_pixels(xi, yi)
+                b = brightness(cor)
+                h = int(remap(y, 0, height, 0, 255))
+                fill(h, 255, 255)
+                if is_mouse_pressed:
+                    square(x, y, esp * b / 255)
+                else:
+                    square(x, y, esp * b // 255)
+
+def key_pressed():
+    save_frame('out##.png')
