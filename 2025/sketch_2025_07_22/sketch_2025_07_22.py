edf81d0f64a32c752ab9c1c6f6f4e8caf6555a9e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-07-22 22:26:51 -0300

    22

---
+++ 2025/sketch_2025_07_22/sketch_2025_07_22.py
@@ -0,0 +1,120 @@
+# Based on sketch 2020_10_28b
+
+from villares.arcs import quarter_circle
+
+N = 24
+FW = 4  # first stroke weight
+SW = 4  # second weight
+first_color = color(255)
+second_color = color(0, 0, 100)
+
+bgd = color(150)
+
+def setup():
+    global w, d, variations, rotations
+    size(909, 909)
+    w = width / N
+    d = w / 7
+    no_fill()
+    rotations = [0] * N * N
+    variations = [
+        random_choice([module0, module2])
+        for i in range(N * N)
+    ]
+    
+def draw():
+    background(bgd)
+#     translate(500, 500)
+#     scale(4)
+#     module0(w)
+    for i in range(N):
+        x = w / 2 + i * w
+        for j in range(N):
+            y = w / 2 + j * w
+            push()
+            translate(x, y)
+            r = rotations[i + j * N]
+            rotate(HALF_PI * r)
+            module_func = variations[i + j * N]
+            module_func(w)
+            pop()
+
+def module0(w):
+    translate(-w / 2, -w / 2)
+    if is_key_pressed and key_code == ALT:
+        stroke(0, 0, 255)
+        rect(0, 0, w, w)
+    r = w / 2
+    dc = w / 8
+    
+    stroke(first_color)
+    stroke_weight(FW)
+    quarter_circle(0, 0, r - d, BOTTOM + RIGHT)
+    quarter_circle(0, 0, r + d, BOTTOM + RIGHT)
+    quarter_circle(w, w, r - d, TOP + LEFT)
+    quarter_circle(w, w, r + d, TOP + LEFT)
+    
+    stroke(second_color)
+    stroke_weight(SW)
+    quarter_circle(0, w, r, TOP + RIGHT)
+    quarter_circle(w, 0, r, BOTTOM + LEFT)
+
+
+def module1(w): 
+    translate(-w / 2, -w / 2)
+    r = w / 2
+    stroke(first_color)
+    stroke_weight(FW)
+    line(0, r - d, r - d, 0)
+    line(w, r - d, r - d, w)
+    line(0, r + d, r + d, 0)
+    line(w, r + d, r + d, w)
+    
+    stroke(second_color)
+    stroke_weight(SW)
+    line(0, r, r, 0)
+    line(w, r, r, w)
+
+
+def module2(w): 
+    translate(-w / 2, -w / 2)
+    r = w / 2
+    
+    stroke(first_color)
+    stroke_weight(FW)
+
+    quarter_circle(0, 0, r - d, BOTTOM + RIGHT)
+    quarter_circle(0, 0, r + d, BOTTOM + RIGHT)
+    quarter_circle(w, w, r - d, TOP + LEFT)
+    quarter_circle(w, w, r + d, TOP + LEFT)
+    
+    stroke(second_color)
+    stroke_weight(SW)
+    quarter_circle(0, 0, r, BOTTOM + RIGHT)
+    quarter_circle(w, w, r, TOP + LEFT)
+
+def key_pressed():
+    if key == 's':
+        save_frame('####.png')
+    elif key == 'r':
+        for i in range(N * N):
+            rotations[i] = int(random(4))
+    elif key == 'R':
+        for i in range(N * N):
+            if variations[i] != module0:
+                rotations[i] = 0
+    elif key == 'v':
+        for i in range(N * N):
+            variations[i] =  random_choice(
+                [module0, module1])        
+    elif key == 'w':
+        for i in range(N * N):
+            variations[i] =  random_choice(
+                [module0, module2])
+    elif key == 'W':
+        for i in range(N * N):
+            variations[i] =  random_choice(
+                [module0, module1, module2]) 
+    elif key == 'V':
+        for i in range(N * N):
+            variations[i] = module2
