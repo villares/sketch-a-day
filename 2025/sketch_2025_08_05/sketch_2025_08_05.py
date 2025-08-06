96dd79ae00cb7a69630103c7d4d69450bd9d572d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-08-05 23:11:39 -0300

    5

---
+++ 2025/sketch_2025_08_05/sketch_2025_08_05.py
@@ -0,0 +1,58 @@
+import py5
+import py5_tools 
+import numpy as np
+
+R = 500
+step = 0.001
+step2 = 0.0005
+OFFY = 500
+W = 50
+N = 25
+
+def setup():
+    py5.size(500, 500)
+    py5.no_smooth()
+    py5.stroke_weight(2)
+    py5.os_noise_seed(2)
+    # Export gif
+    py5_tools.animated_gif('noise_walk.gif', duration=0.1,
+                           frame_numbers=range(1, 361, 3))
+    py5.background(5)
+
+
+def draw():
+    py5.background(0)
+    py5.no_stroke()
+    py5.fill(0, py5.random_int(3, 7))
+    py5.rect(0,0, py5.width, py5.height)
+    py5.translate(py5.width / 2, py5.height / 2)
+    pos_xxs, pos_xys, pos_yxs, pos_yys = [], [], [], []
+    pos_zs, pos_ws = [], []
+    for a in range(1, 361):
+        t = py5.radians(a)
+        f = py5.radians(py5.frame_count)
+        wx = py5.cos(t) * R
+        wy = py5.sin(t) * R
+        wz = py5.cos(f) * R 
+        ww = py5.sin(f) * R 
+        py5.stroke(255)
+        for i in range(N):
+            pos_xxs.append(wx * step - OFFY)
+            pos_xys.append(wy * step - OFFY)
+            pos_yxs.append(wx * step + OFFY)
+            pos_yys.append(wy * step + OFFY)
+            pos_zs.append((i * W + wz) * step2)
+            pos_ws.append((i * W + ww) * step2)
+    npzs = np.array(pos_zs)
+    npws = np.array(pos_ws)
+    nxs = py5.os_noise(
+        np.array(pos_xxs), np.array(pos_xys), npzs, npws
+        ) * py5.width * 0.60
+    nys = py5.os_noise(
+        np.array(pos_yxs), np.array(pos_yys), npzs, npws
+        ) * py5.height * 0.60
+    py5.points(np.column_stack((nxs, nys)))
+    
+py5.run_sketch(block=False)
+
+
