b0b8fa392d3c2103aa8388d0683abd09cb801419
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-09-25 09:53:51 -0300

    24 cleanup

--- 2025/sketch_2025_09_24/sketch_2025_09_24.py
+++ 2025/sketch_2025_09_24/sketch_2025_09_24.py
@@ -1,11 +1,10 @@
 import py5
 import shapely
-from shapely import Polygon
 
 def setup():
     py5.size(500, 500)
     py5.no_fill()
-    s = Polygon((
+    s = shapely.Polygon((
         (100, 100), (400, 100),
         (400, 400), (100, 400)
         ))
