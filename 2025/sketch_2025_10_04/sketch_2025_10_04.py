77c8cb7a282ce8d0c7ee811ee55d1ebbcbaf2c97
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-10-04 23:37:23 -0300

    Update sketch_2025_10_04.py

--- 2025/sketch_2025_10_04/sketch_2025_10_04.py
+++ 2025/sketch_2025_10_04/sketch_2025_10_04.py
@@ -2,10 +2,12 @@ import py5
 
 def setup():
     py5.color_mode(py5.CMAP, py5.mpl_cmaps.VIRIDIS, 100, 100)
-    py5.background('w')
+    py5.background('white')
     for i in range(100):
         for j in range(100):
             py5.stroke(i, j)
             py5.point(i, j)
             
 py5.run_sketch()
+
+
