118836fcfecfaf29ca31c4939fe36dc788dbf8ab
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-10-20 23:29:13 -0300

    20

---
+++ 2025/sketch_2025_10_20/sketch_2025_10_20.py
@@ -0,0 +1,42 @@
+from itertools import product
+from random import sample
+
+import numpy as np
+from shapely import unary_union, make_valid
+from shapely.geometry import MultiPolygon, Polygon, Point
+import py5
+
+grid = [(x, y) for x, y in product(range(-225, 226, 75), repeat=2)
+        if abs(x)==225 or abs(y)==225
+        ]
+print(grid)
+
+def setup():
+    global max_area
+    py5.size(500, 500)
+    py5.color_mode(py5.CMAP, 'magma_r', 300)
+    py5.no_stroke()
+    generate()
+    
+def generate():
+    global result    
+    shapes = Polygon(py5.random_permutation(grid))
+    all_shapes = make_valid(shapes).buffer(-3)
+    result = all_shapes.buffer(3)  # .difference(all_shapes.buffer(-4))
+    if isinstance(result, Polygon):
+        result = MultiPolygon([result])
+    
+def draw():
+    py5.background('black')
+    max_area = max(shp.area for shp in result.geoms)
+    py5.translate(py5.width / 2, py5.height / 2)
+    for shp in result.geoms:
+        area = shp.area
+        py5.fill(area / max_area * 255)
+        py5.shape(shp)#.difference(shp.buffer(-3))) 
+
+def key_pressed():
+    py5.save_frame('####.png')
+    generate()
+
+py5.run_sketch(block=False)
