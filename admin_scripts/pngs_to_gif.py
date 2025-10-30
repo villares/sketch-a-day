91019471ef1367ecc98d666fde42b217e1324115
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-10-30 17:13:36 -0300

    Update pngs_to_gif.py

--- admin_scripts/pngs_to_gif.py
+++ admin_scripts/pngs_to_gif.py
@@ -41,37 +41,38 @@ parser.add_argument(
 args = parser.parse_args()
 
 input_dir = Path(args.input or Path.cwd())
-
-if input_dir.is_dir():
-    try:
-        images = [imageio.v3.imread(file_path)
-                  for file_path in sorted(input_dir.iterdir())
-                  if file_path.suffix.lower() == '.png']
-        if images:
-            output_path = input_dir / args.output
-            imageio.v3.imwrite(
-                output_path,
-                images,
-                duration=int(args.duration),
-                loop=args.loop,
-                )
-            print(f'Animation saved at:\n{output_path}\n')
-            if args.colors:
-                gifsicle(
-                    sources=output_path,
-                    # destination=optimized_file,  # replacing in place!
-                    optimize=True, # Whether to add the optimize flag or not
-                    colors=args.colors, # Number of colors to use
-                    # options=["--verbose"]
-                )
-                print(f'Optimized with gifscile for {args.colors} colors.')
-        else:
-            print(f'No PNG images found at:\n{input_dir}')
-    except Exception as e:
-        if str(e).startswith('all input arrays'):
-            print('Select only images of same size.')
-        else:    
-            print(str(e))
-else:
+if not input_dir.is_dir():
     print(f'{input_dir}\nis not a valid input dir.')
+    exit()
+    
+try:
+    images = [imageio.v3.imread(file_path)
+              for file_path in sorted(input_dir.iterdir())
+              if file_path.suffix.lower() == '.png']
+    if images:
+        output_path = input_dir / args.output
+        imageio.v3.imwrite(
+            output_path,
+            images,
+            duration=int(args.duration),
+            loop=args.loop,
+            )
+        print(f'Animation saved at:\n{output_path}\n')
+        if args.colors:
+            gifsicle(
+                sources=output_path,
+                # destination=optimized_file,  # replacing in place!
+                optimize=True, # Whether to add the optimize flag or not
+                colors=args.colors, # Number of colors to use
+                # options=["--verbose"]
+            )
+            print(f'Optimized with gifscile for {args.colors} colors.')
+    else:
+        print(f'No PNG images found at:\n{input_dir}')
+except Exception as e:
+    if str(e).startswith('all input arrays'):
+        print('Select only images of same size.')
+    else:    
+        print(str(e))
+
 
