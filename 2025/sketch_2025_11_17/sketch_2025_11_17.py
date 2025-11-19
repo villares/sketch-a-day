83c0b088243bffb011b55f61655b76eff27cebc1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-11-18 21:50:45 -0300

    Update sketch_2025_11_17.py

--- 2025/sketch_2025_11_17/sketch_2025_11_17.py
+++ 2025/sketch_2025_11_17/sketch_2025_11_17.py
@@ -1,4 +1,5 @@
-import py5   
+import py5
+
 def setup():
     py5.size(500, 500)
     py5.background(0)
@@ -6,12 +7,13 @@ def setup():
     py5.stroke(255)
     for i in range(18):
         N = 36 - i
+        r = 220 - i * 12
         ang = py5.TWO_PI / N
         for j in range(N):
-            x = 250 + py5.cos(ang * j) * (220 - i * 12)
-            y = 250 + py5.sin(ang * j) * (220 - i * 12) 
+            x = 250 + r * py5.cos(ang * j)
+            y = 250 + r * py5.sin(ang * j) 
             py5.point(x, y)
     py5.save('out.png')
 
-py5.run_sketch(block=False)  # remove block=False if on MacOS
+py5.run_sketch(block=False)
 
