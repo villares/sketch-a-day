b098875db2cbe5eec5a4f5a092ad26d6aa152fb3
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-11-23 13:02:25 -0300

    Update sketch_2025_11_22.py

--- 2025/sketch_2025_11_22/sketch_2025_11_22.py
+++ 2025/sketch_2025_11_22/sketch_2025_11_22.py
@@ -1,13 +1,19 @@
+# This code is an example of py5 <py5coding.org> in module mode
 # from https://github.com/py5coding/python-brasil-2025/blob/main/prototypes/revolve.py
 
-import facets
-import numpy as np
 import py5
+import numpy as np
 from trimesh import creation
 # check https://py5coding.org/how_tos/use_processing_libraries.html
-from peasy import PeasyCam  
 
-# pay attention to clockwise vs counterclockwise
+# # check https://py5coding.org/how_tos/use_processing_libraries.html
+# import py5_tools
+# py5_tools.processing.download_library("PeasyCam")
+from peasy import PeasyCam  # the cool & easy Processing orbit camera cam
+
+import facets  # the beautiful edge filtering feature, soon on a py5 release!
+
+# One most pay attention to clockwise vs counterclockwise paths here
 linestring = np.array(
     [[0.0,     0.0], [50.0, -50.0], [100.0, 0.0],
      [100.0, 100.0], [50.0, 150.0], [0.0, 100.0],  
@@ -37,8 +43,7 @@ def setup():
 def draw():
     py5.background(204, 100, 100)
     py5.lights()
-    #py5.translate(py5.width / 2, py5.height / 2, -200)
-    #py5.scale(2)
+    # py5.translate(py5.width / 2, py5.height / 2, -200) # not needed with PeasyCam
     py5.shape(shape)
 
 
