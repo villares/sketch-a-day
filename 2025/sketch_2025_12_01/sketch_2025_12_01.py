2ab09431fa58ff0a2dfe0e9f538912d5f7e3e192
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-12-01 23:44:34 -0300

    1

---
+++ 2025/sketch_2025_12_01/sketch_2025_12_01.py
@@ -0,0 +1,71 @@
+from itertools import product
+
+import py5
+from py5_tools import animated_gif
+
+
+particles = []  
+
+def setup():
+    py5.size(800, 800)
+    py5.color_mode(py5.CMAP, 'viridis_r', 255)
+    py5.background(255)
+    #animated_gif('out.gif', duration=1/30, frame_numbers=range(1, 400, 2))
+
+def draw():
+    py5.fill(0, 10)
+    #py5.rect(0, 0, py5.width, py5.height)
+    for p in particles.copy():
+        p.update()
+    #print(len(particles))
+ 
+def mouse_dragged():
+    if py5.frame_count % 2:
+        particles.append(Particle(py5.mouse_x, py5.mouse_y))
+
+def key_pressed():
+    if py5.key == ' ':
+        particles.clear()
+        py5.background(255)
+    elif py5.key == 'p':
+        py5.save_frame('###.png')
+    elif py5.key == 'r':
+        for x, y in product(range(100, py5.width - 99, 75), repeat=2):
+            for _ in range(3):
+                particles.append(Particle(x, y, py5.random(x / 20, x / 10)))
+ 
+
+class Particle:
+    
+    def __init__(self, x, y, d=None, can_split=True):
+        self.pos = py5.Py5Vector(x, y)
+        self.vel = py5.Py5Vector2D.random() * 2        
+        self.d = d or py5.random(20, 50)
+        self.handedness = py5.random_choice((-1, 1)) 
+        self.can_split = can_split
+         
+    def update(self):
+        self.display()
+        self.move()
+        self.d = self.d * 0.99
+#         if self.d < 20 and self.can_split:
+#             self.can_split = False
+#             particles.append(Particle(self.pos.x, self.pos.y, 20,
+#                                       can_split=False))
+        if self.d < 1:
+            particles.remove(self)
+        
+    def display(self):
+        py5.no_stroke()
+        py5.fill(py5.remap(self.d, 0, 60, 0, 255), 16)
+       # py5.fill(self.cor, py5.random(200, 255))
+        py5.circle(*self.pos, self.d)
+    
+    def move(self):
+        self.pos += self.vel
+        self.pos.x %= py5.width
+        self.pos.y %= py5.height
+        self.vel.rotate(py5.radians(2) * self.handedness)
+        self.vel *= 0.99
+        
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -28,6 +28,16 @@ If you appreciate what I have been doing, you may also support my artistic work,
 
 ---
 
+### sketch_2025_12_01
+
+![sketch_2025_12_01](https://raw.githubusercontent.com/villares/sketch-a-day/main/2025/sketch_2025_12_01/sketch_2025_12_01.png)
+
+[sketch_2025_12_01](https://github.com/villares/sketch-a-day/tree/main/2025/sketch_2025_12_01) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2025_11_30
 
 ![sketch_2025_11_30](https://raw.githubusercontent.com/villares/sketch-a-day/main/2025/sketch_2025_11_30/sketch_2025_11_30.gif)
