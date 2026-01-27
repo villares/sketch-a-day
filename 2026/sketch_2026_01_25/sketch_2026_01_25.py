cbb7f174dd294abbb46673135cd91290b36f0c40
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-01-27 00:54:39 -0300

    clean up and names in English

--- 2026/sketch_2026_01_25/sketch_2026_01_25.py
+++ 2026/sketch_2026_01_25/sketch_2026_01_25.py
@@ -1,74 +1,69 @@
 import py5_tools
 
-axioma = 'X'
-regras = {
+axiom = 'X'
+rules = {
     'X': 'F+[[X0]-FX]-F[[X0]+FX]-X0',
     'F': 'FF',
-    }
-angulo = 22.5
-passo = 8
+}
+angle = 22.5
+step = 8
 num_iter = 5
 
 def setup():
     size(800, 800)
-    gerar_frase_resultado()
+    generate_string()
     
-def gerar_frase_resultado():
-    global resultado
-    frase_inicial = axioma
+def generate_string():
+    global result
+    starting_string = axiom
     for i in range(num_iter):
-        resultado = ''
-        for simbolo in frase_inicial:
-            resultado += regras.get(simbolo, simbolo)
-        frase_inicial = resultado
-    print(len(resultado))
+        result = ''
+        for symbol in starting_string:
+            result += rules.get(symbol, symbol)
+        starting_string = result
+    print(len(result))
     
 def draw():
     background(240, 250, 240)
     translate(400, 800)
-    a = angulo
-    for simbolo in resultado:
-        if simbolo == 'F':
+    for symbol in result:
+        if symbol == 'F':
             stroke(0)
-            line(0, 0, 0, -passo)
-            translate(0, -passo)
-        elif simbolo == '>':
-            a += 1
-        elif simbolo == '<':
-            a -= 1
-        elif simbolo == '-':
-            rotate(radians(a))
-        elif simbolo == '+':
-            rotate(radians(-a))
-        elif simbolo == '[':
+            line(0, 0, 0, -step)
+            translate(0, -step)
+        elif symbol == '-':
+            rotate(radians(angle))
+        elif symbol == '+':
+            rotate(radians(-angle))
+        elif symbol == '[':
             push_matrix()
-        elif simbolo == ']':
+        elif symbol == ']':
             pop_matrix()
-        elif simbolo == '0':
+        elif symbol == '0':
             no_stroke()
             fill(128, 0, 128)
-            circle(0, 0, passo * 0.5)
+            circle(0, 0, step * 0.5)
    
 def key_pressed():  # esperada pelo framework/biblioteca
     # executada quando alguém aperta uma tecla
-    global angulo, passo, num_iter
+    global angle, step, num_iter
     if key == 'a':
-        angulo += 1
+        angle += 1
     elif key == 'z':
-        angulo -= 1
+        angle -= 1
     elif key == 'd':
         num_iter += 1
-        gerar_frase_resultado()
+        generate_string()
         return
     elif key == 'c':
         num_iter -= 1
-        gerar_frase_resultado()
+        generate_string()
         return
     elif key == 's':
-        passo *=  1.1
+        step *=  1.1
     elif key == 'x':
-        passo /= 1.1
+        step /= 1.1
         
-    print(f'ângulo: {angulo} passo: {passo}')
+    print(f'ângulo: {angle} passo: {step}')
         
    
