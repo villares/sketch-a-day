c2da6417b3749da8958976ce9f22d9e810dea207
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-02-02 23:37:22 -0300

    2

---
+++ 2026/sketch_2026_02_02/sketch_2026_02_02.py
@@ -0,0 +1,118 @@
+from functools import cache
+
+import py5
+import numpy as np
+from shapely import Polygon
+
+vs = []
+shapes = []
+areas = []
+
+def setup():
+    py5.size(800, 800)
+    py5.color_mode(py5.CMAP, 'viridis')
+    start()
+    
+def start():
+    global max_area, group
+    max_area = 1
+    vs[:] = (
+        np.array(pt) for pt in
+        ((0, 0), (py5.width, 0), (py5.width, py5.height), (0, py5.height))
+    )
+    shapes.clear()
+    shapes.append((0, 1, 2, 3))
+    # this can't be done before setup()...
+    group = py5.create_shape(py5.GROUP)  
+    
+def draw():
+    py5.background(0)
+    py5.shape(group)
+    py5.window_title(str(round(py5.get_frame_rate(), 1)))
+    # TODO: try a Py5Shape.GROUP or maybe
+    # py5.convert_cached_shape(shapely.MultiPolygon...) ?
+
+def split_shapes():
+    global group    
+    new_shapes = []
+    while shapes:
+        shp = shapes.pop()
+        if len(shp) == 4:
+            a, b, c, d = shp            
+            ac = py5.dist(*vs[a], *vs[d])
+            bd = py5.dist(*vs[b], *vs[c])
+            if ac < bd:
+                new_shapes.append((a, b, c))
+                new_shapes.append((a, c, d))
+            else:
+                new_shapes.append((a, b, d))
+                new_shapes.append((b, c, d))  
+        elif py5.random(100) < 50:
+            a, b, c = shp
+            abi = len(vs)            
+            vs.append((vs[a] + vs[b]) / 2)
+            bci = len(vs)            
+            vs.append((vs[b] + vs[c]) / 2)
+            aci = len(vs)            
+            vs.append((vs[a] + vs[c]) / 2)
+            new_shapes.append((a, abi, aci))
+            new_shapes.append((abi, b, bci, aci))
+            new_shapes.append((bci, c, aci))
+        else:
+            a, b, c = shp
+            ab = py5.dist(*vs[a], *vs[b])
+            bc = py5.dist(*vs[b], *vs[c])
+            i = len(vs)
+            if ab > bc:
+                vs.append((vs[a] + vs[b]) / 2)
+                new_shapes.append((a, i, c))
+                new_shapes.append((i, b, c))
+            elif bc > ab:
+                vs.append((vs[b] + vs[c]) / 2)
+                new_shapes.append((b, i, a))
+                new_shapes.append((i, c, a))               
+            else:
+                vs.append((vs[a] + vs[c]) / 2)
+                new_shapes.append((a, i, b))
+                new_shapes.append((i, c, b))
+#         else:
+#             new_shapes.append(shp)
+    shapes[:] = new_shapes
+    areas[:] = (shape_area(shp) for shp in shapes)
+    
+    group = py5.create_shape(py5.GROUP)
+    for shp, area in zip(shapes, areas):
+        poly = py5.create_shape()
+        with poly.begin_closed_shape():
+            poly.vertices(np.array(vs)[np.array(shp)])
+        poly.set_fill(py5.color(area / max_area))
+        group.add_child(poly)
+    return max(areas)
+
+@cache
+def shape_centroid(shp):
+    pts = np.array(vs)[np.array(shp)]
+    return sum(pts) / len(pts)
+  
+@cache
+def shape_area(shp):
+    return pts_area(np.array(vs)[np.array(shp)])
+    
+def pts_area(pts):
+    area = 0.0
+    rolled = np.roll(pts, -1, axis=0)
+    for (ax, ay), (bx, by) in zip(pts, rolled):
+        area += ax * by
+        area -= bx * ay
+    return abs(area) / 2.0
+
+def key_pressed():
+    global max_area
+    if py5.key == ' ':
+        max_area = split_shapes()
+    elif py5.key == 'r':
+        start()
+    elif py5.key == 'p':
+        py5.save_frame('###.png')
+        
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -29,6 +29,16 @@ You can follow me on Mastodon [@villares@ciberlandia.pt](https://ciberlandia.pt/
 
 ---
 
+### sketch_2026_02_02
+
+![sketch_2026_02_02](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_02_02/sketch_2026_02_02.png)
+
+[sketch_2026_02_02](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_02_02) [[py5](https://py5coding.org/)]
+
+WIP...
+
+---
+
 ### sketch_2026_02_01
 
 ![sketch_2026_02_01](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_02_01/sketch_2026_02_01.png)
