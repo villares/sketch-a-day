d214de41db6efaf9f7c7bace6c01c02e8f0d17e4
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-02-07 22:26:50 -0300

    7

---
+++ 2026/sketch_2026_02_07/sketch_2026_02_07.py
@@ -0,0 +1,112 @@
+from functools import cache
+
+import py5
+import numpy as np
+from shapely import Polygon
+
+vs = []
+shapes = []
+areas = []
+
+def setup():
+    py5.size(800, 800)
+    py5.color_mode(py5.HSB)
+    py5.stroke_join(py5.ROUND)
+    start()
+    
+def start():
+    global group
+    vs[:] = (
+        np.array(pt) for pt in
+        ((0, 0), (py5.width, 0), (py5.width, py5.height), (0, py5.height))
+    )
+    shapes.clear()
+    shapes.append((0, 1, 2, 3))
+    # this can't be done before setup()...
+    group = py5.create_shape(py5.GROUP)  
+    
+def draw():
+    py5.background(0)
+    py5.shape(group)
+    py5.window_title(str(round(py5.get_frame_rate(), 1)))
+    # TODO: try a Py5Shape.GROUP or maybe
+    # py5.convert_cached_shape(shapely.MultiPolygon...) ?
+
+def split_shapes():
+    global group    
+    new_shapes = []
+    while shapes:
+        shp = shapes.pop()
+        if len(shp) == 4:
+            a, b, c, d = shp            
+            ac = py5.dist(*vs[a], *vs[c])
+            bd = py5.dist(*vs[b], *vs[d])
+            if ac < bd:
+                new_shapes.append((a, b, c))
+                new_shapes.append((a, c, d))
+            else:
+                new_shapes.append((a, b, d))
+                new_shapes.append((b, c, d))
+            continue
+        a, b, c = shp
+        ab = py5.dist(*vs[a], *vs[b])
+        bc = py5.dist(*vs[b], *vs[c])
+        ca = py5.dist(*vs[c], *vs[a])
+        if ab == bc:
+            abi = len(vs)            
+            vs.append((vs[a] + vs[b]) / 2)
+            bci = len(vs)            
+            vs.append((vs[b] + vs[c]) / 2)
+            aci = len(vs)            
+            vs.append((vs[a] + vs[c]) / 2)
+#             new_shapes.append((a, abi, aci))
+#             new_shapes.append((abi, b, bci, aci))
+            new_shapes.append((a, b, bci, aci))            
+            new_shapes.append((bci, c, aci))
+        else:
+            i = len(vs)
+            #r = py5.random_int(2)
+            if ab > bc:
+                vs.append((vs[a] + vs[b]) / 2)
+                new_shapes.append((a, i, c))
+                new_shapes.append((i, b, c))
+            if bc > ab:
+                vs.append((vs[b] + vs[c]) / 2)
+                new_shapes.append((b, i, a))
+                new_shapes.append((i, c, a))               
+            else:
+                vs.append((vs[a] + vs[c]) / 2)
+                new_shapes.append((a, i, b))
+                new_shapes.append((i, c, b))
+#         else:
+#             new_shapes.append(shp)
+    shapes[:] = new_shapes
+    areas[:] = (shape_area(shp) for shp in shapes)
+    max_area = max(areas)
+    
+    group = py5.create_shape(py5.GROUP)
+    for shp, area in zip(shapes, areas):
+        poly = py5.create_shape()
+        with poly.begin_closed_shape():
+            poly.vertices(np.array(vs)[np.array(shp)])
+        c = py5.color(area / 50 % 255,
+                      255,
+                      55 + 200 * area / max_area)
+        poly.set_fill(c)
+        group.add_child(poly)
+    
+  
+def shape_area(shp):
+    return Polygon(
+        np.array(vs)[np.array(shp)]
+    ).area
+        
+def key_pressed():
+    if py5.key == ' ':
+        split_shapes()
+    elif py5.key == 'r':
+        start()
+    elif py5.key == 'p':
+        py5.save_frame('###b.png')
+        
+py5.run_sketch(block=False)
