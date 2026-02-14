accd7a1bedb2407bb102aa3f4a0cb93bfb0851e3
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-02-13 22:50:50 -0300

    13

---
+++ 2026/sketch_2026_02_13/sketch_2026_02_13.py
@@ -0,0 +1,110 @@
+import py5
+import numpy as np
+from shapely import Polygon
+
+vs = []
+shapes = []
+areas = []
+
+def setup():
+    py5.size(800, 800, py5.P2D)
+    py5.color_mode(py5.HSB)
+    py5.stroke_weight(2)
+    start() 
+    
+def start():
+    global group
+    vs[:] = (
+        np.array(pt) for pt in
+        ((0, 0), (py5.width, 0), (py5.width, py5.height), (0, py5.height))
+    )
+    shapes.clear()
+    shapes.append((0, 1, 2, 3))
+    # this can't be done before setup()...
+    group = py5.create_shape(py5.GROUP)  
+    
+def draw():
+    py5.background(0)
+    py5.shape(group)
+    py5.window_title(str(round(py5.get_frame_rate(), 1)))
+  
+def split_shapes():
+    global group    
+    new_shapes = []
+    while shapes:
+        shp = shapes.pop()
+        if len(shp) == 4:
+            a, b, c, d = shp            
+            ac = py5.dist(*vs[a], *vs[c])
+            bd = py5.dist(*vs[b], *vs[d])
+            if ac < bd:
+                new_shapes.append((a, b, c))
+                new_shapes.append((a, c, d))
+            else:
+                new_shapes.append((a, b, d))
+                new_shapes.append((b, c, d))
+            continue
+        a, b, c = shp
+        ab = py5.dist(*vs[a], *vs[b])
+        bc = py5.dist(*vs[b], *vs[c])
+        ca = py5.dist(*vs[c], *vs[a])
+        if py5.random(100) > 50:
+            abi = len(vs)            
+            vs.append((vs[a] + vs[b]) / 2)
+            bci = len(vs)            
+            vs.append((vs[b] + vs[c]) / 2)
+            aci = len(vs)            
+            vs.append((vs[a] + vs[c]) / 2)
+            new_shapes.append((a, abi, aci))
+            new_shapes.append((abi, b, bci, aci))
+#             new_shapes.append((a, b, bci, aci))            
+            new_shapes.append((bci, c, aci))
+        else:
+            i = len(vs)
+            #r = py5.random_int(2)
+            if ab > bc:
+                vs.append((vs[a] + vs[b]) / 2)
+                new_shapes.append((a, i, c))
+                new_shapes.append((i, b, c))
+            if bc > ab:
+                vs.append((vs[b] + vs[c]) / 2)
+                new_shapes.append((b, i, a))
+                new_shapes.append((i, c, a))               
+            else:
+                vs.append((vs[a] + vs[c]) / 2)
+                new_shapes.append((a, i, b))
+                new_shapes.append((i, c, b))
+#         else:
+#             new_shapes.append(shp)
+    shapes[:] = new_shapes
+    areas[:] = (shape_area(shp) for shp in shapes)
+    max_area = max(areas)
+    
+    group = py5.create_shape(py5.GROUP)
+    for shp, area in zip(shapes, areas):
+        poly = py5.create_shape()
+        pts = np.array(vs)[np.array(shp)]
+        with poly.begin_closed_shape():
+            poly.vertices(pts)
+        n = len(pts)
+        m = sum(pts) / n
+        cs = [py5.color(255) if n == 3
+              else py5.color(area % 255, 255, 255)
+              for i in range(n)]
+        poly.set_fills(cs)
+        group.add_child(poly)
+    
+def shape_area(shp):
+    return Polygon(
+        np.array(vs)[np.array(shp)]
+    ).area
+        
+def key_pressed():
+    if py5.key == ' ':
+        split_shapes()
+    elif py5.key == 'r':
+        start()
+    elif py5.key == 'p':
+        py5.save_frame('###b.png')
+        
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -29,6 +29,16 @@ You can follow me on Mastodon [@villares@ciberlandia.pt](https://ciberlandia.pt/
 
 ---
 
+### sketch_2026_02_13
+
+![sketch_2026_02_13](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_02_13/sketch_2026_02_13.png)
+
+[sketch_2026_02_13](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_02_13) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2026_02_12
 
 ![sketch_2026_02_12](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_02_12/sketch_2026_02_12.gif)
