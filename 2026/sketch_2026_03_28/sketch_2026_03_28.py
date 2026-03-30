b7bc5ab4dccd4c49ccb46f697599e57003ad93d3
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-03-29 21:27:26 -0300

    Update sketch_2026_03_28.py

--- 2026/sketch_2026_03_28/sketch_2026_03_28.py
+++ 2026/sketch_2026_03_28/sketch_2026_03_28.py
@@ -5,7 +5,6 @@ import py5
 import py5_tools
 import shapely
 
-
 from matplotlib.font_manager import FontProperties
 from matplotlib.textpath import TextPath
 from py5.shape_conversion import textpath_to_shapely_converter as tp_to_sly
@@ -16,20 +15,19 @@ polys = (tp_to_sly(TextPath((20, -120), "tosconf", prop=fp)) |
 
 def setup():
     py5.size(500, 500)
+    py5.no_stroke()
     py5_tools.animated_gif('out.gif',
                             frame_numbers=range(1, 37),
                             duration=0.03) 
  
 def draw():
     py5.background(0)
+    w = py5.width
     h = 26
     gap = 10
     start = py5.frame_count % (h + gap)
-    w = py5.width
-    py5.no_stroke()
-    for y in range(-start, py5.width, h + gap):
-        pontos = ((0, y), (w, y),
-                  (w, y + h), (0, y + h))
+    for y in range(-start, py5.height, h + gap):
+        pontos = ((0, y), (w, y), (w, y + h), (0, y + h))
         p = shapely.Polygon(pontos).buffer(-1)
         pi = polys.intersection(p).buffer(5)
         py5.color_mode(py5.HSB)
