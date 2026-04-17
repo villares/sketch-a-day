5a84b5c0a52e728d7412660368ca128db43914d7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-04-17 16:56:10 -0300

    Update sketch_2026_04_17.py

--- 2026/sketch_2026_04_17/sketch_2026_04_17.py
+++ 2026/sketch_2026_04_17/sketch_2026_04_17.py
@@ -1,7 +1,6 @@
 # This is a py5 "module mode" sketch
 # learn about py5 modes at https://py5coding.org
 
-
 import py5
 import numpy as np
 from cmath import log as clog
@@ -23,15 +22,16 @@ def setup():
 def draw():
     py5.translate(300, 300)
     py5.background(0)
+    # draw the basic grid in gray
     py5.stroke(128)
     py5.points(xy_pts)
-
+    # calculate the new grid
     c_pts = xy_pts[:, 0] + 1j * xy_pts[:, 1]
-    non_zero = c_pts != 0
+    non_zero = c_pts != 0  # a mask to remove zeros 
     c_log = np.zeros_like(c_pts, dtype=complex)
     c_log[non_zero] = np.log(c_pts[non_zero])
     log_xy_pts = np.column_stack((c_log.real, c_log.imag))
-    
+    # draw the new grid in green
     py5.stroke(0, 255, 0)
     py5.points(log_xy_pts * 90 - (300, 0))
 
