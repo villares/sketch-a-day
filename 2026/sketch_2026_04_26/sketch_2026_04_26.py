afd62ac1003dd232b6c31201627052c493b839fb
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-04-26 23:58:33 -0300

    26

---
+++ 2026/sketch_2026_04_26/sketch_2026_04_26.py
@@ -0,0 +1,86 @@
+import py5
+import shapely
+
+star_data = [
+    {'x': 400, 'y': 100, 'n': 5, 'ra': 50, 'rb': 10, 'buffer': 0},
+    {'x': 200, 'y': 200, 'n': 7, 'ra': 150,'rb': 100,'buffer': 0},
+    {'x': 350, 'y': 400, 'n': 20,'ra': 80, 'rb': 40, 'buffer': 0},
+]
+picked_star = None
+
+def setup():
+    py5.size(500, 500)
+    py5.color_mode(py5.CMAP, 'viridis', 500 * 100)
+    
+    
+def draw():
+    global under_mouse
+    py5.background('black')
+    mx, my = py5.mouse_x, py5.mouse_y
+    under_mouse = None
+    for i, sd in enumerate(star_data):
+        shp = star(**sd)
+        py5.no_stroke()
+        inside = py5.dist(sd['x'], sd['y'], mx, my) < max(sd['ra'], sd['rb']) \
+                 or shp.contains(shapely.Point(mx, my)) 
+        if inside:
+            under_mouse = i
+            py5.no_fill()
+            py5.stroke('red')
+            py5.circle(sd['x'], sd['y'],  max(sd['ra'], sd['rb']) * 2)
+        py5.fill(shp.area)
+        py5.shape(shp)
+          
+def star(x, y, n, ra, rb, buffer=0, rot=0):    
+    passo = py5.TWO_PI / n
+    vs = [] 
+    for i in range(n): 
+        angulo = i * passo + rot
+        vx = x + ra * py5.sin(angulo)
+        vy = y + ra * py5.cos(angulo)
+        vs.append((vx, vy))
+        vx = x + rb * py5.sin(angulo + passo / 2)
+        vy = y + rb * py5.cos(angulo + passo / 2)
+        vs.append((vx, vy)) 
+    return shapely.Polygon(vs).buffer(buffer)
+    
+def mouse_pressed():
+    global picked_star
+    picked_star = under_mouse
+    
+def mouse_dragged():
+    if picked_star is not None:
+        sd = star_data[picked_star]
+        sd['x'] += py5.mouse_x - py5.pmouse_x
+        sd['y'] += py5.mouse_y - py5.pmouse_y
+
+def mouse_released():
+    global picked_star
+    picked_star = None
+    
+def mouse_wheel(e):
+    if under_mouse is not None:
+        sd = star_data[under_mouse]
+        if e.is_control_down():
+            sd['buffer'] += e.get_count()
+        elif e.is_shift_down():
+            sd['rb'] += e.get_count()
+        else:
+            sd['ra'] += e.get_count()
+ 
+
+def key_pressed():
+    if py5.key == 'p':
+        py5.save_frame('out.png')
+    elif py5.key == 's':
+        star_data.append(
+            {'x': py5.mouse_x, 'y': py5.mouse_y,
+             'n': 4, 'ra': 50, 'rb': 10, 'buffer': 0},
+            )
+
+py5.run_sketch(block=False)
+        
+        
+    
+    
+    
--- docs/README.md
+++ docs/README.md
@@ -27,6 +27,16 @@ You can follow me on Mastodon [@villares@ciberlandia.pt](https://ciberlandia.pt/
 
 ---
 
+### sketch_2026_04_26
+
+![sketch_2026_04_26](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_04_26/sketch_2026_04_26.png)
+
+[sketch_2026_04_26](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_04_26) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2026_04_25
 
 ![sketch_2026_04_25](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_04_25/sketch_2026_04_25.png)
