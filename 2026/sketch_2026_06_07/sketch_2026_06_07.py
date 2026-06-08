5b23d1c3c71c930830b5ca92159288162aed5b5b
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-06-08 16:17:31 -0300

    sketch_2026_06_07

---
+++ 2026/sketch_2026_06_07/sketch_2026_06_07.py
@@ -0,0 +1,29 @@
+# This is a py5 "module mode" sketch
+# learn about py5 modes at https://py5coding.org
+
+"""
+inspired by https://freeradical.zone/@bitartbot/116696573113492478
+f(x,y) = ((((y % 9) * (~y)) & (-(x & x))) ^ (~((-y) ^ (-y)))) % 6
+"""
+import py5
+
+def f(x, y):
+    return ((((y % 9) * (~y)) & (-(x & x))) ^ (~((-y) ^ (-y)))) % 6
+
+def setup():
+    py5.size(800, 800)
+    py5.no_smooth()
+    py5.no_stroke()
+    py5.background(255)
+    n = 4
+    w =  int(py5.width / n) 
+    for x in range(w):
+        for y in range(w):
+            r = f(x, y) 
+            py5.fill((r % 5) * (255/4))
+            py5.square(x * n, y * n, n)
+
+    py5.save_frame('out.png')
+
+
+py5.run_sketch(block=False)
