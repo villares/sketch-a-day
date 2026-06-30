78de80067c76995fffa9d7d88b9ddf67c4eb85a1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-06-30 00:24:57 -0300

    sketch_2026_06_29

---
+++ 2026/sketch_2026_06_29/sketch_2026_06_29.py
@@ -0,0 +1,64 @@
+from itertools import product
+
+import numpy as np
+import py5
+import shapely
+
+grid_points = list(product(range(-450, 451, 100), range(-400, 401, 100)))
+scale_factor = 2
+
+def setup():
+    global out
+    py5.size(1000, 900)
+    py5.stroke_join(py5.ROUND)
+    n = len(grid_points)
+    w = np.linspace(20, 40, n)
+    h = np.linspace(60, 0, n)
+    angs = np.linspace(0, py5.TAU, n)
+    star_infos = list(zip(grid_points, w, h, angs))
+    out = py5.create_graphics(py5.width * scale_factor, py5.height * scale_factor)
+    py5.begin_record(out)
+    out.scale(scale_factor)
+    py5.translate(py5.width / 2, py5.height / 2)
+    py5.background(255)
+    py5.no_fill()
+    py5.stroke(150)
+    py5.stroke_weight(1)
+    for pos, w, h, _ in star_infos:
+        with py5.push_matrix():
+            py5.translate(*pos)
+            s = star(0, 0, 10, w, h, buffer=pos[0]/100)
+            for i in range(-10, 11, 1):
+                if i != 0:
+                    py5.shape(s.buffer(i * 4))
+    py5.stroke_weight(2)
+    for pos, w, h, rot in star_infos:
+        b = py5.remap(rot, 0, py5.TAU, 0, 255)
+        py5.stroke(h * 5, abs(pos[1] / 4), b / 2)
+        py5.shape(star(*pos, 10, w, h, buffer=pos[0]/100))
+        
+    py5.end_record()
+    
+        
+def star(x, y, n, ra, rb, buffer=0, rot=0):    
+    passo = py5.TWO_PI / n
+    vs = [] 
+    for i in range(n): 
+        angulo = i * passo + rot
+        vx = x + ra * py5.sin(angulo)
+        vy = y + ra * py5.cos(angulo)
+        vs.append((vx, vy))
+        vx = x + rb * py5.sin(angulo + passo / 2)
+        vy = y + rb * py5.cos(angulo + passo / 2)
+        vs.append((vx, vy)) 
+    return shapely.Polygon(vs).buffer(buffer)
+    
+    
+def key_pressed():
+    if py5.key == 'h':
+        out.save(f'outg-x{scale_factor}.png')
+    else:
+        py5.save('out-b.png')
+    
+py5.run_sketch(block=False)
+
