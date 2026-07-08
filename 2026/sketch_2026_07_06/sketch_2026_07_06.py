ce1d536ceb04a2ff68b3a84a6720ca6fe2a084f2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-07-08 11:11:56 -0300

    Create sketch_2026_07_06.py

---
+++ 2026/sketch_2026_07_06/sketch_2026_07_06.py
@@ -0,0 +1,72 @@
+# This is a py5 "module mode" sketch
+# learn about py5 modes at https://py5coding.org
+
+# zoom & pan transformation values
+zpt = {'x': 0, 'y': 0, 'scale': 1, 'amount': 0}  
+
+import py5
+from functools import cache
+
+tilt = 0
+
+@cache
+def f(x, y):
+    return (y % 9 * y & x ^ y) % 6
+
+def setup():
+    py5.size(800, 800, py5.P3D)
+    py5.no_smooth()
+    #py5.no_stroke()
+    
+def draw():
+    py5.ortho()
+    #py5.lights()
+    py5.background('black')
+    with py5.push_matrix():
+        py5.translate(zpt['x'], zpt['y'])
+        py5.scale(zpt['scale'])
+        py5.stroke_weight(1 / zpt['scale'])
+        py5.stroke('gray')
+        py5.translate(py5.width * 0.45, py5.height * 0.3, -3s50)
+        py5.rotate_x(py5.radians(tilt)) #py5.mouse_y))
+        py5.rotate_z(py5.radians(tilt))
+        py5.translate(-py5.width / 2, -py5.height / 2)    
+        py5.color_mode(py5.CMAP, 'viridis', 255)
+        #py5.background(0)
+        n = 8
+        w =  int(py5.width / n)
+        for x in range(w):
+            for y in range(w):
+                r = f(x, y)
+                py5.fill(r * (255 / 5))
+                box(x * n, y * n, n * (r + 1) / 3, n, n, n / 2)
+
+def box(x, y, z, w, h=None, d=None):
+    h = h or w
+    d = d or h
+    with py5.push_matrix():
+        py5.translate(x, y, z)
+        py5.box(w, h, d)
+
+def key_pressed():
+    global tilt
+    #py5.save_frame(f'out###-{py5.millis()}.png')
+    if py5.key == 'a':
+        tilt += 5
+    if py5.key == 'z':
+        tilt -= 5
+
+def mouse_wheel(e):
+    xrd = (py5.mouse_x - zpt['x']) / zpt['scale']
+    yrd = (py5.mouse_y - zpt['y']) / zpt['scale']
+    zpt['amount'] -= e.get_count()
+    zpt['scale'] = 1.1 ** zpt['amount']
+    zpt['x'] = int(py5.mouse_x - xrd * zpt['scale'])
+    zpt['y'] = int(py5.mouse_y - yrd * zpt['scale'])
+
+def mouse_dragged():
+    zpt['x'] += py5.mouse_x - py5.pmouse_x
+    zpt['y'] += py5.mouse_y - py5.pmouse_y
+
+
+py5.run_sketch(block=False)
