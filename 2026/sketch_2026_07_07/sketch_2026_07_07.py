e7358b944421590bf1bf33381c70861e96f940a2
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-07-09 09:18:51 -0300

    sketch_2026_07_07

--- 2026/sketch_2026_07_07/sketch_2026_07_07.py
+++ 2026/sketch_2026_07_07/sketch_2026_07_07.py
@@ -1,72 +1,60 @@
-# This is a py5 "module mode" sketch
-# learn about py5 modes at https://py5coding.org
+from itertools import product
 
-# zoom & pan transformation values
-zpt = {'x': 0, 'y': 0, 'scale': 1, 'amount': 0}  
-
+import numpy as np
 import py5
-from functools import cache
+from py5_tools import animated_gif
+import shapely
 
-tilt = 0
 
-@cache
-def f(x, y):
-    return (y % 9 * y & x ^ y) % 6
+grid_points = list(product(range(-450, 451, 100), range(-400, 401, 100)))
+scale_factor = 5
+
 
 def setup():
-    py5.size(800, 800, py5.P3D)
-    py5.no_smooth()
-    #py5.no_stroke()
+    global out, star_infos
+    py5.size(1000, 900)
+    n = len(grid_points)
+    w = np.linspace(20, 40, n)
+    h = np.linspace(80, 0, n)
+    angs = np.linspace(0, py5.TAU, n)
+    star_infos = list(zip(grid_points, w, h, angs))
+    animated_gif('out.gif', frame_numbers=range(1, 181, 5), duration=0.27)
     
 def draw():
-    py5.ortho()
-    #py5.lights()
-    py5.background('black')
+    py5.translate(py5.width / 2, py5.height / 2)
+    py5.background(0)
+    py5.no_stroke()
+    for ri in star_infos:
+        draw_star(ri)
+    
+def draw_star(star_info):
+    pos, w, h, rot = star_info
+    f = py5.frame_count
+    b = py5.remap(rot, 0, py5.TAU, 0, 255)
+    py5.fill((h * 10 + f / 180 * 255) % 255, abs(pos[1] / 2), b)
     with py5.push_matrix():
-        py5.translate(zpt['x'], zpt['y'])
-        py5.scale(zpt['scale'])
-        py5.stroke_weight(1 / zpt['scale'])
-        py5.stroke('gray')
-        py5.translate(py5.width * 0.45, py5.height * 0.3, -3s50)
-        py5.rotate_x(py5.radians(tilt)) #py5.mouse_y))
-        py5.rotate_z(py5.radians(tilt))
-        py5.translate(-py5.width / 2, -py5.height / 2)    
-        py5.color_mode(py5.CMAP, 'viridis', 255)
-        #py5.background(0)
-        n = 8
-        w =  int(py5.width / n)
-        for x in range(w):
-            for y in range(w):
-                r = f(x, y)
-                py5.fill(r * (255 / 5))
-                box(x * n, y * n, n * (r + 1) / 3, n, n, n / 2)
-
-def box(x, y, z, w, h=None, d=None):
-    h = h or w
-    d = d or h
-    with py5.push_matrix():
-        py5.translate(x, y, z)
-        py5.box(w, h, d)
-
+        py5.translate(*pos)
+        py5.rotate(py5.radians(f * 2))
+        py5.shape(star(0, 0, 10, w, h, buffer=pos[0]/100 ))  
+    
+def star(x, y, n, ra, rb, buffer=0, rot=0):    
+    passo = py5.TWO_PI / n
+    vs = [] 
+    for i in range(n): 
+        angulo = i * passo + rot
+        vx = x + ra * py5.sin(angulo)
+        vy = y + ra * py5.cos(angulo)
+        vs.append((vx, vy))
+        vx = x + rb * py5.sin(angulo + passo / 2)
+        vy = y + rb * py5.cos(angulo + passo / 2)
+        vs.append((vx, vy)) 
+    return shapely.Polygon(vs).buffer(buffer)
+    
+    
 def key_pressed():
-    global tilt
-    #py5.save_frame(f'out###-{py5.millis()}.png')
-    if py5.key == 'a':
-        tilt += 5
-    if py5.key == 'z':
-        tilt -= 5
-
-def mouse_wheel(e):
-    xrd = (py5.mouse_x - zpt['x']) / zpt['scale']
-    yrd = (py5.mouse_y - zpt['y']) / zpt['scale']
-    zpt['amount'] -= e.get_count()
-    zpt['scale'] = 1.1 ** zpt['amount']
-    zpt['x'] = int(py5.mouse_x - xrd * zpt['scale'])
-    zpt['y'] = int(py5.mouse_y - yrd * zpt['scale'])
-
-def mouse_dragged():
-    zpt['x'] += py5.mouse_x - py5.pmouse_x
-    zpt['y'] += py5.mouse_y - py5.pmouse_y
-
-
+    if py5.key == 'h':
+        out.save(f'out-x{scale_factor}.png')
+    else:
+        py5.save('out-c.png')
+    
 py5.run_sketch(block=False)
