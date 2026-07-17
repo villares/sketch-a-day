73452f86ad3646309f6e5ef7b06abba062c69c59
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-07-16 23:25:20 -0300

    Update sketch_2026_07_15.py

--- 2026/sketch_2026_07_15/sketch_2026_07_15.py
+++ 2026/sketch_2026_07_15/sketch_2026_07_15.py
@@ -32,7 +32,7 @@ def draw():
     py5.background('black')
     py5.translate(py5.width / 2 , py5.height / 2)
     py5.rotate_x(py5.radians(30))
-    py5.translate(-py5.width / 2 , -py5.height / 2 - 7p0)
+    py5.translate(-py5.width / 2 , -py5.height / 2 - 70)
     py5.shape(group)
     py5.window_title(str(round(py5.get_frame_rate(), 1)))
     # TODO: try a Py5Shape.GROUP or maybe
