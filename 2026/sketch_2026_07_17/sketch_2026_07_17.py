31dfe438853599e40fd9b68a1e472f8332c615f7
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-07-17 23:18:25 -0300

    sketch_2026_07_17

---
+++ 2026/sketch_2026_07_17/sketch_2026_07_17.py
@@ -0,0 +1,132 @@
+# This is a py5 "module mode" sketch
+# learn about py5 modes at https://py5coding.org
+
+import py5
+import numpy as np
+import shapely
+import trimesh 
+
+vs = []
+shapes = []
+areas = []
+
+def setup():
+    py5.size(800, 800, py5.P3D)
+    py5.color_mode(py5.CMAP, 'plasma')
+    start()
+    
+def start():
+    global max_area, group
+    max_area = 1
+    vs[:] = (
+        np.array(pt) for pt in
+        ((0, 0), (py5.width, 0), (py5.width, py5.height), (0, py5.height))
+    )
+    shapes.clear()
+    shapes.append((0, 1, 2, 3))
+    # this can't be done before setup()...
+    group = py5.create_shape(py5.GROUP)  
+    
+def draw():
+    py5.background('black')
+    py5.translate(py5.width / 2 , py5.height / 2, -200)
+    py5.rotate_x(py5.radians(0))
+    py5.translate(-py5.width / 2 , -py5.height / 2 - 30)
+    py5.shape(group)
+    py5.window_title(str(round(py5.get_frame_rate(), 1)))
+    # TODO: try a Py5Shape.GROUP or maybe
+    # py5.convert_cached_shape(shapely.MultiPolygon...) ?
+
+def split_shapes():
+    global group, max_area 
+    new_shapes = []
+    while shapes:
+        shp = shapes.pop()
+        if len(shp) == 4:
+            a, b, c, d = shp            
+            ac = py5.dist(*vs[a], *vs[d])
+            bd = py5.dist(*vs[b], *vs[c])
+            if ac <= bd:
+                new_shapes.append((a, b, c))
+                new_shapes.append((a, c, d))
+            else:
+                if not py5.is_key_pressed:
+                    new_shapes.append(shp)
+                else:
+                    new_shapes.append((a, b, d))
+                    new_shapes.append((b, c, d))  
+        elif py5.random(100) < 50:
+            a, b, c = shp
+            abi = len(vs)            
+            vs.append((vs[a] + vs[b]) / 2)
+            bci = len(vs)            
+            vs.append((vs[b] + vs[c]) / 2)
+            aci = len(vs)            
+            vs.append((vs[a] + vs[c]) / 2)
+            new_shapes.append((a, abi, aci))
+            new_shapes.append((abi, b, bci, aci))
+            new_shapes.append((bci, c, aci))
+        else:
+            a, b, c = shp
+            ab = py5.dist(*vs[a], *vs[b])
+            bc = py5.dist(*vs[b], *vs[c])
+            i = len(vs)
+            if ab > bc:
+                vs.append((vs[a] + vs[b]) / 2)
+                new_shapes.append((a, i, c))
+                new_shapes.append((i, b, c))
+            elif bc > ab:
+                vs.append((vs[b] + vs[c]) / 2)
+                new_shapes.append((b, i, a))
+                new_shapes.append((i, c, a))               
+            else:
+                vs.append((vs[a] + vs[c]) / 2)
+                new_shapes.append((a, i, b))
+                new_shapes.append((i, c, b))
+#         else:
+#             new_shapes.append(shp)
+    shapes[:] = new_shapes
+    areas[:] = (shape_area(shp) for shp in shapes)
+    max_area = max(areas)
+    group = py5.create_shape(py5.GROUP)
+    scene = trimesh.scene.Scene()
+    
+    for shp, area in zip(shapes, areas):
+        p = shapely.Polygon(np.array(vs)[np.array(shp)])
+        ar = area / max_area
+        t = trimesh.creation.extrude_polygon(p, 25)
+        t.apply_translation((0, 0, -ar * 75))
+        s = py5.convert_shape(t)
+        if len(shp) == 4:
+            a, b, c, d = shp            
+            ac = py5.dist(*vs[a], *vs[c])
+            bd = py5.dist(*vs[b], *vs[d])
+            #if ac == bd:
+            #    continue #s.set_fill(py5.color('black'))                
+            s.set_fill(py5.color(ar))
+        else:
+            s.set_fill(py5.color(ar))
+        group.add_child(s)
+    
+  
+#@cache
+def shape_area(shp):
+    return pts_area(np.array(vs)[np.array(shp)])
+    
+def pts_area(pts):
+    area = 0.0
+    rolled = np.roll(pts, -1, axis=0)
+    for (ax, ay), (bx, by) in zip(pts, rolled):
+        area += ax * by
+        area -= bx * ay
+    return abs(area) / 2.0
+
+def key_pressed():
+    if py5.key == ' ':
+        split_shapes()
+    elif py5.key == 'r':
+        start()
+    elif py5.key == 'p':
+        py5.save_frame('###.png')
+        
+py5.run_sketch(block=False)
