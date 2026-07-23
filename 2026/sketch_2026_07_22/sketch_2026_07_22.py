2aff7dc4f9211517b5a0f2b8826cccff0984631f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-07-22 23:38:07 -0300

    sketch_2026_07_22

---
+++ 2026/sketch_2026_07_22/sketch_2026_07_22.py
@@ -0,0 +1,41 @@
+import py5
+
+from py5_tools import animated_gif
+from shapely import Point
+
+
+def setup():
+    global p
+    py5.size(640, 640)
+    animated_gif('out.gif', frame_numbers=range(1, 101), duration=0.1)
+    p = Point(256, 256).buffer(128) | Point(384, 384).buffer(128)
+    
+    
+def draw():
+    py5.background(100, 100, 128)
+    py5.no_fill()
+    #py5.shape(p)
+    py5.fill(255)
+    #t = py5.remap(py5.mouse_x, 0, py5.width, 0, 1)
+    t = py5.frame_count / 100
+    v = p.exterior.interpolate(t, normalized=True)
+    py5.fill(v.x / 3, v.y / 3, 255)
+    py5.no_stroke()
+    py5.circle(v.x, v.y, 20 + v.x * 20 / v.y)
+
+def key_pressed():
+    if py5.key == 's':
+        py5.save_frame('#####.png')
+
+
+def lerp_along_points(amt, pts):
+    # Based on LerpVectorsExample by Jeremy Douglass
+    amt = constrain(amt, 0, 1)  # let's play safe
+    if len(pts) == 1:
+        return pts[0]
+    cunit = 1.0 / (len(pts) - 1)
+    return lerp(pts[floor(amt / cunit)],
+                pts[ceil(amt / cunit)],
+                amt % cunit / cunit)
+
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -26,6 +26,16 @@ You can follow me on Mastodon [@villares@ciberlandia.pt](https://ciberlandia.pt/
 
 ---
 
+### sketch_2026_07_22
+
+![sketch_2026_07_22](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_07_22/sketch_2026_07_22.gif)
+
+[sketch_2026_07_22](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_07_22) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2026_07_21
 
 ![sketch_2026_07_21](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_07_21/sketch_2026_07_21.gif)
