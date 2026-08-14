1902258700d3505c554175a99b74d3f3082781ad
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-08-13 23:22:08 -0300

    sketch_2026_08_11

---
+++ 2026/sketch_2026_08_12/sketch_2026_08_12.py
@@ -0,0 +1,52 @@
+import py5
+
+from py5_tools import animated_gif
+
+from itertools import product
+
+def setup():
+    global p
+    py5.size(640, 640, py5.P3D)
+    py5.no_smooth()
+    #animated_gif('out.gif', frame_numbers=range(1, 101), duration=0.1)
+    
+    
+def draw():
+    py5.translate(320, 320)
+    py5.rotate_x(py5.radians(30))
+    py5.rotate(py5.radians(-30))
+    py5.translate(-320, -320, 200)
+    py5.background(100, 100, 128)
+    py5.no_fill()
+    py5.stroke(0)
+    py5.stroke_weight(1)
+    for x, y in product(range(10, 640, 20), repeat=2):
+        with py5.push_matrix():
+            d = 10 + (x + y) / 20
+            py5.translate(0, 0, d)
+            py5.circle(x, y, d)
+    py5.stroke(255)
+    for x, y in product(range(10, 640, 20), repeat=2):
+        with py5.push_matrix():
+            d = 10 + (x - y) / 20
+            py5.translate(0, 0, d)
+            py5.circle(x, y, d)
+        
+        
+        
+def key_pressed():
+    if py5.key == 's':
+        py5.save_frame('#####.png')
+
+
+def lerp_along_points(amt, pts):
+    # Based on LerpVectorsExample by Jeremy Douglass
+    amt = constrain(amt, 0, 1)  # let's play safe
+    if len(pts) == 1:
+        return pts[0]
+    cunit = 1.0 / (len(pts) - 1)
+    return lerp(pts[floor(amt / cunit)],
+                pts[ceil(amt / cunit)],
+                amt % cunit / cunit)
+
+py5.run_sketch(block=False)
--- docs/README.md
+++ docs/README.md
@@ -27,9 +27,29 @@ You can follow me on Mastodon [@villares@ciberlandia.pt](https://ciberlandia.pt/
 
 ---
 
+### sketch_2026_08_13
+
+![sketch_2026_08_13](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_08_13/sketch_2026_08_13.png)
+
+[sketch_2026_08_13](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_08_13) [[py5](https://py5coding.org/)]
+
+
+
+---
+
+### sketch_2026_08_12
+
+![sketch_2026_08_12](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_08_12/sketch_2026_08_12.png)
+
+[sketch_2026_08_12](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_08_12) [[py5](https://py5coding.org/)]
+
+
+
+---
+
 ### sketch_2026_08_11
 
-![sketch_2026_08_11](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_08_11/sketch_2026_08_11.gif)
+![sketch_2026_08_11](https://raw.githubusercontent.com/villares/sketch-a-day/main/2026/sketch_2026_08_11/sketch_2026_08_11original.gif)
 
 [sketch_2026_08_11](https://github.com/villares/sketch-a-day/tree/main/2026/sketch_2026_08_11) [[py5](https://py5coding.org/)]
 
