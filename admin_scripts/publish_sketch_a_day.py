6e0d619a753115d31cc7713cb3d088cf469b2eef
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-08-18 10:51:44 -0300

    Update publish_sketch_a_day.py

--- admin_scripts/publish_sketch_a_day.py
+++ admin_scripts/publish_sketch_a_day.py
@@ -1,10 +1,7 @@
 #! /home/villares/thonny-env/bin/python
 
-from dreamhost_deploy import deploy, OVERWRITE_HTML, SKIP_IMAGES, OVERWRITE_ASSETS
+from dreamhost_deploy import deploy
 
 deploy(
-    repo_keys=["sketch-a-day"],
-    newer_than=2,
-    overwrite_html=OVERWRITE_HTML,
-    overwrite_assets=True,
+    repo_keys=["sketch-a-day"]
 )
