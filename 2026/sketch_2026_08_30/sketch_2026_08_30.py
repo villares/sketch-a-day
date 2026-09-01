579767bb2ecddf4f1cf641951f8b32ebb88f1243
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-08-31 21:30:28 -0300

    30

--- 2026/sketch_2026_08_30/sketch_2026_08_30.py
+++ 2026/sketch_2026_08_30/sketch_2026_08_30.py
@@ -1,42 +1,50 @@
-from itertools import product
-
 import py5
 
 N = 10
-oi = oj = 0
+offset_x = offset_y = 0
+keys = set()
 
 def setup():
-    global grid_pos, cell_size
+    global cell_size
     py5.size(600, 600)
     py5.text_align(py5.CENTER, py5.CENTER)
     py5.text_size(20)
     cell_size = py5.width / N
     py5.rect_mode(py5.CENTER)
-    grid_pos = list(product(range(N), repeat=2))
     
 def draw():
+    global offset_x, offset_y
     py5.background(200)
     py5.no_fill()
-    for i, j in grid_pos:
-        x = i * cell_size + cell_size / 2
-        y = j * cell_size + cell_size / 2
-        py5.square(x, y, cell_size)
-        py5.text(f'{i + oi},{j + oj}', x, y)
+        
+    si = int(offset_x / cell_size) - 1
+    ei = int((offset_x + py5.width) / cell_size) + 1
+    sj = int(offset_y / cell_size) - 1
+    ej = int((offset_y + py5.height) / cell_size) + 1
+    for i in range(si, ei):
+        for j in range(sj, ej):
+            x = i * cell_size + cell_size / 2 - offset_x
+            y = j * cell_size + cell_size / 2 - offset_y
+            py5.square(x, y, cell_size)
+            py5.text(f'{i},{j}', x, y)
 
-
+    if py5.LEFT in keys:
+        offset_x -= 2
+    if py5.RIGHT in keys:
+        offset_x += 2
+    if py5.UP in keys:
+        offset_y -= 2
+    if py5.DOWN in keys:
+        offset_y += 2
 
 def key_pressed():
-    global oi, oj
     if py5.key == 's':
         py5.save_frame('###.png')
     elif py5.key == py5.CODED:
-        if py5.key_code == py5.LEFT:
-            oi -= 1
-        if py5.key_code == py5.RIGHT:
-            oi += 1
-        if py5.key_code == py5.UP:
-            oj -= 1
-        if py5.key_code == py5.DOWN:
-            oj += 1
+        keys.add(py5.key_code)
+
+def key_released():
+    if py5.key == py5.CODED:
+        keys.discard(py5.key_code)
 
 py5.run_sketch(block=False)
